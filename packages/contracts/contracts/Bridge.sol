17d86b9c2d3d8cd9d85dc491e6f96207ee4abc8c
cwhinfrey <cwhinfrey@gmail.com>
2020-10-19 17:37:27 -0700

    Add initial test

--- packages/contracts/contracts/Bridge.sol
+++ packages/contracts/contracts/Bridge.sol
@@ -5,40 +5,34 @@ import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
 
 contract Bridge is ERC20 {
     using MerkleProof for bytes32[];
-    
+
     mapping(bytes32 => bool) withdrawalRoots;
-    
+
     constructor()
         public
         ERC20("DAI Liquidity Pool Token", "LDAI")
     {}
-    
+
     function setWithdrawalRoot(bytes32 _newWithdrawalRoot) public {
         withdrawalRoots[_newWithdrawalRoot] = true;
-    }
-
-    function hello() public pure returns (string memory) {
-        return "hello";
     }
 
     function withdraw(uint256 _amount, uint256 _withdrawalNonce, bytes32 _withdrawalRoot, bytes32[] memory _proof) public {
         bytes32 withdrawalHash = getWithdrawalHash(
             _amount,
             _withdrawalNonce,
-            msg.sender,
-            _withdrawalRoot
+            msg.sender
         );
         require(_proof.verify(_withdrawalRoot, withdrawalHash), "BDG: Invalid withdrawal proof");
 
         _mint(msg.sender, _amount);
     }
-    
-    function getWithdrawalHash(uint256 _amount, uint256 _withdrawalNonce, address _sender, bytes32 _withdrawalRoot) public pure returns (bytes32) {
+
+    function getWithdrawalHash(uint256 _amount, uint256 _withdrawalNonce, address _sender) public pure returns (bytes32) {
         return keccak256(abi.encode(
             _amount,
             _withdrawalNonce,
-            _sender,
-            _withdrawalRoot
+            _sender
         ));
     }
 }
--- packages/contracts/test/Bridge.test.ts
+++ packages/contracts/test/Bridge.test.ts
@@ -1,14 +1,112 @@
-import { ethers } from "@nomiclabs/buidler"
-import { Signer } from "ethers"
-import { expect } from "chai"
+import { expect } from 'chai'
+import { ethers } from '@nomiclabs/buidler'
+import { BigNumber, ContractFactory, Signer } from 'ethers'
+import MerkleTree from '../lib/MerkleTree'
 
-describe("Bridge", function() {
-  it("Should return the new greeting once it's changed", async function() {
-    const Bridge = await ethers.getContractFactory("Bridge")
+type WithdrawalProps = {
+  amount: BigNumber,
+  nonce: number,
+  sender: string
+}
+
+class Withdrawal {
+  amount: BigNumber
+  nonce: number
+  sender: string
+
+  constructor(props: WithdrawalProps) {
+    this.amount = props.amount
+    this.nonce = props.nonce
+    this.sender = props.sender
+  }
+
+  getWithdrawalHash(): Buffer {
+    const data = ethers.utils.defaultAbiCoder.encode(
+      [
+        'uint256',
+        'uint256',
+        'address'
+      ],
+      [
+        this.amount,
+        this.nonce,
+        this.sender
+      ]
+    )
+    const hash = ethers.utils.keccak256(data)
+    return Buffer.from(hash.slice(2), 'hex')
+  }
+}
+
+describe("Bridge", () => {
+  let accounts: Signer[]
+  let user: Signer
+  let Bridge: ContractFactory
+  let withdrawals: Withdrawal[]
+
+  before(async () => {
+    accounts = await ethers.getSigners()
+    user = accounts[0]
+    Bridge = await ethers.getContractFactory('Bridge')
+    withdrawals = [
+      new Withdrawal({
+        amount: BigNumber.from('12345'),
+        nonce: 0,
+        sender: await user.getAddress(),
+      }),
+      new Withdrawal({
+        amount: BigNumber.from('12345'),
+        nonce: 0,
+        sender: await accounts[1].getAddress(),
+      })
+    ]
+  })
+
+  it('Should allow for a withdrawal', async () => {
     const bridge = await Bridge.deploy()
-    
-    const resp = await bridge.hello()
-    console.log('resp: ', resp)
-    expect(resp).to.eq('hello')
-  });
-});
+
+    const withdrawalHashes = withdrawals.map( withdrawal => withdrawal.getWithdrawalHash() )
+
+    const tree = new MerkleTree(withdrawalHashes)
+    const root = tree.getRoot()
+
+    await bridge.setWithdrawalRoot(root)
+
+    const userInitialBalance: BigNumber = await bridge.balanceOf(user.getAddress())
+
+    await bridge.connect(user).withdraw(
+      withdrawals[0].amount,
+      withdrawals[0].nonce,
+      root,
+      tree.getProof(withdrawalHashes[0])
+    )
+
+    const userBalance: BigNumber = await bridge.balanceOf(user.getAddress())
+
+    const amountWithdrawn = userBalance.sub(userInitialBalance)
+
+    expect(amountWithdrawn.toString()).to.eq(withdrawals[0].amount.toString())
+  })
+
+  describe('Withdrawal', () => {
+    describe('getWithdrawalHash()', () => {
+      it('should match onchain hash calculation', async () => {
+        const bridge = await Bridge.deploy()
+
+        const onchainHashHex = await bridge.getWithdrawalHash(
+          withdrawals[0].amount,
+          withdrawals[0].nonce,
+          withdrawals[0].sender
+        )
+        const onchainHash = Buffer.from(
+          onchainHashHex.slice(2),
+          'hex'
+        )
+
+        const offchainHash = withdrawals[0].getWithdrawalHash()
+
+        expect(Buffer.compare(onchainHash, offchainHash)).to.eq(0)
+      })
+    })
+  })
+})

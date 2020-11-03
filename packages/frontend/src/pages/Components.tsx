ea05155d4b786f79114015452fd82f5c210a515e
cwhinfrey <cwhinfrey@gmail.com>
2020-11-03 15:47:59 -0800

    Add large button

--- packages/frontend/src/pages/Components.tsx
+++ packages/frontend/src/pages/Components.tsx
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
 import Card from '@material-ui/core/Card'
 import Typography from '@material-ui/core/Typography'
+import LargeButton from '../components/buttons/LargeButton'
+import SendIcon from '@material-ui/icons/Send'
 
 const useStyles = makeStyles(() => ({
   container: {
@@ -10,6 +12,13 @@ const useStyles = makeStyles(() => ({
   },
   section: {
     margin: '2.1rem'
+  },
+  sectionTitle: {
+    marginBottom: '2.8rem'
+  },
+  largeButton: {
+    width: '30.0rem',
+    marginBottom: '2.8rem'
   }
 }))
 
@@ -22,7 +31,7 @@ const Section: FC<SectionProps> = ({ title, children }) => {
 
   return (
     <Card className={styles.section}>
-      <Typography variant="h4">
+      <Typography variant="h4" className={styles.sectionTitle}>
         { title }
       </Typography>
       { children }
@@ -37,12 +46,20 @@ const Components: FC = () => {
     <Box display="flex" flexDirection="column" alignItems="center">
       <div className={styles.container}>
         <Section title="Cards">
-            <Typography variant="h6">
-              Your looking at one
-            </Typography>
-            <Typography variant="body1">
-              This is a card. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Typography>
+          <Typography variant="h6">
+            Your looking at one
+          </Typography>
+          <Typography variant="body1">
+            This is a card. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Typography>
+        </Section>
+        <Section title="Buttons">
+          <LargeButton
+            className={styles.largeButton}
+            startIcon={<SendIcon />}
+          >
+            LargeButton
+          </LargeButton>
         </Section>
         <Section title="Typography">
           <Typography variant="h1">

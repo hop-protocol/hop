939a42969a5c93369580bf1f0b1e2068ffbe56f3
Shane Fontaine <shane6fontaine@gmail.com>
2020-11-12 18:32:41 -0800

    Contracts: Redeploy L1 contracts with canonical DAI

--- packages/contracts/scripts/deployments/arbitrum/deployArbitrumL1.ts
+++ packages/contracts/scripts/deployments/arbitrum/deployArbitrumL1.ts
@@ -22,13 +22,13 @@ async function deployArbitrum () {
   L1_MessengerWrapper = await ethers.getContractFactory('contracts/wrappers/Arbitrum.sol:Arbitrum')
   MockMessenger = await ethers.getContractFactory('contracts/test/MockMessenger.sol:MockMessenger')
 
+  const DAI_ADDRESS = '0x7d669A64deb8a4A51eEa755bb0E19FD39CE25Ae9'
+  l1_poolToken = MockERC20.attach(DAI_ADDRESS)
+
   /**
    * Deployments
    */
 
-  // Deploy contracts
-  l1_poolToken = await MockERC20.deploy('Dai Stable Token', 'DAI')
-  await l1_poolToken.deployed()
   l1_bridge = await L1_Bridge.deploy(l1_poolToken.address)
   await l1_bridge.deployed()
   l1_messenger = await MockMessenger.deploy()

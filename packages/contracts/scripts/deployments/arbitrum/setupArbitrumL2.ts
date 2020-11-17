cd7716bd8c8f8a0c4e63d211d2ea717263a162b8
Shane Fontaine <shane6fontaine@gmail.com>
2020-11-16 18:45:38 -0800

    Contracts: Hook up Uniswap on L2

--- packages/contracts/scripts/deployments/arbitrum/setupArbitrumL2.ts
+++ packages/contracts/scripts/deployments/arbitrum/setupArbitrumL2.ts
@@ -1,52 +1,75 @@
 require('dotenv').config()
 
 import { ethers } from 'hardhat'
-import { ContractFactory, Contract } from 'ethers'
+import { BigNumber, ContractFactory, Contract, Signer, Wallet } from 'ethers'
 
-// const SWAP_DEADLINE_BUFFER = BigNumber.from('3600')
+const SWAP_DEADLINE_BUFFER = BigNumber.from('3600')
+const USER_INITIAL_BALANCE = BigNumber.from('1000000000000000000')
 
 async function deployArbitrum () {
+  let accounts: Signer[]
+  let user: Signer | Wallet
+
   // Factories
   let L1_Bridge: ContractFactory
   let L2_Bridge: ContractFactory
-  // let MockERC20: ContractFactory
-  // let UniswapRouter: ContractFactory
+  let MockERC20: ContractFactory
+  let UniswapRouter: ContractFactory
+  let UniswapFactory: ContractFactory
 
   // L1
   let l1_bridge: Contract
 
   // L2
   let l2_bridge: Contract
-  // let l2_uniswapRouter: Contract
-  // let l2_oDai: Contract
+  let l2_uniswapRouter: Contract
+  let l2_uniswapFactory: Contract
+  let l2_oDai: Contract
+
+  // Instantiate the wallets
+  accounts = await ethers.getSigners()
+  user = accounts[0]
 
   // Get the contract Factories
-  // MockERC20 = await ethers.getContractFactory('contracts/test/MockERC20.sol:MockERC20')
+  MockERC20 = await ethers.getContractFactory('contracts/test/MockERC20.sol:MockERC20')
   L1_Bridge = await ethers.getContractFactory('contracts/bridges/L1_Bridge.sol:L1_Bridge')
   L2_Bridge = await ethers.getContractFactory('contracts/bridges/L2_ArbitrumBridge.sol:L2_ArbitrumBridge')
-  // UniswapRouter = await ethers.getContractFactory('contracts/uniswap/UniswapV2Router02.sol:UniswapV2Router02')
+  UniswapRouter = await ethers.getContractFactory('contracts/uniswap/UniswapV2Router02.sol:UniswapV2Router02')
+  UniswapFactory = await ethers.getContractFactory('@uniswap/v2-core/contracts/UniswapV2Factory.sol:UniswapV2Factory')
 
   /**
    * Deployments
    */
 
   // Connect Contracts
-
   l1_bridge = L1_Bridge.attach('0xC9898E162b6a43dc665B033F1EF6b2bc7B0157B4')
+
   l2_bridge = L2_Bridge.attach('0xf8E96392b1Ba3B2FD88041894a93e089E93C0dcd')
-  // l2_uniswapRouter = UniswapRouter.attach('0x958F7a85E32e948Db30F7332ee809ED26B43298a')
-  // l2_oDai = MockERC20.attach('0x7d669a64deb8a4a51eea755bb0e19fd39ce25ae9')
+  l2_oDai = MockERC20.attach('0x7d669a64deb8a4a51eea755bb0e19fd39ce25ae9')
+  l2_uniswapRouter = UniswapRouter.attach('0xBae19197DFa25105E832b8fAfeAB88aCa275385F')
+  l2_uniswapFactory = UniswapFactory.attach('0xEaAec7a29B6ccE9e831C8d07e989fa4163026177')
 
   // Set up bridges
   await l2_bridge.setL1BridgeAddress(l1_bridge.address)
 
-
   // Set up Uniswap
-  // add liquidity
-  // get exchange address
+  await l2_oDai.approve(l2_uniswapRouter.address, USER_INITIAL_BALANCE.div(2))
+  await l2_bridge.approve(l2_uniswapRouter.address, USER_INITIAL_BALANCE.div(2))
+  await l2_uniswapRouter.addLiquidity(
+    l2_oDai.address,
+    l2_bridge.address,
+    USER_INITIAL_BALANCE.div(2),
+    USER_INITIAL_BALANCE.div(2),
+    '0',
+    '0',
+    await user.getAddress(),
+    '999999999999'
+  )
 
   // Finalize bridge
-  // await l2_bridge.setExchangeValues(SWAP_DEADLINE_BUFFER, exchange_address, l2_oDai.address)
+  const uniswapPairAddress: string = await l2_uniswapFactory.getPair(l2_oDai.address, l2_bridge.address)
+  await l2_bridge.setExchangeValues(SWAP_DEADLINE_BUFFER, uniswapPairAddress, l2_oDai.address)
+
 }
 
 /* tslint:disable-next-line */

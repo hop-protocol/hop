1bf19882b49ed1ddac47c1265b88b03fa3c9585e
Miguel Mota <miguelmota2@gmail.com>
2020-11-17 01:43:35 -0800

    bridge-node: move config to json file

--- packages/bridge-node/src/config/config.ts
+++ packages/bridge-node/src/config/config.ts
@@ -1,23 +1,31 @@
 require('dotenv').config()
+import {
+  L1RpcUrl,
+  L2RpcUrl,
+  L1BridgeAddress,
+  L2BridgeAddress,
+  L1PoolTokenAddress,
+  L1MessengerAddress,
+  L1MessengerWrapperAddress,
+  L2TokenAddress,
+  L2MessengerAddress,
+  L2UniswapFactory,
+  L2UniswapRouter,
+  L2CanonicalBridge
+} from './base.json'
 
 export const committeePrivateKey = process.env.COMMITTEE_PRIVATE_KEY
-
-export const L1RpcUrl = 'https://kovan.rpc.authereum.com'
-export const L2RpcUrl = 'https://kovan2.arbitrum.io/rpc'
-
-export const L1BridgeAddress = '0xC9898E162b6a43dc665B033F1EF6b2bc7B0157B4'
-export const L2BridgeAddress = '0xf8E96392b1Ba3B2FD88041894a93e089E93C0dcd'
-
-export const L1PoolTokenAddress = '0x7d669A64deb8a4A51eEa755bb0E19FD39CE25Ae9'
-
-export const L1MessengerAddress = '0x7b542e47Bf4aeC075cC2a3bB1871890AAfB0D5d9'
-export const L1MessengerWrapperAddress =
-  '0xaE254AC0EA0aA32bBdEb207C1F58e1bA98F0cF26'
-
-export const L2TokenAddress = '0x7d669a64deb8a4a51eea755bb0e19fd39ce25ae9'
-export const L2MessengerAddress = '0xDc08a1D8b62f02F55149D51F68504412fdF2Ce3c'
-
-export const L2UniswapFactory = '0xEaAec7a29B6ccE9e831C8d07e989fa4163026177'
-export const L2UniswapRouter = '0xBae19197DFa25105E832b8fAfeAB88aCa275385F'
-
-export const L2CanonicalBridge = '0x0000000000000000000000000000000000000064'
+export {
+  L1RpcUrl,
+  L2RpcUrl,
+  L1BridgeAddress,
+  L2BridgeAddress,
+  L1PoolTokenAddress,
+  L1MessengerAddress,
+  L1MessengerWrapperAddress,
+  L2TokenAddress,
+  L2MessengerAddress,
+  L2UniswapFactory,
+  L2UniswapRouter,
+  L2CanonicalBridge
+}

be24f118e10016eb58b93986d7f0ddc11c30eef9
cwhinfrey <cwhinfrey@gmail.com>
2020-11-20 13:54:51 -0800

    frontend: Add contracts hook

---
+++ packages/frontend/src/config/config.ts
@@ -0,0 +1,17 @@
+
+/**
+ * Addresses
+ */
+
+export const addresses = {
+  l1Dai: '0x7d669A64deb8a4A51eEa755bb0E19FD39CE25Ae9',
+  l1Messenger: '0xE681857DEfE8b454244e701BA63EfAa078d7eA85',
+  l1Bridge: '0xC9898E162b6a43dc665B033F1EF6b2bc7B0157B4',
+  l1BridgeWrapper: '0xb5cAC377180fcE007664Cc65ff044d685e0F1A3b',
+  arbitrumDai: '0x7d669a64deb8a4a51eea755bb0e19fd39ce25ae9',
+  arbitrumMessenger: '0x0000000000000000000000000000000000000064',
+  arbitrumBridge: '0xf8E96392b1Ba3B2FD88041894a93e089E93C0dcd',
+  arbitrumUniswapFactory: '0xEaAec7a29B6ccE9e831C8d07e989fa4163026177',
+  arbitrumUniswapRouter: '0xBae19197DFa25105E832b8fAfeAB88aCa275385F',
+  arbitrumUniswapExchange: '0xea535dF09be62d5542161D1a4A429A831d329638'
+}

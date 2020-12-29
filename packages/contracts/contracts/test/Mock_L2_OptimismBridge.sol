0bb23a6294542c5caed8c0145f81ccf9294ba201
cwhinfrey <cwhinfrey@gmail.com>
2020-12-29 10:33:29 -0800

    contracts: Add SPDX-License-Identifier to contracts

--- packages/contracts/contracts/test/Mock_L2_OptimismBridge.sol
+++ packages/contracts/contracts/test/Mock_L2_OptimismBridge.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
--- packages/contracts/contracts/test/mockOVM_CrossDomainMessenger.sol
+++ packages/contracts/contracts/test/mockOVM_CrossDomainMessenger.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 

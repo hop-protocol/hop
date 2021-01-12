ee9346e01525c33b1f73206e9c7a97045b55a055
Miguel Mota <miguelmota2@gmail.com>
2021-01-11 19:31:06 -0800

    FE: Only show DAI token on send page

--- packages/frontend/src/pages/Send/Send.tsx
+++ packages/frontend/src/pages/Send/Send.tsx
@@ -51,7 +51,15 @@ const useStyles = makeStyles(() => ({
 const Send: FC = () => {
   const styles = useStyles()
 
-  const { user, tokens, networks, contracts, txConfirm, txHistory } = useApp()
+  const {
+    user,
+    tokens: allTokens,
+    networks,
+    contracts,
+    txConfirm,
+    txHistory
+  } = useApp()
+  const tokens = allTokens.filter((token: Token) => token.symbol === 'DAI')
   const l1Bridge = contracts?.l1Bridge
   const arbitrumBridge = contracts?.arbitrumBridge
   const arbitrumUniswapRouter = contracts?.arbitrumUniswapRouter

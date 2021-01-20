74c20198de1ee65fb4f19f63cf1f0a0758fda7e9
Miguel Mota <miguelmota2@gmail.com>
2021-01-19 19:12:22 -0800

    bridge-node: Add error handling to arb bot

--- packages/bridge-node/src/config/config.ts
+++ packages/bridge-node/src/config/config.ts
@@ -12,8 +12,7 @@ export const L2ArbitrumBridgeAddress =
   process.env.L2_ARBITRUM_BRIDGE_ADDRESS || config.L2ArbitrumBridgeAddress
 export const L2OptimismBridgeAddress =
   process.env.L2_OPTIMISM_BRIDGE_ADDRESS || config.L2OptimismBridgeAddress
-export const L1PoolTokenAddress =
-  process.env.L1_POOL_TOKEN_ADDRESS || config.L1PoolTokenAddress
+export const L1DaiAddress = process.env.L1_DAI_ADDRESS || config.L1DaiAddress
 export const L1MessengerAddress =
   process.env.L1_MESSENGER_ADDRESS || config.L1MessengerAddress
 export const L1MessengerWrapperAddress =

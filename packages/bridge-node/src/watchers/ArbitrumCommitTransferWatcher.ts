3e48ec3482d67da1cf307371b252d3e12cc5abc3
Miguel Mota <miguelmota2@gmail.com>
2021-01-19 21:37:38 -0800

    bridge-node: throttle commit transfers on send event

--- packages/bridge-node/src/watchers/ArbitrumCommitTransferWatcher.ts
+++ packages/bridge-node/src/watchers/ArbitrumCommitTransferWatcher.ts
@@ -3,12 +3,12 @@ import assert from 'assert'
 import * as ethers from 'ethers'
 import wait from '@authereum/utils/core/wait'
 import L2ArbitrumBridgeContract from 'src/contracts/L2ArbitrumBridgeContract'
+import { TransferSentEvent } from 'src/constants'
 import chalk from 'chalk'
 import Logger from 'src/logger'
+import { throttle } from 'src/utils'
 
 const logger = new Logger('[commitTransferWatcher]', { color: 'yellow' })
-
-// TODO: trigger on event
 
 class CommitTransfersWatcher {
   async start () {
@@ -24,7 +24,7 @@ class CommitTransfersWatcher {
     return L2ArbitrumBridgeContract.commitTransfers()
   }
 
-  async check () {
+  check = throttle(async () => {
     try {
       const pendingTransfer = await L2ArbitrumBridgeContract.pendingTransfers(0)
       // check if pending transfers exist
@@ -40,13 +40,30 @@ class CommitTransfersWatcher {
     logger.log('L2 Arbitrum commitTransfers tx:', chalk.yellow(tx.hash))
     const receipt = await tx.wait()
     assert(receipt.status === 1)
+  }, 15 * 1000)
+
+  handleTransferSentEvent = async (
+    transferHash: string,
+    recipient: string,
+    amount: string,
+    transferNonce: string,
+    relayerFee: string,
+    meta: any
+  ) => {
+    try {
+      await this.check()
+    } catch (err) {
+      logger.error('commitTransfers tx error:', err.message)
+    }
   }
 
   async watch () {
+    L2ArbitrumBridgeContract.on(TransferSentEvent, this.handleTransferSentEvent)
+
     while (true) {
-      await wait(5 * 1000)
       try {
         await this.check()
+        await wait(10 * 1000)
       } catch (err) {
         logger.error('commitTransfers tx error:', err.message)
         await wait(20 * 1000)

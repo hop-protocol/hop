a5c5c0365b56a3bb907c5c4545a87a8cbc70e260
Shane Fontaine <shane6fontaine@gmail.com>
2021-04-29 14:38:43 -0700

    Add Polygon testnet addresses

--- packages/addresses/src/goerli.ts
+++ packages/addresses/src/goerli.ts
@@ -3,18 +3,18 @@ import { Addresses } from './types'
 export const addresses: Addresses = {
   USDC: {
     ethereum: {
-      l1CanonicalToken: '',
-      l1Bridge: ''
+      l1CanonicalToken: '0x655F2166b0709cd575202630952D71E2bB0d61Af',
+      l1Bridge: '0x9aA5870a735ea8c43D5ca1C9322fF6187430a044'
     },
     polygon: {
-      l1CanonicalBridge: '',
-      l2CanonicalBridge: '',
-      l2CanonicalToken: '',
-      l2Bridge: '',
-      l2HopBridgeToken: '',
-      l2AmmWrapper: '',
-      l2SaddleSwap: '',
-      l2SaddleLpToken: '',
+      l1CanonicalBridge: '0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74',
+      l2CanonicalBridge: '0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1',
+      l2CanonicalToken: '0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1',
+      l2Bridge: '0xFf0ecd10325E9E60d941e8864f45CcF73b929e9e',
+      l2HopBridgeToken: '0xcD1C7da9D80A03Aee7FB7743B5376612530DC8e5',
+      l2AmmWrapper: '0x560d954308D6236F5d77665533c3Ea1c31daF358',
+      l2SaddleSwap: '0x6C0DE4cA0901827dd5C3D56708B698B493F28004',
+      l2SaddleLpToken: '0x37e9dEcb4D1Eb26915729B40F08c997d4FfE7793',
       l1PosRootChainManager: '0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74',
       l1PosErc20Predicate: '0xdD6596F2029e6233DEFfaCa316e6A95217d4Dc34'
     }

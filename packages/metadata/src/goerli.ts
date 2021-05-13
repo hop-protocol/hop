482256c4eb6a49c398f95acd95c599631a3119bd
Miguel Mota <hello@miguelmota.com>
2021-05-13 14:53:55 -0700

    metadata: Use correct decimals for goerli USDC

--- packages/metadata/src/goerli.ts
+++ packages/metadata/src/goerli.ts
@@ -1,11 +1,5 @@
-import deepmerge from 'deepmerge'
 import { tokens } from './tokens'
-import { Tokens } from './types'
 
 export const metadata = {
-  tokens: deepmerge(tokens, {
-    USDC: {
-      decimals: 18 // TODO: Remove once new goerli contracts are deployed
-    }
-  }) as Tokens
+  tokens
 }

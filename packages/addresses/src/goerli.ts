4dff1e11a864f0c48cf8c8ae7d5d8ab0ef4cc544
Shane Fontaine <shane6fontaine@gmail.com>
2021-05-13 14:22:51 -0700

    Update DAI and USDC for goerli polygon

--- packages/addresses/src/goerli.ts
+++ packages/addresses/src/goerli.ts
@@ -3,19 +3,19 @@ import { Addresses } from './types'
 export const addresses: Addresses = {
   USDC: {
     ethereum: {
-      l1CanonicalToken: '0x655F2166b0709cd575202630952D71E2bB0d61Af',
-      l1Bridge: '0x9aA5870a735ea8c43D5ca1C9322fF6187430a044'
+      l1CanonicalToken: '0x98339D8C260052B7ad81c28c16C0b98420f2B46a',
+      l1Bridge: '0x325aD30CA387f845D5D9bb3e0a9E4c061b5ACc3A'
     },
     polygon: {
       l1CanonicalBridge: '0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74',
-      l1MessengerWrapper: '0x05E3B4A9C758b79776F98CbD15571fc7f5e8d2bD',
-      l2CanonicalBridge: '0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1',
-      l2CanonicalToken: '0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1',
-      l2Bridge: '0xFf0ecd10325E9E60d941e8864f45CcF73b929e9e',
-      l2HopBridgeToken: '0xcD1C7da9D80A03Aee7FB7743B5376612530DC8e5',
-      l2AmmWrapper: '0x560d954308D6236F5d77665533c3Ea1c31daF358',
-      l2SaddleSwap: '0x6C0DE4cA0901827dd5C3D56708B698B493F28004',
-      l2SaddleLpToken: '0x37e9dEcb4D1Eb26915729B40F08c997d4FfE7793',
+      l1MessengerWrapper: '0xbf4afb067Cdd452EC80f75862b2C6D39FeB96476',
+      l2CanonicalBridge: '0x6D4dd09982853F08d9966aC3cA4Eb5885F16f2b2',
+      l2CanonicalToken: '0x6D4dd09982853F08d9966aC3cA4Eb5885F16f2b2',
+      l2Bridge: '0x6cA4c985a0D67A37ac2F4d8370ebE74e248A53f2',
+      l2HopBridgeToken: '0xA93eC98Ead897Ee8EF0b5CC9E2a658875719B224',
+      l2AmmWrapper: '0x653354468D19e39Ae4843F91999A891fea508DF9',
+      l2SaddleSwap: '0x19365928c043172bD510FF926040ca3f397e1057',
+      l2SaddleLpToken: '0xf38d8966064e0751f164e77f43B3593eb16b234A',
       l1PosRootChainManager: '0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74',
       l1PosErc20Predicate: '0xdD6596F2029e6233DEFfaCa316e6A95217d4Dc34'
     }
@@ -23,18 +23,18 @@ export const addresses: Addresses = {
   DAI: {
     ethereum: {
       l1CanonicalToken: '0xC61bA16e864eFbd06a9fe30Aab39D18B8F63710a',
-      l1Bridge: '0x0842E4448577F53848fb357D5BA6937556d4Cc68'
+      l1Bridge: '0xA7d1733b49cBC400dC918d0286092C543FA03863'
     },
     polygon: {
       l1CanonicalBridge: '0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74',
-      l1MessengerWrapper: '0x0154b54B4CB747f8976971a1D501256d2A588817',
+      l1MessengerWrapper: '0x74d838E85EAF33Ec79D14738B08aB30229C01a70',
       l2CanonicalBridge: '0xb224913CE3851b0a0d7C0FB461eEF40f2e31ddb8',
       l2CanonicalToken: '0xb224913CE3851b0a0d7C0FB461eEF40f2e31ddb8',
-      l2Bridge: '0x27E5A055fcea534c46Ac01D90EEec8Df9A6cB109',
-      l2HopBridgeToken: '0x73Aac46baBE2C265868578E0292D0f778eD695E1',
-      l2AmmWrapper: '0x0596658A905AE95660d880eA7b0D60d243Bc0B17',
-      l2SaddleSwap: '0x4C7fF0e48524b6aB37603dFb800a04095893dDf5',
-      l2SaddleLpToken: '0x434e501FB37540Ff6757D15EdDD25d0Da0B97a90',
+      l2Bridge: '0xe756ec869624C2bC9fA0b49E877ad39b8e988008',
+      l2HopBridgeToken: '0xcCf4a395F47e4038C2B88C15cC725654af99329A',
+      l2AmmWrapper: '0xDA41463abD488937c770C77Adb996C20044fB1b1',
+      l2SaddleSwap: '0x22c5AcA60C2f6D832f8C11992fd52fcA8A0F10ec',
+      l2SaddleLpToken: '0x29204640A74f1E39f418865651c37ca1e3eD407C',
       l1PosRootChainManager: '0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74',
       l1PosErc20Predicate: '0x37c3bfC05d5ebF9EBb3FF80ce0bd0133Bf221BC8'
     }

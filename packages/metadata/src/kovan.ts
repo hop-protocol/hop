52435ba0e3666c646b79613f29150c03a87a69c6
Shane Fontaine <shane6fontaine@gmail.com>
2021-05-25 14:56:01 -0700

    metadata: use correct kovan decimals

--- packages/metadata/src/kovan.ts
+++ packages/metadata/src/kovan.ts
@@ -1,11 +1,5 @@
-import deepmerge from 'deepmerge'
 import { tokens } from './tokens'
-import { Tokens } from './types'
 
 export const metadata = {
-  tokens: deepmerge(tokens, {
-    USDC: {
-      decimals: 18 // TODO: Remove once new kovan contracts are deployed
-    }
-  }) as Tokens
+  tokens
 }

7e35c0b9cd8bc81cda991069dc809a8920b08e9a
cwhinfrey <cwhinfrey@gmail.com>
2021-06-01 18:00:33 -0700

    frontend: Update AmountSelectorCard

--- packages/frontend/src/pages/Convert/AmountSelectorCard.tsx
+++ packages/frontend/src/pages/Convert/AmountSelectorCard.tsx
@@ -1,28 +1,16 @@
-import React, {
-  FC,
-  ChangeEvent,
-  useState,
-  useEffect,
-  useCallback,
-  useRef
-} from 'react'
-import clsx from 'clsx'
-import { utils as ethersUtils } from 'ethers'
+import React, { useMemo, FC, ChangeEvent } from 'react'
+import { BigNumber } from 'ethers'
+import { formatUnits } from 'ethers/lib/utils'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
-import CircularProgress from '@material-ui/core/CircularProgress'
 import Typography from '@material-ui/core/Typography'
-import MenuItem from '@material-ui/core/MenuItem'
+import Skeleton from '@material-ui/lab/Skeleton'
+import clsx from 'clsx'
 import LargeTextField from 'src/components/LargeTextField'
-import FlatSelect from 'src/components/selects/FlatSelect'
-import Network from 'src/models/Network'
 import Token from 'src/models/Token'
-import { useApp } from 'src/contexts/AppContext'
-import useInterval from 'src/hooks/useInterval'
 import { commafy } from 'src/utils'
-import logger from 'src/logger'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -35,17 +23,32 @@ const useStyles = makeStyles(theme => ({
   topRow: {
     marginBottom: '1.8rem'
   },
+  networkSelectionBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
   networkLabel: {
-    marginLeft: theme.padding.extraLight,
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: '0.4rem',
     overflow: 'hidden',
     textOverflow: 'ellipsis'
   },
+  networkIconContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '4rem',
+    height: '4rem'
+  },
   networkIcon: {
+    display: 'flex',
     height: '2.6rem',
     margin: '0.5rem'
   },
   greyCircle: {
-    padding: '1.8rem',
+    margin: '0.5rem',
+    padding: '1.3rem',
     borderRadius: '1.8rem',
     backgroundColor: '#C4C4C4'
   },
@@ -66,83 +69,43 @@ const useStyles = makeStyles(theme => ({
 }))
 
 type Props = {
-  value: string
+  value?: string
   label: string
   token?: Token
   onChange?: (value: string) => void
-  selectedNetwork?: Network
-  networkOptions?: Network[]
-  onNetworkChange?: (network?: Network) => void
-  onBalanceChange?: (balance: number | null) => void
+  title?: string
+  titleIconUrl?: string
+  balance?: BigNumber
+  loadingBalance?: boolean
+  loadingValue?: boolean
+  disableInput?: boolean
   className?: string
 }
 
 const AmountSelectorCard: FC<Props> = props => {
   const {
-    value,
+    value = '',
     label,
     token,
     onChange,
-    selectedNetwork,
-    networkOptions,
-    onNetworkChange,
-    onBalanceChange,
+    title,
+    titleIconUrl,
+    balance,
+    loadingBalance = false,
+    loadingValue = false,
+    disableInput = false,
     className
   } = props
   const styles = useStyles()
-  const { user } = useApp()
 
-  const [balance, setBalance] = useState<string | null>(null)
-  // request debouncer so only latest request response is set
-  const debouncer = useRef<number>(0)
-
-  useEffect(() => {
-    if (onBalanceChange) {
-      onBalanceChange(Number(balance))
+  const balanceLabel = useMemo(() => {
+    let label: string = ''
+    if (token && balance) {
+      label = formatUnits(balance, token?.decimals)
+      label = commafy(label, 4)
     }
+    return label
   }, [balance])
-
-  const getBalance = useCallback(() => {
-    const ctx = debouncer.current
-    const _getBalance = async () => {
-      if (user && token && selectedNetwork) {
-        try {
-          const _balance = await user.getBalance(token, selectedNetwork)
-          if (ctx === debouncer.current) {
-            setBalance(
-              ethersUtils.formatUnits(_balance.toString(), token.decimals)
-            )
-          }
-        } catch (err) {
-          setBalance(null)
-          throw err
-        }
-      }
-    }
-
-    _getBalance().catch(logger.error)
-  }, [user, token, selectedNetwork])
-
-  useEffect(() => {
-    // switching tabs will cause getBalance to be called with incorrect token
-    // so we wait until there's no more switching to get balance
-    debouncer.current++
-    setBalance(null)
-    const t = setTimeout(() => {
-      getBalance()
-    }, 10)
-    return () => {
-      clearTimeout(t)
-    }
-  }, [selectedNetwork])
-
-  useEffect(() => {
-    getBalance()
-  }, [getBalance, user, token, selectedNetwork, debouncer.current])
-
-  useInterval(() => {
-    getBalance()
-  }, 5e3)
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
@@ -152,7 +115,11 @@ const AmountSelectorCard: FC<Props> = props => {
   }
   const handleMaxClick = () => {
     if (onChange) {
-      onChange(Number(balance).toFixed(2))
+      let max = ''
+      if (balance && token) {
+        max = formatUnits(balance, token.decimals)
+      }
+      onChange(max)
     }
   }
 
@@ -167,70 +134,38 @@ const AmountSelectorCard: FC<Props> = props => {
         <Typography variant="subtitle2" color="textSecondary">
           {label}
         </Typography>
-        <div className={styles.balance}>
-          {Number(balance) > 0 ? (
-            <button className={styles.maxButton} onClick={handleMaxClick}>
-              MAX
-            </button>
-          ) : null}
-          <Typography variant="subtitle2" color="textSecondary">
-            Balance:{' '}
-            {!user ? (
-              '0.00'
-            ) : balance === null ? (
-              <CircularProgress size={12} />
-            ) : (
-              commafy(balance)
-            )}
-          </Typography>
-        </div>
+        {loadingBalance ? (
+          <Skeleton variant="text" width="15.0rem"></Skeleton>
+        ) : balance ? (
+          <div className={styles.balance}>
+            {balance.gt(0) && !disableInput ? (
+              <button className={styles.maxButton} onClick={handleMaxClick}>
+                MAX
+              </button>
+            ) : null}
+            <Typography variant="subtitle2" color="textSecondary">
+              Balance: {balanceLabel}
+            </Typography>
+          </div>
+        ) : null}
       </Box>
       <Grid container alignItems="center">
         <Grid item xs={6}>
-          <FlatSelect
-            value={selectedNetwork?.slug || 'default'}
-            onChange={event => {
-              const network = networkOptions?.find(
-                _network => _network.slug === event.target.value
-              )
-              if (onNetworkChange) {
-                onNetworkChange(network)
-              }
-            }}
-          >
-            {networkOptions && (
-              <MenuItem value="default" key={'select-network'}>
-                <Box display="flex" flexDirection="row" alignItems="center">
-                  <div className={styles.greyCircle} />
-                  <Typography
-                    variant="subtitle2"
-                    className={styles.networkLabel}
-                  >
-                    Select Network
-                  </Typography>
-                </Box>
-              </MenuItem>
-            )}
-            {(networkOptions || [selectedNetwork])?.map(
-              (network: Network | undefined, i: number) => (
-                <MenuItem value={network?.slug} key={i}>
-                  <Box display="flex" flexDirection="row" alignItems="center">
-                    <img
-                      src={network?.imageUrl}
-                      className={styles.networkIcon}
-                      alt={network?.name}
-                    />
-                    <Typography
-                      variant="subtitle2"
-                      className={styles.networkLabel}
-                    >
-                      {network?.name}
-                    </Typography>
-                  </Box>
-                </MenuItem>
-              )
-            )}
-          </FlatSelect>
+          <Box className={styles.networkSelectionBox}>
+            <Box className={styles.networkIconContainer}>
+              <img
+                src={titleIconUrl}
+                className={styles.networkIcon}
+                alt={title}
+              />
+            </Box>
+            <Typography
+              variant="subtitle2"
+              className={styles.networkLabel}
+            >
+              {title}
+            </Typography>
+          </Box>
         </Grid>
         <Grid item xs={6}>
           <LargeTextField
@@ -238,6 +173,8 @@ const AmountSelectorCard: FC<Props> = props => {
             onChange={handleInputChange}
             placeholder="0.0"
             units={token?.symbol}
+            disabled={disableInput}
+            loadingValue={loadingValue}
           />
         </Grid>
       </Grid>

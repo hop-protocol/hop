8ac78d4e1412f8a43424b1e0776006efd4ef954f
Shane Fontaine <shane6fontaine@gmail.com>
2021-06-09 18:22:36 -0700

    hop-node: add logic to return the expected challenge watcher

--- packages/hop-node/src/db/TransferRootsDb.ts
+++ packages/hop-node/src/db/TransferRootsDb.ts
@@ -109,20 +109,32 @@ class TransferRootsDb extends BaseDb {
     })
   }
 
+  // TODO: This should be a new DB for a TransferBond, not a TransferRoot
+  // This will add new requirements to this return statement
   async getChallengeableTransferRoots (): Promise<TransferRoot[]> {
     const transfers = await this.getTransferRoots()
     return transfers.filter(item => {
       return (
-        // TODO
+        !item.confirmed &&
+        !item.confirmedAt &&
+        item.bonded &&
+        !item.sentConfirmTx &&
+        !item.sentConfirmTxAt
       )
     })
   }
 
+  // TODO: This should be a new DB for a TransferBond, not a TransferRoot
+  // This will add new requirements to this return statement
   async getResolvableTransferRoots (): Promise<TransferRoot[]> {
     const transfers = await this.getTransferRoots()
     return transfers.filter(item => {
       return (
-        // TODO
+        !item.confirmed &&
+        !item.confirmedAt &&
+        item.bonded &&
+        !item.sentConfirmTx &&
+        !item.sentConfirmTxAt
       )
     })
   }

b12562018658b1ce9792da06fa0b19b207b7672b
cwhinfrey <cwhinfrey@gmail.com>
2021-06-30 16:57:15 -0700

    frontend: Add functionality to StakeWidget

--- packages/frontend/src/pages/Stake/Stake.tsx
+++ packages/frontend/src/pages/Stake/Stake.tsx
@@ -1,10 +1,14 @@
-import React, { FC, useState, useEffect } from 'react'
+import React, { FC, useState, useEffect, useMemo } from 'react'
+import { Contract } from 'ethers'
 import { makeStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
 import { Token } from '@hop-protocol/sdk'
+import { stakingRewardsAbi } from '@hop-protocol/abi'
 import { useApp } from 'src/contexts/AppContext'
+import { useWeb3Context } from 'src/contexts/Web3Context'
 import StakeWidget from 'src/pages/Stake/StakeWidget'
+import useAsyncMemo from 'src/hooks/useAsyncMemo'
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -15,12 +19,12 @@ const useStyles = makeStyles(theme => ({
 const Stake: FC = () => {
   const styles = useStyles()
 
-  const { bridges } = useApp()
-  const [rewardsTokens, setRewardsTokens] = useState<Token[]>()
+  const { bridges, sdk, networks } = useApp()
+  const { provider } = useWeb3Context()
+  const [stakingTokens, setStakingTokens] = useState<Token[]>()
 
   useEffect(() => {
     const fetchRewardsTokens = async () => {
-      console.log('fetchRewardsTokens')
       const bridge = bridges.find(bridge =>
         bridge.getTokenSymbol() === 'USDC'
       )
@@ -31,11 +35,41 @@ const Stake: FC = () => {
         tokens.push(lpToken)
       }
 
-      setRewardsTokens(tokens)
+      setStakingTokens(tokens)
     }
 
     fetchRewardsTokens()
   }, [bridges])
+
+  const stakingRewards = useAsyncMemo(async () => {
+    const polygonProvider = await sdk.getSignerOrProvider('polygon')
+    const _provider = provider?.network.name === 'polygon' ? provider : polygonProvider
+    return new Contract('0x20E145F873EaaAe46f69C98B7cE1f4BB8cC9644B', stakingRewardsAbi, _provider)
+  }, [sdk, provider])
+
+  const rewardsToken = useAsyncMemo(async () => {
+    const polygonProvider = await sdk.getSignerOrProvider('polygon')
+    const _provider = provider?.network.name === 'polygon' ? provider : polygonProvider
+
+    const token = new Token(
+      'goerli',
+      'polygon',
+      '0x101E9d2E3975d29DA9191F5933490a55916135a4',
+      6,
+      'hUSDC',
+      'Hop USDC',
+      '',
+      _provider
+    )
+
+    return token
+  }, [sdk, provider])
+
+  const polygonNetwork = useMemo(() => {
+    return networks.find(network => {
+      return network.slug === 'polygon'
+    })
+  }, [networks])
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
@@ -43,8 +77,14 @@ const Stake: FC = () => {
         Stake
       </Typography>
       <div className={styles.container}>
-        {rewardsTokens?.map(token => (
-          <StakeWidget token={token} key={token.symbol}/>
+        {stakingTokens?.map(token => (
+          <StakeWidget
+            network={polygonNetwork}
+            stakingToken={token}
+            rewardsToken={rewardsToken}
+            stakingRewards={stakingRewards}
+            key={token.symbol}
+          />
         ))}
       </div>
     </Box>
--- packages/frontend/src/pages/Stake/StakeWidget.tsx
+++ packages/frontend/src/pages/Stake/StakeWidget.tsx
@@ -1,67 +1,295 @@
-import React, { FC, useState, ChangeEvent } from 'react'
+import React, { FC, useState, useMemo } from 'react'
+import { BigNumber, Contract } from 'ethers'
+import { parseUnits } from 'ethers/lib/utils'
 import { makeStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
-import Typography from '@material-ui/core/Typography'
 import { Token } from '@hop-protocol/sdk'
 import { useApp } from 'src/contexts/AppContext'
+import { useWeb3Context } from 'src/contexts/Web3Context'
 import AmountSelectorCard from 'src/components/AmountSelectorCard'
 import Button from 'src/components/buttons/Button'
 import useBalance from 'src/hooks/useBalance'
+import useAsyncMemo from 'src/hooks/useAsyncMemo'
+import { UINT256 } from 'src/constants'
+import Network from 'src/models/Network'
+import Transaction from 'src/models/Transaction'
+import useStakeBalance from 'src/pages/Stake/useStakeBalance'
+import { toTokenDisplay } from 'src/utils'
+import Alert from 'src/components/alert/Alert'
+import usePollValue from 'src/hooks/usePollValue'
+import MuiButton from '@material-ui/core/Button'
 
 const useStyles = makeStyles(theme => ({
   buttons: {
     marginTop: theme.padding.default
   },
   button: {
-    margin: `0 ${theme.padding.light}`
+    margin: `0 ${theme.padding.light}`,
+    width: '17.5rem'
+  },
+  claimButton: {
+    marginTop: theme.padding.default
+  },
+  alert: {
+    marginTop: theme.padding.default
+  },
+  withdrawButton: {
+    marginTop: theme.padding.light,
+    opacity: 0.5
   }
 }))
 
 type Props = {
-  token: Token
+  network: Network | undefined
+  stakingToken: Token | undefined
+  rewardsToken: Token | undefined
+  stakingRewards: Contract | undefined
 }
 
-const Stake: FC<Props> = props => {
+const StakeWidget: FC<Props> = props => {
   const styles = useStyles()
-  const { token } = props
-  const { networks } = useApp()
+  const {
+    network,
+    stakingToken,
+    rewardsToken,
+    stakingRewards
+  } = props
+  const { networks, txConfirm, txHistory } = useApp()
+  const { checkConnectedNetworkId, address } = useWeb3Context()
+  const { stakeBalance } = useStakeBalance(stakingRewards, address)
+
+  const formattedStakeBalance = toTokenDisplay(stakeBalance, stakingToken?.decimals)
+
+  const earned = usePollValue<BigNumber>(async () => {
+    if (!address) return undefined
+    return stakingRewards?.earned(address.toString())
+  }, 5e3)
+
+  const formattedEarned = toTokenDisplay(earned, rewardsToken?.decimals, rewardsToken?.symbol)
 
   const polygon = networks.find(network =>
     network.slug === 'polygon'
   )
 
+  const { balance: lpBalance, loading: loadingLpBalance } = useBalance(stakingToken, polygon)
   const [amount, setAmount] = useState('')
-  const { balance: lpBalance, loading: loadingLpBalance } = useBalance(token, polygon)
+  const parsedAmount = (amount && stakingToken)
+    ? parseUnits(amount, stakingToken.decimals)
+    : undefined
+
+  const allowance = useAsyncMemo(async () => {
+    if (!stakingRewards) {
+      return undefined
+    }
+    return stakingToken?.allowance(stakingRewards.address)
+  }, [stakingToken, stakingRewards])
+
+  const needsApproval = useMemo(() => {
+    if (!allowance || !parsedAmount) return undefined
+    return allowance.lt(parsedAmount)
+  }, [allowance, parsedAmount])
+
+  const isStakeEnabled = useMemo(() => {
+    if (!parsedAmount || !lpBalance) return false
+    if (needsApproval) return false
+    if (parsedAmount.gt(lpBalance)) return false
+    return true
+  }, [parsedAmount, lpBalance, needsApproval])
+
+  const warning = useMemo(() => {
+    if (!parsedAmount || !lpBalance) return undefined
+    if (parsedAmount.gt(lpBalance)) {
+      return 'Insufficient balance'
+    }
+  }, [parsedAmount, lpBalance])
+
+  const approve = async () => {
+    if (
+      !stakingRewards ||
+      !network ||
+      !stakingToken
+    ) {
+      throw new Error('Undefined approval parameter')
+    }
+
+    const networkId = Number(network.networkId)
+    const isNetworkConnected = await checkConnectedNetworkId(networkId)
+    if (!isNetworkConnected) return
+
+    const tx = await txConfirm?.show({
+      kind: 'approval',
+      inputProps: {
+        tagline: `Allow Hop to spend your ${stakingToken.symbol} on ${network?.slug}`,
+        amount: amount,
+        token: stakingToken.symbol
+      },
+      onConfirm: async (approveAll: boolean) => {
+        const approveAmount = approveAll ? UINT256 : parsedAmount
+        return stakingToken.approve(
+          stakingRewards?.address,
+          approveAmount
+        )
+      }
+    })
+
+    await tx?.wait()
+    if (tx?.hash && network) {
+      txHistory?.addTransaction(
+        new Transaction({
+          hash: tx.hash,
+          networkName: network.slug,
+          token: stakingToken
+        })
+      )
+    }
+  }
+
+  const stake = async () => {
+    if (!stakingRewards) {
+      throw new Error('StakingRewards not instantiated')
+    }
+
+    if (!network) {
+      throw new Error('Network must be defined')
+    }
+
+    const networkId = Number(network.networkId)
+    const isNetworkConnected = await checkConnectedNetworkId(networkId)
+    if (!isNetworkConnected) return
+
+    const tx = await txConfirm?.show({
+      kind: 'stake',
+      inputProps: {
+        amount: amount,
+        token: stakingToken
+      },
+      onConfirm: async () => {
+        return stakingRewards.stake(parsedAmount)
+      }
+    })
+
+    if (tx?.hash && network) {
+      txHistory?.addTransaction(
+        new Transaction({
+          hash: tx.hash,
+          networkName: network.slug,
+          token: stakingToken
+        })
+      )
+    }
+  }
+
+  const claim = async () => {
+    if (!stakingRewards) {
+      throw new Error('StakingRewards not instantiated')
+    }
+
+    if (!network) {
+      throw new Error('Network must be defined')
+    }
+
+    const networkId = Number(network.networkId)
+    const isNetworkConnected = await checkConnectedNetworkId(networkId)
+    if (!isNetworkConnected) return
+
+    await stakingRewards.getReward()
+  }
+
+  const withdraw = async () => {
+    if (
+      !stakingRewards ||
+      !network ||
+      !stakeBalance
+    ) {
+      throw new Error('Missing withdraw param')
+    }
+
+    const networkId = Number(network.networkId)
+    const isNetworkConnected = await checkConnectedNetworkId(networkId)
+    if (!isNetworkConnected) return
+
+    const tx = await txConfirm?.show({
+      kind: 'withdrawStake',
+      inputProps: {
+        token: stakingToken,
+        amount: formattedStakeBalance
+      },
+      onConfirm: async (amountPercent: number) => {
+        if (!amountPercent) return
+
+        if (amountPercent === 100) {
+          return stakingRewards.exit()
+        }
+
+        const withdrawAmount = stakeBalance.mul(amountPercent).div(100)
+
+        return stakingRewards.withdraw(withdrawAmount)
+      }
+    })
+
+    if (tx?.hash && network) {
+      txHistory?.addTransaction(
+        new Transaction({
+          hash: tx.hash,
+          networkName: network.slug,
+          token: stakingToken
+        })
+      )
+    }
+  }
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
       <AmountSelectorCard
-        label="USDC-hUSDC LP"
+        label={`Staked: ${formattedStakeBalance}`}
         value={amount}
-        token={token}
+        token={stakingToken}
         onChange={setAmount}
-        title="Polygon"
+        title="USDC-hUSDC LP"
         balance={lpBalance}
         loadingBalance={loadingLpBalance}
       />
-      <Box className={styles.buttons} display="flex" flexDirection="row" alignItems="center">
+      <Alert severity="warning" text={warning} className={styles.alert}/>
+      <Box display="flex" flexDirection="column" alignItems="center">
         <Button
-          className={styles.button}
+          className={styles.claimButton}
           large
           highlighted
+          onClick={claim}
         >
-          Approve
+          Claim {formattedEarned}
         </Button>
-        <Button
-          className={styles.button}
-          large
-          disabled
-        >
-          Stake
-        </Button>
+        <Box className={styles.buttons} display="flex" flexDirection="row" alignItems="center">
+          <Button
+            className={styles.button}
+            large
+            highlighted={!!needsApproval}
+            disabled={!needsApproval}
+            onClick={approve}
+          >
+            Approve
+          </Button>
+          <Button
+            className={styles.button}
+            large
+            highlighted={needsApproval === false}
+            disabled={!isStakeEnabled}
+            onClick={stake}
+          >
+            Stake
+          </Button>
+        </Box>
+        {earned?.gt(0) && (
+          <MuiButton
+            className={styles.withdrawButton}
+            onClick={withdraw}
+          >
+            Withdraw
+          </MuiButton>
+        )}
       </Box>
     </Box>
   )
 }
 
-export default Stake
+export default StakeWidget

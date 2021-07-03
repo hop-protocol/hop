62a9d24b76a999756acf439407d30c74e7b574d3
cwhinfrey <cwhinfrey@gmail.com>
2021-07-02 20:24:28 -0700

    frontend: Add token query params

--- packages/frontend/src/contexts/AppContext/useBridges.ts
+++ packages/frontend/src/contexts/AppContext/useBridges.ts
@@ -1,27 +1,44 @@
 import { useMemo, useState, useEffect } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
+import qs from 'qs'
 import { Hop, HopBridge } from '@hop-protocol/sdk'
 import { addresses } from 'src/config'
 
 const useBridges = (sdk: Hop) => {
+  const history = useHistory()
+  const location = useLocation()
+  const queryParams = useMemo(() => {
+    return qs.parse(location.search, { ignoreQueryPrefix: true })
+  }, [location])
+
   const bridges = useMemo(() => {
     return Object.keys(addresses.tokens).map(symbol => {
       return sdk.bridge(symbol)
     })
   }, [sdk])
 
-  const [selectedBridge, setSelectedBridge] = useState<HopBridge>(bridges[0])
+  const [selectedBridge, _setSelectedBridge] = useState<HopBridge>(bridges[0])
 
   useEffect(() => {
-    const updatedBridge = bridges.find(bridge => {
-      return bridge.getTokenSymbol() === selectedBridge.getTokenSymbol()
+    const _selectedBridge = bridges.find(bridge =>
+      bridge.getTokenSymbol() === queryParams.token
+    )
+
+    if (_selectedBridge) {
+      _setSelectedBridge(_selectedBridge)
+    }
+  }, [queryParams, bridges])
+
+  const setSelectedBridge = (bridge: HopBridge) => {
+    queryParams.token = bridge.getTokenSymbol()
+
+    history.push({
+      pathname: location.pathname,
+      search: qs.stringify(queryParams)
     })
 
-    if (updatedBridge) {
-      setSelectedBridge(updatedBridge)
-    } else {
-      setSelectedBridge(bridges[0])
-    }
-  }, [selectedBridge, bridges])
+    _setSelectedBridge(bridge)
+  }
 
   return { bridges, selectedBridge, setSelectedBridge }
 }

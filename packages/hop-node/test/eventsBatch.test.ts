f6eef3d51d7aaadc454b5f64cffcf98f44a870bc
Miguel Mota <hello@miguelmota.com>
2021-07-07 14:03:56 -0700

    hop-node: Add events batch cache key test

--- packages/hop-node/test/eventsBatch.test.ts
+++ packages/hop-node/test/eventsBatch.test.ts
@@ -4,30 +4,84 @@ import Bridge from 'src/watchers/classes/Bridge'
 import { Chain, Token } from 'src/constants'
 import contracts from 'src/contracts'
 
-test(
-  'eventsBatch',
-  async () => {
-    const { l2Bridge } = contracts.get(Token.USDC, Chain.xDai)
-    const bridge = new Bridge(l2Bridge)
-    const { totalBlocks, batchBlocks } = config.sync[Chain.xDai]
-    const maxIterations = Math.round(totalBlocks / batchBlocks)
-    let iterations = 0
-    const remainder = Math.round(totalBlocks % batchBlocks)
+describe('eventsBatch', () => {
+  it(
+    'eventsBatch',
+    async () => {
+      const { l2Bridge } = contracts.get(Token.USDC, Chain.xDai)
+      const bridge = new Bridge(l2Bridge)
+      const { totalBlocks, batchBlocks } = config.sync[Chain.xDai]
+      const maxIterations = Math.round(totalBlocks / batchBlocks)
+      const remainder = Math.round(totalBlocks % batchBlocks)
+      let iterations = 0
 
-    expect(totalBlocks).toBeGreaterThanOrEqual(100000)
-    expect(totalBlocks).toBeLessThan(500000)
-    expect(batchBlocks).toBe(1000)
+      expect(totalBlocks).toBeGreaterThanOrEqual(100000)
+      expect(totalBlocks).toBeLessThan(500000)
+      expect(batchBlocks).toBe(1000)
 
-    await bridge.eventsBatch(async (start: number, end: number, i: number) => {
-      iterations++
-      if (iterations < maxIterations) {
-        expect(end - start).toBe(batchBlocks)
-      } else {
-        expect(end - start).toBe(remainder)
-      }
-    })
+      await bridge.eventsBatch(
+        async (start: number, end: number, i: number) => {
+          iterations++
+          if (iterations < maxIterations) {
+            expect(end - start).toBe(batchBlocks)
+          } else {
+            expect(end - start).toBe(remainder)
+          }
+        }
+      )
 
-    expect(iterations).toBe(maxIterations)
-  },
-  60 * 1000
-)
+      expect(iterations).toBe(maxIterations)
+    },
+    60 * 1000
+  )
+
+  it.skip(
+    'eventsBatch with cacheKey',
+    async () => {
+      const { l2Bridge } = contracts.get(Token.USDC, Chain.xDai)
+      const bridge = new Bridge(l2Bridge)
+      const { totalBlocks, batchBlocks } = config.sync[Chain.xDai]
+      const maxIterations = Math.round(totalBlocks / batchBlocks)
+      const remainder = Math.round(totalBlocks % batchBlocks)
+      const halfway = Math.round(maxIterations / 2)
+      const cacheKey = `${Date.now()}`
+      let iterations = 0
+
+      expect(totalBlocks).toBeGreaterThanOrEqual(100000)
+      expect(totalBlocks).toBeLessThan(500000)
+      expect(batchBlocks).toBe(1000)
+
+      let lastStart = 0
+      let lastEnd = 0
+
+      await bridge.eventsBatch(
+        async (start: number, end: number, i: number) => {
+          iterations++
+          // exit halfway through
+          if (iterations === halfway) {
+            lastStart = start
+            lastEnd = end
+            return false
+          }
+          return true
+        },
+        { cacheKey }
+      )
+
+      expect(iterations).toBe(halfway)
+
+      await bridge.eventsBatch(
+        async (start: number, end: number, i: number) => {
+          if (iterations === halfway) {
+            expect(start).toBe(lastStart)
+          }
+          iterations++
+        },
+        { cacheKey }
+      )
+
+      expect(iterations).toBe(maxIterations)
+    },
+    60 * 1000
+  )
+})

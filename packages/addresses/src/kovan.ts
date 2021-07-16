4bc88f93ca5ed36d7deb4db6e086b700b9f358b6
Shane Fontaine <shane6fontaine@gmail.com>
2021-07-15 19:11:59 -0700

    addresses/sdk: bump and release

--- packages/addresses/src/kovan.ts
+++ packages/addresses/src/kovan.ts
@@ -52,17 +52,17 @@ export const addresses: Addresses = {
         l1Amb: '0xFe446bEF1DbF7AFE24E81e05BC8B271C1BA9a560',
         l2Amb: '0xFe446bEF1DbF7AFE24E81e05BC8B271C1BA9a560',
         canonicalBridgeMaxPerTx: '10000'
-      },
+      }
       // optimism: {
-        // l1CanonicalBridge: '0xC48528a44f2D961D179D69434645E54ac85732a1',
-        // l1MessengerWrapper: '0x2cb57494fB3890C754CED2296071F228CAEee35d',
-        // l2CanonicalBridge: '0x7B2ab06D22Cd230102215532928f60770376a2B3',
-        // l2CanonicalToken: '0xFB6528eFbEe8B900CeBf2c4Cf709b1EF36D46A60',
-        // l2Bridge: '0xdb7C63d1Cb01877A91e915056FfC07Ff5a38b202',
-        // l2HopBridgeToken: '0x98C4d14e3D43E114fE40B6c13B199AA8F702bC90',
-        // l2AmmWrapper: '0x34A5BE7add88633D56b4F002C6f9D8abD1703654',
-        // l2SaddleSwap: '0x742DDbF5f4C9F70bDB85d980282cfDF09387697B',
-        // l2SaddleLpToken: '0x7076AC95eBb2514A2b17aC761BF7bcB404B23829'
+      // l1CanonicalBridge: '0xC48528a44f2D961D179D69434645E54ac85732a1',
+      // l1MessengerWrapper: '0x2cb57494fB3890C754CED2296071F228CAEee35d',
+      // l2CanonicalBridge: '0x7B2ab06D22Cd230102215532928f60770376a2B3',
+      // l2CanonicalToken: '0xFB6528eFbEe8B900CeBf2c4Cf709b1EF36D46A60',
+      // l2Bridge: '0xdb7C63d1Cb01877A91e915056FfC07Ff5a38b202',
+      // l2HopBridgeToken: '0x98C4d14e3D43E114fE40B6c13B199AA8F702bC90',
+      // l2AmmWrapper: '0x34A5BE7add88633D56b4F002C6f9D8abD1703654',
+      // l2SaddleSwap: '0x742DDbF5f4C9F70bDB85d980282cfDF09387697B',
+      // l2SaddleLpToken: '0x7076AC95eBb2514A2b17aC761BF7bcB404B23829'
       // }
     }
   }

74e121dc8d8ee87b18b12728f7c43149e88da9c5
Miguel Mota <hello@miguelmota.com>
2021-07-19 11:32:09 -0700

    hop-node: cli: Add filter flags for transfers table

--- packages/hop-node/src/cli/transfersTable.ts
+++ packages/hop-node/src/cli/transfersTable.ts
@@ -14,6 +14,10 @@ program
   .option('--config <string>', 'Config file to use.')
   .option('--env <string>', 'Environment variables file')
   .option('--chain <string>', 'Chain')
+  .option('--unbonded', 'Return only unbonded transfers')
+  .option('--uncommitted', 'Return only uncommitted transfers')
+  .option('--unconfirmed', 'Return only unconfirmed transfers')
+  .option('--unsettled', 'Return only unsettled transfers')
   .option('--transfer-id <string>', 'Transfer ID')
   .action(async (source: any) => {
     try {
@@ -24,27 +28,32 @@ program
       }
       const chain = source.chain
       const transferId = source.transferId
+      const filters = {
+        unbonded: !!source.unbonded,
+        uncommitted: !!source.uncommitted,
+        unconfirmed: !!source.unconfirmed,
+        unsettled: !!source.unsettled
+      }
       if (!chain) {
         throw new Error('chain is required')
       }
-      const headers = [
-        'transfer ID'.padEnd(68, ' '),
-        'bonded'.padEnd(10, ' '),
-        'committed'.padEnd(10, ' '),
-        'confirmed'.padEnd(10, ' '),
-        'rootSet'.padEnd(10, ' '),
-        'settled'.padEnd(10, ' '),
-        'amount'.padEnd(14, ' '),
-        'source'.padEnd(10, ' '),
-        'destination'.padEnd(12, ' '),
-        'timestamp'.padEnd(10, ' ')
-      ]
-      console.log(headers.join(' '))
+      const printHeaders = () => {
+        const headers = [
+          'transfer ID'.padEnd(68, ' '),
+          'bonded'.padEnd(10, ' '),
+          'committed'.padEnd(10, ' '),
+          'confirmed'.padEnd(10, ' '),
+          'rootSet'.padEnd(10, ' '),
+          'settled'.padEnd(10, ' '),
+          'amount'.padEnd(14, ' '),
+          'source'.padEnd(10, ' '),
+          'destination'.padEnd(12, ' '),
+          'timestamp'.padEnd(10, ' ')
+        ]
+        console.log(headers.join(' '))
+      }
       const printTransfer = (transfer: any) => {
         const { transferId, formattedAmount, sourceChain, destinationChain, bonded, committed, settled, transferRoot, timestampRelative } = transfer
-        if (settled) {
-          return
-        }
         const confirmed = !!transferRoot?.rootConfirmed
         const rootSet = !!transferRoot?.rootSet
         const needsSettlement = !!(bonded && confirmed && !settled)
@@ -72,12 +81,27 @@ program
         } else if (completed) {
           color = 'green'
         }
+        if (filters.unbonded && bonded) {
+          return false
+        }
+        if (filters.uncommitted && committed) {
+          return false
+        }
+        if (filters.unconfirmed && confirmed) {
+          return false
+        }
+        if (filters.unsettled && settled) {
+          return false
+        }
         console.log(color ? chalk[color](str) : str)
       }
       if (transferId) {
         const transfer = await getTransfer(chain, transferId)
+        printHeaders()
         printTransfer(transfer)
       } else {
+        console.log('searching all transfers. This will take a few minutes to complete.')
+        printHeaders()
         await getTransfers(chain, (transfer: any) => {
           printTransfer(transfer)
         })

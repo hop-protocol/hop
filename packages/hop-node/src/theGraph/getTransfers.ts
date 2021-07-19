6c4fca12c45ae0fbf698c5744c6f55910ec66b74
Miguel Mota <hello@miguelmota.com>
2021-07-18 20:50:09 -0700

    hop-node: cli: Transfers fetch rate limit fix

--- packages/hop-node/src/theGraph/getTransfers.ts
+++ packages/hop-node/src/theGraph/getTransfers.ts
@@ -2,12 +2,12 @@ import getTransfer from './getTransfer'
 import getTransferIds from './getTransferIds'
 
 export default async function getTransfers (chain: string, cb: any): Promise<any[]> {
-  const transfers = await getTransferIds(chain)
-  const promises = transfers.map(async (x: any) => {
+  const transferIds = await getTransferIds(chain)
+  const transfers : any[] = []
+  for (const x of transferIds) {
     const transfer = await getTransfer(chain, x.transferId)
+    transfers.push(transfer)
     cb(transfer)
-    return transfer
-  })
-
-  return Promise.all(promises)
+  }
+  return transfers
 }

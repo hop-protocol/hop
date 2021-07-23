298e201e3167ce55e76218516f80d3c66fe7270a
Miguel Mota <hello@miguelmota.com>
2021-07-23 15:07:02 -0700

    hop-node: Add config update command

---
+++ packages/hop-node/src/cli/showConfig.ts
@@ -0,0 +1,26 @@
+import {
+  Config,
+  parseConfigFile,
+  setGlobalConfigFromConfigFile
+} from './shared/config'
+import { logger, program } from './shared'
+
+program
+  .command('show-config')
+  .description('Update config file')
+  .option('--config <string>', 'Config file to use.')
+  .option('--env <string>', 'Environment variables file')
+  .action(async source => {
+    try {
+      const configPath = source?.config || source?.parent?.config
+      const config: Config = await parseConfigFile(configPath)
+      if (configPath) {
+        await setGlobalConfigFromConfigFile(config)
+      }
+      console.log(JSON.stringify(config, null, 2))
+      process.exit(0)
+    } catch (err) {
+      logger.error(err.message)
+      process.exit(1)
+    }
+  })

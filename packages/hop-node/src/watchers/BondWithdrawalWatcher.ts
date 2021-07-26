b3894ba5f835ec4286a7f09455585c9eea642da1
Miguel Mota <hello@miguelmota.com>
2021-07-26 15:47:06 -0700

    hop-node: Check transfer bonde state before sending

--- packages/hop-node/src/watchers/BondWithdrawalWatcher.ts
+++ packages/hop-node/src/watchers/BondWithdrawalWatcher.ts
@@ -252,13 +252,30 @@ class BondWithdrawalWatcher extends BaseWatcherWithEventHandlers {
       }
     }
 
+    await this.waitTimeout(transferId, destinationChainId)
+
+    const bondedAmount = await destBridge.getBondedWithdrawalAmount(transferId)
+    if (bondedAmount.gt(0)) {
+      logger.warn('transfer already bonded. Adding to db and skipping')
+      const event = await destBridge.getBondedWithdrawalEvent(transferId)
+      const { transactionHash } = event
+      const { from: sender } = await destBridge.getTransaction(
+        event.transactionHash
+      )
+      await this.db.transfers.update(transferId, {
+        withdrawalBonded: true,
+        withdrawalBonder: sender,
+        withdrawalBondedTxHash: transactionHash
+      })
+      return
+    }
+
     if (this.dryMode) {
       logger.warn('dry mode: skipping bondWithdrawalWatcher transaction')
       return
     }
-    logger.debug('sending bondWithdrawal tx')
 
-    await this.waitTimeout(transferId, destinationChainId)
+    logger.debug('sending bondWithdrawal tx')
 
     const { from: sender, data } = await sourceL2Bridge.getTransaction(
       transferSentTxHash

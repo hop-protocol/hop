486111b1ab0aea3ffbbd50ae1da3e08ffcfed35e
Shane Fontaine <shane6fontaine@gmail.com>
2021-07-28 18:28:16 -0700

    addresses/sdk: bump and release

--- packages/addresses/src/types.ts
+++ packages/addresses/src/types.ts
@@ -6,41 +6,41 @@ export type Bridges = {
       bridgeDeployedBlockNumber: number
     }
     arbitrum: {
-      l1CanonicalBridge: string,
-      l1MessengerWrapper: string,
-      l2CanonicalBridge: string,
-      l2CanonicalToken: string,
-      l2Bridge: string,
-      l2HopBridgeToken: string,
-      l2AmmWrapper: string,
-      l2SaddleSwap: string,
+      l1CanonicalBridge: string
+      l1MessengerWrapper: string
+      l2CanonicalBridge: string
+      l2CanonicalToken: string
+      l2Bridge: string
+      l2HopBridgeToken: string
+      l2AmmWrapper: string
+      l2SaddleSwap: string
       l2SaddleLpToken: string
       bridgeDeployedBlockNumber: number
     }
     optimism: {
-      l1CanonicalBridge: string,
-      l1MessengerWrapper: string,
-      l2CanonicalBridge: string,
-      l2CanonicalToken: string,
-      l2Bridge: string,
-      l2HopBridgeToken: string,
-      l2AmmWrapper: string,
-      l2SaddleSwap: string,
+      l1CanonicalBridge: string
+      l1MessengerWrapper: string
+      l2CanonicalBridge: string
+      l2CanonicalToken: string
+      l2Bridge: string
+      l2HopBridgeToken: string
+      l2AmmWrapper: string
+      l2SaddleSwap: string
       l2SaddleLpToken: string
       bridgeDeployedBlockNumber: number
     }
     polygon: {
-      l1CanonicalBridge: string,
-      l1MessengerWrapper: string,
-      l2CanonicalBridge: string,
-      l2CanonicalToken: string,
-      l2Bridge: string,
-      l2HopBridgeToken: string,
-      l2AmmWrapper: string,
-      l2SaddleSwap: string,
-      l2SaddleLpToken: string,
-      l1FxBaseRootTunnel: string,
-      l1PosRootChainManager: string,
+      l1CanonicalBridge: string
+      l1MessengerWrapper: string
+      l2CanonicalBridge: string
+      l2CanonicalToken: string
+      l2Bridge: string
+      l2HopBridgeToken: string
+      l2AmmWrapper: string
+      l2SaddleSwap: string
+      l2SaddleLpToken: string
+      l1FxBaseRootTunnel: string
+      l1PosRootChainManager: string
       l1PosPredicate: string
       bridgeDeployedBlockNumber: number
     }

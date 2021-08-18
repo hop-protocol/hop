7e10456cc96a242866a0d3372a9b4c6c5b610305
Miguel Mota <hello@miguelmota.com>
2021-08-18 13:43:21 -0700

    frontend: Fix stake APR calculate and use BigNumber

---
+++ packages/frontend/src/utils/shiftBNDecimals.ts
@@ -0,0 +1,8 @@
+import { BigNumber } from 'ethers'
+
+function shiftBNDecimals (bn: BigNumber, shiftAmount: number): BigNumber {
+  if (shiftAmount < 0) throw new Error('shiftAmount must be positive')
+  return bn.mul(BigNumber.from(10).pow(shiftAmount))
+}
+
+export default shiftBNDecimals

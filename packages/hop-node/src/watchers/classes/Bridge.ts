0ecdaa0f13aa571f17d90ddcb2b4975708f32116
Miguel Mota <hello@miguelmota.com>
2021-08-24 17:01:22 -0700

    hop-node: Fix token price big number conversion

--- packages/hop-node/src/watchers/classes/Bridge.ts
+++ packages/hop-node/src/watchers/classes/Bridge.ts
@@ -521,16 +521,16 @@ export default class Bridge extends ContractBase {
   }
 
   async compareBonderFeeCost (bonderFee: BigNumber, gasLimit: BigNumber, gasPrice: BigNumber) {
-    const usdBNPrecision = 100000
+    const bnMultiplier = 10000000
     const ethDecimals = 18
     const gasCost = gasLimit.mul(gasPrice)
     const ethUsdPrice = await this.priceFeed.getPriceByTokenSymbol('ETH')
     const tokenUsdPrice = await this.priceFeed.getPriceByTokenSymbol(this.tokenSymbol)
-    const tokenUsdPriceBn = BigNumber.from(tokenUsdPrice * usdBNPrecision)
-    const ethUsdPriceBn = BigNumber.from(ethUsdPrice * usdBNPrecision)
+    const tokenUsdPriceBn = BigNumber.from(tokenUsdPrice * bnMultiplier)
+    const ethUsdPriceBn = BigNumber.from(ethUsdPrice * bnMultiplier)
     const bonderFee18d = shiftBNDecimals(bonderFee, ethDecimals - this.tokenDecimals)
     const usdBonderFee = bonderFee18d
-    const usdGasCost = gasCost.mul(ethUsdPriceBn).div(usdBNPrecision)
+    const usdGasCost = gasCost.mul(ethUsdPriceBn).div(bnMultiplier)
     const isTooLow = bonderFee.eq(0) || usdBonderFee.lt(usdGasCost.div(2))
     if (isTooLow) {
       const usdBonderFeeFormatted = formatUnits(usdBonderFee, ethDecimals)

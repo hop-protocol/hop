70da81b3ad7c525724c768cac4855a3e4b5d10e6
Miguel Mota <hello@miguelmota.com>
2021-09-14 17:58:26 -0700

    sdk: Require minimum 1 dollar bonder fee

--- packages/sdk/src/priceFeed/PriceFeed.ts
+++ packages/sdk/src/priceFeed/PriceFeed.ts
@@ -2,9 +2,29 @@ import CoinGecko from './CoinGecko'
 
 class PriceFeed {
   private service = new CoinGecko()
+  cacheTimeMs = 5 * 60 * 1000
+
+  cache: {
+    [tokenSymbol: string]: {
+      timestamp: number
+      price: number
+    }
+  } = {}
 
   async getPriceByTokenSymbol (tokenSymbol: string) {
-    return this.service.getPriceByTokenSymbol(tokenSymbol)
+    const cached = this.cache[tokenSymbol]
+    if (cached) {
+      const isRecent = cached.timestamp > Date.now() - this.cacheTimeMs
+      if (isRecent) {
+        return cached.price
+      }
+    }
+    const price = await this.service.getPriceByTokenSymbol(tokenSymbol)
+    this.cache[tokenSymbol] = {
+      timestamp: Date.now(),
+      price
+    }
+    return price
   }
 }
 

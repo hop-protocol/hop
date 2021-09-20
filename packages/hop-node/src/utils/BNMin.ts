3afea207949554c39e2ce1bceda53901c275d4f6
Miguel Mota <hello@miguelmota.com>
2021-09-20 15:20:03 -0700

    hop-node: bn min/max util: Update account for null values

--- packages/hop-node/src/utils/BNMin.ts
+++ packages/hop-node/src/utils/BNMin.ts
@@ -1,7 +1,13 @@
 import { BigNumber } from 'ethers'
 
 function BNMin (a: BigNumber, b: BigNumber) {
-  return a.lt(b) ? a : b
+  if (!a) {
+    return b
+  }
+  if (!b) {
+    return a
+  }
+  return a?.lt(b) ? a : b
 }
 
 export default BNMin

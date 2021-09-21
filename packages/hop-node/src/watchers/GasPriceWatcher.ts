38663afbc2e9c05591ebc92c6d84800d7549dd5c
Miguel Mota <hello@miguelmota.com>
2021-09-20 18:18:38 -0700

    hop-node: GasPricesWatcher: Add error logger

--- packages/hop-node/src/watchers/GasPriceWatcher.ts
+++ packages/hop-node/src/watchers/GasPriceWatcher.ts
@@ -1,3 +1,4 @@
+import Logger from 'src/logger'
 import getRpcProvider from 'src/utils/getRpcProvider'
 import wait from 'src/utils/wait'
 import { Db, getGasPricesDb } from 'src/db'
@@ -12,11 +13,16 @@ class GasPriceWatcher {
   provider: providers.Provider
   db: Db
   intervalMs : number = 30 * 1000
+  logger: Logger
 
   constructor (config: Config) {
     this.chainSlug = config.chainSlug
     this.provider = getRpcProvider(this.chainSlug)
     this.db = getGasPricesDb()
+    this.logger = new Logger({
+      tag: 'GasPricesWatcher',
+      prefix: `${this.chainSlug}`
+    })
   }
 
   start () {
@@ -25,13 +31,17 @@ class GasPriceWatcher {
 
   async poll () {
     while (true) {
-      const gasPrice = await this.provider.getGasPrice()
-      const timestamp = Math.floor(Date.now() / 1000)
-      await this.db.addGasPrice({
-        chain: this.chainSlug,
-        gasPrice,
-        timestamp
-      })
+      try {
+        const gasPrice = await this.provider.getGasPrice()
+        const timestamp = Math.floor(Date.now() / 1000)
+        await this.db.addGasPrice({
+          chain: this.chainSlug,
+          gasPrice,
+          timestamp
+        })
+      } catch (err) {
+        this.logger.error(`poll error: ${err.message}`)
+      }
       await wait(this.intervalMs)
     }
   }

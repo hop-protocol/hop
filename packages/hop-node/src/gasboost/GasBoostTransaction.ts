f66ba15dcd326c2ea7735ea663ad453540844090
Shane Fontaine <shane6fontaine@gmail.com>
2021-10-10 11:48:10 -0700

    hop-node: add arbitrum node nonce error

--- packages/hop-node/src/gasboost/GasBoostTransaction.ts
+++ packages/hop-node/src/gasboost/GasBoostTransaction.ts
@@ -566,7 +566,7 @@ class GasBoostTransaction extends EventEmitter implements providers.TransactionR
 
         return tx
       } catch (err) {
-        const nonceTooLow = /(nonce.*too low|same nonce|already been used|NONCE_EXPIRED|OldNonce)/gi.test(err.message)
+        const nonceTooLow = /(nonce.*too low|same nonce|already been used|NONCE_EXPIRED|OldNonce|invalid transaction nonce)/gi.test(err.message)
         if (nonceTooLow) {
           this.logger.error(`nonce ${this.nonce} too low`)
           this.logger.error(err.message)

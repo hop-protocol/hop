25e21ecdb2f45f46974e3592f28faee3c3846ca7
Miguel Mota <hello@miguelmota.com>
2021-10-11 14:13:57 -0700

    hop-node: cli: Add AMM swap command

--- packages/hop-node/src/cli/swap.ts
+++ packages/hop-node/src/cli/swap.ts
@@ -1,15 +1,20 @@
+import L2Bridge from 'src/watchers/classes/L2Bridge'
+import contracts from 'src/contracts'
+import getCanonicalTokenSymbol from 'src/utils/getCanonicalTokenSymbol'
+import isHToken from 'src/utils/isHToken'
+import { BigNumber } from 'ethers'
+import { Chain, TokenIndex } from 'src/constants'
 import {
   FileConfig,
   parseConfigFile,
   setGlobalConfigFromConfigFile
 } from 'src/config'
-import { swap } from 'src/uniswap'
-
 import { logger, program } from './shared'
+import { swap as uniswapSwap } from 'src/uniswap'
 
 program
   .command('swap')
-  .description('Swap tokens on uniswap')
+  .description('Swap tokens on Uniswap or via Hop AMM')
   .option('--config <string>', 'Config file to use.')
   .option('--env <string>', 'Environment variables file')
   .option('--chain <string>', 'Chain')
@@ -47,17 +52,68 @@ program
       if (!max && !amount) {
         throw new Error('"from" token amount is required')
       }
+      if (fromToken === toToken) {
+        throw new Error('from-token and to-token cannot be the same')
+      }
+      const fromTokenIsHToken = isHToken(fromToken)
+      const toTokenIsHToken = isHToken(toToken)
+      const isAmmSwap = fromTokenIsHToken || toTokenIsHToken
+      let tx : any
+      if (isAmmSwap) {
+        logger.debug('L2 AMM swap')
+        if (fromTokenIsHToken && toTokenIsHToken) {
+          throw new Error('both from-token and to-token cannot be hTokens')
+        }
+        const fromTokenCanonicalSymbol = getCanonicalTokenSymbol(fromToken)
+        const toTokenCanonicalSymbol = getCanonicalTokenSymbol(toToken)
+        if (fromTokenCanonicalSymbol !== toTokenCanonicalSymbol) {
+          throw new Error('both from-token and to-token must be the same asset type')
+        }
+        if (chain === Chain.Ethereum) {
+          throw new Error('no AMM on Ethereum chain')
+        }
+        const token = fromTokenIsHToken ? toToken : fromToken
+        const l2BridgeContract = contracts.get(token, chain)?.l2Bridge
+        const l2Bridge = new L2Bridge(l2BridgeContract)
+        const amm = l2Bridge.amm
+        const ammWrapper = l2Bridge.ammWrapper
+        const amountIn = l2Bridge.parseUnits(amount)
+        let amountOut : BigNumber
+        if (fromTokenIsHToken) {
+          amountOut = await amm.calculateToHTokensAmount(amountIn)
+        } else {
+          amountOut = await amm.calculateFromHTokensAmount(amountIn)
+        }
 
-      const tx = await swap({
-        chain,
-        fromToken,
-        toToken,
-        amount,
-        max,
-        deadline,
-        slippage,
-        recipient
-      })
+        let fromTokenIndex : number
+        let toTokenIndex : number
+        if (fromTokenIsHToken) {
+          fromTokenIndex = TokenIndex.HopBridgeToken
+          toTokenIndex = TokenIndex.CanonicalToken
+        } else {
+          fromTokenIndex = TokenIndex.CanonicalToken
+          toTokenIndex = TokenIndex.HopBridgeToken
+        }
+
+        const slippageToleranceBps = (slippage || 0.5) * 100
+        const minBps = Math.ceil(10000 - slippageToleranceBps)
+        const minAmountOut = amountOut.mul(minBps).div(10000)
+
+        logger.debug(`attempting to swap ${amount} ${fromToken} for at least ${minAmountOut} ${toToken}`)
+        tx = await amm.swap(fromTokenIndex, toTokenIndex, amountIn, minAmountOut)
+      } else {
+        logger.debug('uniswap swap')
+        tx = await uniswapSwap({
+          chain,
+          fromToken,
+          toToken,
+          amount,
+          max,
+          deadline,
+          slippage,
+          recipient
+        })
+      }
       if (!tx) {
         throw new Error('tx object not received')
       }

8173f570d9c0b4eb35dee54d29ebaecb0293bbc0
Isaac Kang <isaac.kang@protonmail.ch>
2021-10-12 18:59:29 -0400

    add method to update tx in useTxHistory

--- packages/frontend/src/contexts/AppContext/useTxHistory.ts
+++ packages/frontend/src/contexts/AppContext/useTxHistory.ts
@@ -8,19 +8,26 @@ export interface TxHistory {
   setTransactions: (txs: Transaction[]) => void
   addTransaction: (tx: Transaction) => void
   clear: () => void
+  updateTransaction: (tx: Transaction) => void
 }
 
-const useTxHistory = (): TxHistory => {
+const useTxHistory = (defaultTxs?: Transaction[]): TxHistory => {
   // logger.debug('useTxHistory render')
   const [transactions, setTransactions] = useState<Transaction[]>(() => {
     try {
       const txs = loadState('recentTransactions')
+      if (!txs && defaultTxs) return defaultTxs
       if (!txs) return []
       return txs.map((obj: Transaction) => Transaction.fromObject(obj))
     } catch (err) {
       return []
     }
   })
+
+  function updateTransaction(tx: Transaction) {
+    const filtered = transactions.filter((t: Transaction) => t.hash !== tx.hash)
+    setTransactions(sortByRecentTimestamp([...filtered, tx]).slice(0, 3))
+  }
 
   const handleChange = useCallback(
     (pending: boolean, tx: Transaction) => {
@@ -62,6 +69,7 @@ const useTxHistory = (): TxHistory => {
     setTransactions,
     addTransaction,
     clear,
+    updateTransaction,
   }
 }
 

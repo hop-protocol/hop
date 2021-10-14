7e5becc64a0cd29670752cee971008d9e3e476b3
cwhinfrey <cwhinfrey@gmail.com>
2021-10-13 19:23:55 -0700

    frontend: Add ETH/WETH warning for Optimism

--- packages/frontend/src/components/TokenWrapper/TokenWrapper.tsx
+++ packages/frontend/src/components/TokenWrapper/TokenWrapper.tsx
@@ -35,6 +35,9 @@ const useStyles = makeStyles(theme => ({
     margin: `0 ${theme.padding.light}`,
     width: '17.5rem',
   },
+  warning: {
+    marginBottom: theme.padding.default
+  }
 }))
 
 export type Props = {
@@ -85,10 +88,16 @@ const TokenWrapper: FC<Props> = (props: Props) => {
     return null
   }
 
+  let wethWarning = ''
+  if (canonicalToken?.symbol === 'ETH' && props.network?.slug === 'optimism') {
+    wethWarning = 'ETH and WETH are the same asset on Optimism and wrapping/unwrapping will do nothing.'
+  }
+
   return (
     <Expandable title="Wrap/Unwrap">
       <Box display="flex" alignItems="center" className={styles.tokenWrapper}>
         <Box display="flex" flexDirection="column" alignItems="center" className={styles.root}>
+          <Alert severity="warning" text={wethWarning} className={styles.warning} />
           <AmountSelectorCard
             secondaryToken={canonicalToken}
             secondaryBalance={canonicalTokenBalance}

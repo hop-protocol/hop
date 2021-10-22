01f0b857b4da193ed50e8f2761c2172de90aa187
Shane Fontaine <shane6fontaine@gmail.com>
2021-10-21 19:51:52 -0700

    hop-node: handle arbitrum-specific gasPrice

--- packages/hop-node/src/watchers/classes/Bridge.ts
+++ packages/hop-node/src/watchers/classes/Bridge.ts
@@ -726,7 +726,11 @@ export default class Bridge extends ContractBase {
   ) {
     const chainNativeTokenSymbol = this.getChainNativeTokenSymbol(chain)
     const provider = getRpcProvider(chain)
-    const gasPrice = await provider.getGasPrice()
+    let gasPrice = await provider.getGasPrice()
+    // Arbitrum returns a gasPriceBid of 2x what is generally paid
+    if (this.chainSlug === Chain.Arbitrum) {
+      gasPrice = gasPrice.div(2)
+    }
     const gasCost = gasLimit.mul(gasPrice)
 
     const {

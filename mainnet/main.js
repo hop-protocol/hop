75416257e4add2be01a68d1e6c6e6324cb897959
Shane Fontaine <shane6fontaine@gmail.com>
2021-10-25 21:35:52 -0700

    feat: Add WBTC to explorer

--- mainnet/main.js
+++ mainnet/main.js
@@ -2,7 +2,7 @@
 
 const poll = true
 const fetchInterval = 10 * 1000
-const enabledTokens = ['USDC', 'USDT', 'DAI', 'MATIC', 'ETH']
+const enabledTokens = ['USDC', 'USDT', 'DAI', 'MATIC', 'ETH', 'WBTC']
 const enabledChains = ['ethereum', 'xdai', 'polygon', 'optimism', 'arbitrum']
 
 let queryParams = {}

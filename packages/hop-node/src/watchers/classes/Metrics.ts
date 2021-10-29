5c3642be20e9b9ef29c44aa1b8e66d43292b8343
Shane Fontaine <shane6fontaine@gmail.com>
2021-10-28 17:35:24 -0700

    hop-node: reintroduce removed coe

--- packages/hop-node/src/watchers/classes/Metrics.ts
+++ packages/hop-node/src/watchers/classes/Metrics.ts
@@ -1,22 +1,22 @@
-// import { metrics } from 'src/metrics'
+import { metrics } from 'src/metrics'
 
 class Metrics {
-  // metrics = metrics
+  metrics = metrics
 
   setBonderBalance (chain: string, token: string, balance: number) {
-    // this.metrics?.bonderBalance.set({ chain, token }, balance)
+    this.metrics?.bonderBalance.set({ chain, token }, balance)
   }
 
   setDisk (totalSize: number, freeSize: number, usedSize: number) {
-    // this.metrics?.totalDisk.set({}, totalSize)
-    // this.metrics?.freeDisk.set({}, freeSize)
-    // this.metrics?.usedDisk.set({}, usedSize)
+    this.metrics?.totalDisk.set({}, totalSize)
+    this.metrics?.freeDisk.set({}, freeSize)
+    this.metrics?.usedDisk.set({}, usedSize)
   }
 
   setMemory (totalMemory: number, freeMemory: number, usedMemory: number) {
-    // this.metrics?.totalMemory.set({}, totalMemory)
-    // this.metrics?.freeMemory.set({}, freeMemory)
-    // this.metrics?.usedMemory.set({}, usedMemory)
+    this.metrics?.totalMemory.set({}, totalMemory)
+    this.metrics?.freeMemory.set({}, freeMemory)
+    this.metrics?.usedMemory.set({}, usedMemory)
   }
 }
 

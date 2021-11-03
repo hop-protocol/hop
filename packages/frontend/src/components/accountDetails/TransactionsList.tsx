dd46ac3b73392a834d104d7ecbcdb471bac168d6
Isaac Kang <isaac.kang@protonmail.ch>
2021-11-03 00:02:54 -0400

    rider: show # of confirmations in tx history list
    addresses #252

--- packages/frontend/src/components/accountDetails/TransactionsList.tsx
+++ packages/frontend/src/components/accountDetails/TransactionsList.tsx
@@ -10,7 +10,10 @@ import TransactionStatus from './TransactionStatus'
 import { useTxStatusStyles } from './useTxStatusStyles'
 
 function TransactionRow({ tx, styles }: { tx: Transaction; styles: any }) {
-  const { completed, destCompleted } = useTransactionStatus(tx, tx.networkName)
+  const { completed, destCompleted, confirmations, networkConfirmations } = useTransactionStatus(
+    tx,
+    tx.networkName
+  )
 
   return (
     <Flex justifyBetween mb=".5rem" alignCenter>
@@ -26,6 +29,8 @@ function TransactionRow({ tx, styles }: { tx: Transaction; styles: any }) {
           link={tx.explorerLink}
           destNetworkName={tx.destNetworkName}
           styles={styles}
+          confirmations={confirmations}
+          networkWaitConfirmations={networkConfirmations}
         />
 
         <TransactionStatus

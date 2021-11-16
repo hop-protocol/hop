b05435023f54326b108f71c8862187aa5c271436
Isaac Kang <isaac.kang@protonmail.ch>
2021-11-15 21:06:04 -0500

    revert useContracts

--- packages/frontend/src/contexts/AppContext/useContracts.ts
+++ packages/frontend/src/contexts/AppContext/useContracts.ts
@@ -23,7 +23,6 @@ export type Contracts = {
   }
   getContract: (address: string, abi: ABI[], provider: Provider) => Contract | undefined
   getErc20Contract: (address: string, provider: Provider) => Contract
-  l1BridgeInterface: any
 }
 
 type TokenContracts = {
@@ -95,7 +94,6 @@ const useContracts = (networks: Network[], tokens: Token[]): Contracts => {
     providers,
     getContract,
     getErc20Contract,
-    l1BridgeInterface: tokenContracts.ETH.ethereum.l1Bridge.interface,
   }
 }
 

c3179f421d3ae059da82694782828264f1f7b7ce
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2021-11-17 18:28:21 -0800

    hop-node: update stake command

--- packages/hop-node/src/cli/stake.ts
+++ packages/hop-node/src/cli/stake.ts
@@ -1,53 +1,95 @@
 import chainSlugToId from 'src/utils/chainSlugToId'
+import BondWithdrawalWatcher from 'src/watchers/BondWithdrawalWatcher'
+import L1Bridge from 'src/watchers/classes/L1Bridge'
+import L2Bridge from 'src/watchers/classes/L2Bridge'
+import Token from 'src/watchers/classes/Token'
 import { BigNumber } from 'ethers'
+import { Chain } from 'src/constants'
+import {
+  findWatcher,
+  getWatchers
+} from 'src/watchers/watchers'
 import {
   parseConfigFile,
   setGlobalConfigFromConfigFile
 } from 'src/config'
-
+import wait from 'src/utils/wait'
 import { logger, program } from './shared'
 
-async function stake (
+async function sendTokensToL2 (
+  bridge: L1Bridge,
   chain: string,
-  token: string,
-  amount: number,
-  skipSendToL2: boolean = false
+  parsedAmount: BigNumber
+) {
+  logger.debug('Sending tokens to L1')
+  const tx = await bridge.convertCanonicalTokenToHopToken(
+    chainSlugToId(chain)!,
+    parsedAmount
+  )
+  await tx.wait()
+}
+
+async function stake (
+  bridge: L2Bridge | L1Bridge,
+  parsedAmount: BigNumber
 ) {
   logger.debug('Staking')
-  const isBonder = await this.bridge.isBonder()
+  const isBonder = await bridge.isBonder()
   if (!isBonder) {
     throw new Error('not an allowed bonder on chain')
   }
 
-  let tx
-  if (!skipSendToL2) {
-    const chainId: number = chainSlugToId(chain)!
-    tx = await this.bridge.sendCanonicalTokensToL2(
-      chainId,
-      amount
-    )
-    await tx.wait()
+  let stakeTokenBalance: BigNumber
+  const isStakeOnL2 = bridge.chainSlug !== Chain.Ethereum
+  if (isStakeOnL2) {
+    let token: Token = await (bridge as L2Bridge).hToken()
+    stakeTokenBalance = await token.getBalance()
+  } else {
+    stakeTokenBalance = await bridge.getBalance(await bridge.getBonderAddress())
   }
 
-  if (!token) {
-  const hTokenBalance = await this.bridge.getBalance()
-  const parsedAmount: BigNumber = this.bridge.parseUnits(amount)
-  if (hTokenBalance.lt(parsedAmount)) {
+  const formattedAmount = bridge.formatUnits(parsedAmount)
+  if (stakeTokenBalance.lt(parsedAmount)) {
     throw new Error(
       `not enough hToken balance to stake. Have ${this.bridge.formatUnits(
-        hTokenBalance
-      )}, need ${amount}`
+        stakeTokenBalance
+      )}, need ${formattedAmount}`
     )
   }
 
-  this.logger.debug(`attempting to stake ${amount} tokens`)
-  tx = await this.bridge.stake(amount)
-  this.logger.info(`stake tx: ${(tx.hash)}`)
+  logger.debug(`attempting to stake ${formattedAmount} tokens`)
+  const tx = await bridge.stake(parsedAmount)
+  logger.info(`stake tx: ${(tx.hash)}`)
   const receipt = await tx.wait()
   if (receipt.status) {
-    this.logger.debug(`stake successful`)
+    logger.debug(`stake successful`)
   } else {
-    this.logger.error('stake unsuccessful')
+    logger.error('stake unsuccessful')
+  }
+}
+
+async function pollConvertTxReceive (bridge: L2Bridge, convertAmount: BigNumber) {
+  const l2Bridge = bridge as L2Bridge
+  const bonderAddress = await bridge.getBonderAddress()
+  while (true) {
+    const blockNumber = await l2Bridge.getBlockNumber()
+    const start = blockNumber - 100
+    const end = blockNumber
+    const events = await l2Bridge.getTransferFromL1CompletedEvents(
+      start,
+      end
+    )
+    for (const event of events) {
+      const { amount, recipient } = event.args
+      if (recipient !== bonderAddress) {
+        continue
+      }
+      if (!amount.eq(convertAmount)) {
+        continue
+      }
+      return true
+    }
+    await wait(10 * 1000)
   }
 }
 
@@ -75,8 +117,37 @@ program
       if (!amount) {
         throw new Error('amount is required. E.g. 100')
       }
+      if (!chain) {
+        throw new Error('chain is required')
+      }
+      // Arbitrary watcher since only the bridge is needed
+      const watchers = await getWatchers({
+        enabledWatchers: ['bondWithdrawal'],
+        tokens: [token],
+        dryMode: false
+      })
 
-      await stake(chain, token, amount, skipSendToL2)
+      const watcher = findWatcher(watchers, BondWithdrawalWatcher, chain) as BondWithdrawalWatcher
+      if (!watcher) {
+        throw new Error('watcher not found')
+      }
+      const bridge: L2Bridge | L1Bridge = watcher.bridge
+      const parsedAmount: BigNumber = bridge.parseUnits(amount)
+
+      const isStakeOnL2 = chain !== Chain.Ethereum
+      if (isStakeOnL2 && !skipSendToL2) {
+        const l1Watcher = findWatcher(watchers, BondWithdrawalWatcher, Chain.Ethereum) as BondWithdrawalWatcher
+        if (!l1Watcher) {
+          throw new Error('watcher not found')
+        }
+        const l1Bridge: L1Bridge = (l1Watcher.bridge as L1Bridge)
+        await sendTokensToL2(l1Bridge, chain, parsedAmount)
+        logger.debug('Tokens sent to L2. Waiting for receipt on L2.')
+        await pollConvertTxReceive(bridge as L2Bridge, parsedAmount)
+        logger.debug('Tokens received on L2.')
+      }
+
+      await stake(bridge, parsedAmount)
       process.exit(0)
     } catch (err) {
       logger.error(err)

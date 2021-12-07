a363d3f6e46adcf3a1487cc7b52ccd2b253ed3a3
Isaac Kang <isaac.kang@protonmail.ch>
2021-12-06 21:44:55 -0500

    frontend: re-add the Settings icon/button and set button min width
    previously, the `ConnectWalletButton` would look unusual if the
    screen size was too small. this is now addressed by setting a minimum
    width of 12 rem.

--- packages/frontend/src/components/buttons/Button.tsx
+++ packages/frontend/src/components/buttons/Button.tsx
@@ -23,7 +23,7 @@ const useStyles = makeStyles(theme => ({
     textTransform: 'none',
     padding: large ? '0.8rem 4.2rem' : '0.8rem 2.8rem',
     height: large ? '6.0rem' : '4.0rem',
-    fontSize: large ? '2.2rem' : '1.6rem',
+    fontSize: large ? '2.2rem' : '1.5rem',
     color: highlighted ? 'white' : theme.palette.text.primary,
     background: highlighted
       ? theme.bgGradient.main
--- packages/frontend/src/components/buttons/StyledButton.tsx
+++ packages/frontend/src/components/buttons/StyledButton.tsx
@@ -1,8 +1,22 @@
 import { FC } from 'react'
 import styled from 'styled-components/macro'
-import { shadow, ShadowProps } from 'styled-system'
+import {
+  layout,
+  LayoutProps,
+  shadow,
+  ShadowProps,
+  space,
+  SpaceProps,
+  typography,
+  TypographyProps,
+} from 'styled-system'
 import Button, { ButtonProps } from './Button'
 
-export const StyledButton: FC<ShadowProps & ButtonProps> = styled(Button)`
-  ${shadow}
+export const StyledButton: FC<
+  SpaceProps & ShadowProps & TypographyProps & LayoutProps & ButtonProps
+> = styled(Button)`
+  ${typography}
+  ${space};
+  ${layout};
+  ${shadow};
 `

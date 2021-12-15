465eabc278d7d313295375e20a00562a7015beb4
Jack Collins <jackmpcollins@gmail.com>
2021-12-14 22:19:51 -0800

    Remove redundant ethers import

--- packages/frontend/src/components/header/TxPill.tsx
+++ packages/frontend/src/components/header/TxPill.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { ethers } from 'ethers';
 import { makeStyles } from '@material-ui/core/styles'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { useApp } from 'src/contexts/AppContext'

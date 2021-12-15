fd4d7840953a223d2c77403f1fed7bfe477be77f
Isaac Kang <isaac.kang@protonmail.ch>
2021-12-14 22:59:08 -0500

    frontend: add button to add asset to metamask
    users will have to switch networks in order to add the asset to
    the destination network of the transfer. this is an additional step,
    but we feel this feature would not be as useful if the user could
    only add the asset to the connected network

--- packages/frontend/src/components/ui/Icon.tsx
+++ packages/frontend/src/components/ui/Icon.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Circle } from 'react-feather'
 import styled from 'styled-components/macro'
 import { color, layout, space, SpaceProps, LayoutProps, ColorProps } from 'styled-system'
 
@@ -22,5 +23,12 @@ const Icon = ({
     <StyledIcon src={src} width={width || 24} alt={alt || `${src.slice(4)} icon`} color={color} />
   )
 }
+
+Icon.Circle = styled(Circle)<any>`
+  ${({ size }) => size || '16px'};
+  ${space};
+  ${layout};
+  ${color};
+`
 
 export default Icon
---
+++ packages/frontend/src/hooks/useAddTokenToMetamask.ts
@@ -0,0 +1,66 @@
+import { useCallback, useEffect, useState } from 'react'
+import { Token } from '@hop-protocol/sdk'
+import { useApp } from 'src/contexts/AppContext'
+import { useWeb3Context } from 'src/contexts/Web3Context'
+import { networkIdToSlug, wait } from 'src/utils'
+
+export function useAddTokenToMetamask(
+  token?: Token | null,
+  destNetworkName?: string | null
+): {
+  addToken: () => void
+  addTokenToDestNetwork: () => void
+  success?: boolean
+} {
+  const { sdk } = useApp()
+  const { connectedNetworkId, provider } = useWeb3Context()
+  const [success, setSuccess] = useState<boolean>()
+
+  const addToken = useCallback(
+    (networkId?: number) => {
+      if (provider && token) {
+        const { symbol, image, decimals } = token
+        const networkName = networkIdToSlug(networkId || connectedNetworkId)
+        const params = {
+          type: 'ERC20',
+          options: {
+            address: sdk.getL2CanonicalTokenAddress(symbol, networkName),
+            symbol,
+            decimals,
+            image,
+          },
+        }
+
+        provider
+          .send('wallet_watchAsset', params as any)
+          .then(setSuccess)
+          .catch(() => setSuccess(false))
+      } else {
+        setSuccess(false)
+      }
+    },
+    [token, provider, connectedNetworkId]
+  )
+
+  useEffect(() => {}, [provider])
+
+  const addTokenToDestNetwork = useCallback(async () => {
+    if (provider && token && destNetworkName) {
+      const destinationChain = sdk.toChainModel(destNetworkName)
+      if (destinationChain.chainId !== token.chain.chainId) {
+        await provider.send('wallet_switchEthereumChain', [
+          {
+            chainId: `0x${Number(destinationChain?.chainId).toString(16)}`,
+          },
+        ])
+      }
+
+      await wait(1500)
+      await addToken(destinationChain.chainId)
+    } else {
+      setSuccess(false)
+    }
+  }, [provider, token, destNetworkName])
+
+  return { addToken, addTokenToDestNetwork, success }
+}

133a5a3d0824618cfe27c954734ed7c3cdf23695
Miguel Mota <hello@miguelmota.com>
2021-12-19 22:56:35 -0800

    stats-worker: Add pool tvl stats

--- packages/stats-worker/src/worker.ts
+++ packages/stats-worker/src/worker.ts
@@ -1,18 +1,24 @@
 import AprStats from './AprStats'
 import VolumeStats from './VolumeStats'
+import TvlStats from './TvlStats'
 import S3Upload from './S3Upload'
 import wait from 'wait'
 
 class Worker {
   aprStats = new AprStats()
   volumeStats = new VolumeStats()
+  tvlStats = new TvlStats()
   hosting = new S3Upload()
   pollIntervalMs: number = 60 * 60 * 1000
 
   async start () {
     console.log('worker started')
     console.log(`polling every ${this.pollIntervalMs}ms`)
-    await Promise.all([this.aprStatsPoll(), this.volumeStatsPoll()])
+    await Promise.all([
+      this.aprStatsPoll(),
+      this.volumeStatsPoll(),
+      this.tvlStatsPoll()
+    ])
   }
 
   async volumeStatsPoll () {
@@ -22,6 +28,20 @@ class Worker {
         console.log('fetching volume stats')
         await this.volumeStats.trackDailyVolume()
         console.log('done tracking volume stats')
+      } catch (err) {
+        console.error(err)
+      }
+      await wait(this.pollIntervalMs)
+    }
+  }
+
+  async tvlStatsPoll () {
+    console.log('tvlStatsPoll started')
+    while (true) {
+      try {
+        console.log('fetching tvl stats')
+        await this.tvlStats.trackTvl()
+        console.log('done tracking tvl stats')
       } catch (err) {
         console.error(err)
       }

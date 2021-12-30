332ee2065cac3e6b2550e212ed60989f6c41d7a7
Isaac Kang <isaac.kang@protonmail.ch>
2021-12-30 04:23:28 -0500

    use ButtonLink component

--- packages/frontend/src/components/Button/index.ts
+++ packages/frontend/src/components/Button/index.ts
@@ -1,1 +1,2 @@
+export { Button } from './Button'
 export { ButtonLink } from './ButtonLink'
--- packages/frontend/src/pages/Claim/Claimed.tsx
+++ packages/frontend/src/pages/Claim/Claimed.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Div, Flex, Icon, SvgImg, Text } from 'src/components/ui'
+import { Div, Flex, Icon, Text } from 'src/components/ui'
 import { useAddTokenToMetamask } from 'src/hooks'
 import mmIcon from 'src/assets/logos/metamask.svg'
 import { ReactComponent as twitterIcon } from 'src/assets/logos/twitter.svg'
 import { ReactComponent as discordIcon } from 'src/assets/logos/discord.svg'
 import { StyledButton } from 'src/components/buttons/StyledButton'
+import { ButtonLink } from 'src/components/Button'
 
 export function Claimed() {
   const { addHopToken } = useAddTokenToMetamask()
@@ -16,32 +17,23 @@ export function Claimed() {
         Discord to get involved in governance
       </Text>
 
-      <StyledButton
-        width="250px"
-        bg="background.default"
-        py={3}
-        flat
+      <ButtonLink
+        href="https://discord.com/invite/PwCF88emV4"
         onClick={() => console.log('share twitter')}
+        iconColor="#00A2F5"
+        iconComponent={twitterIcon}
       >
-        <Flex alignCenter>
-          <SvgImg mr={3} size={24} color="#00A2F5" component={twitterIcon} />
-          <Div>Share on Twitter</Div>
-        </Flex>
-      </StyledButton>
+        Share on Twitter
+      </ButtonLink>
 
-      <StyledButton
-        mt={3}
-        width="250px"
-        bg="background.default"
-        py={3}
-        flat
+      <ButtonLink
+        href="https://discord.com/invite/PwCF88emV4"
         onClick={() => console.log('share discord')}
+        iconColor="#7289DA"
+        iconComponent={discordIcon}
       >
-        <Flex alignCenter>
-          <SvgImg mr={3} size={24} color="#7289DA" component={discordIcon} />
-          <Div>Join the Discord</Div>
-        </Flex>
-      </StyledButton>
+        Join the Discord
+      </ButtonLink>
 
       <StyledButton bg="background.default" py={3} mt={3} flat onClick={addHopToken}>
         <Div mr={2}>Add Hop Token to MetaMask</Div>

6baed2b090fd2a5c2bd42adc517cff8aa503f83d
Miguel Mota <hello@miguelmota.com>
2022-01-07 00:33:55 -0800

    stats-worker: Add start flags

--- packages/stats-worker/src/worker.ts
+++ packages/stats-worker/src/worker.ts
@@ -4,6 +4,12 @@ import TvlStats from './TvlStats'
 import S3Upload from './S3Upload'
 import wait from 'wait'
 
+type Options = {
+  apr?: boolean
+  tvl?: boolean
+  volume?: boolean
+}
+
 class Worker {
   aprStats = new AprStats()
   volumeStats = new VolumeStats()
@@ -11,14 +17,23 @@ class Worker {
   hosting = new S3Upload()
   pollIntervalMs: number = 60 * 60 * 1000
 
-  async start () {
+  async start (options: Options = {}) {
     console.log('worker started')
     console.log(`polling every ${this.pollIntervalMs}ms`)
-    await Promise.all([
-      this.aprStatsPoll(),
-      this.volumeStatsPoll(),
-      this.tvlStatsPoll()
-    ])
+    const promises: Promise<any>[] = []
+    if (options.apr) {
+      promises.push(this.aprStatsPoll())
+    }
+    if (options.tvl) {
+      promises.push(this.tvlStatsPoll())
+    }
+    if (options.volume) {
+      promises.push(this.volumeStatsPoll())
+    }
+    if (!promises.length) {
+      throw new Error('at least one option is required')
+    }
+    await Promise.all(promises)
   }
 
   async volumeStatsPoll () {

a2e9d18b1456f3a40b5fd4392b0b24dc21e02946
Miguel Mota <hello@miguelmota.com>
2022-01-23 20:30:56 -0800

    hop-node: cli: Update incomplete settlements poller to use on-chain events

--- packages/hop-node/src/cli/incompleteSettlements.ts
+++ packages/hop-node/src/cli/incompleteSettlements.ts
@@ -1,57 +1,21 @@
-import getIncompleteSettlements from 'src/theGraph/getIncompleteSettlements'
-import { Chain } from 'src/constants'
-import { actionHandler, parseBool, parseString, root } from './shared'
-
-const chains = [
-  Chain.Ethereum,
-  Chain.Arbitrum,
-  Chain.Optimism,
-  Chain.Gnosis,
-  Chain.Polygon
-]
+import IncompleteSettlementsWatcher from 'src/watchers/IncompleteSettlementsWatcher'
+import { actionHandler, parseString, root } from './shared'
 
 root
   .command('incomplete-settlements')
   .description('Get incomplete settlements')
-  .option('--source-chain <slug>', 'Source chain', parseString)
-  .option('--destination-chain <slug>', 'Destination Chain', parseString)
   .option('--token <symbol>', 'Token', parseString)
-  .option('--all-chains [boolean]', 'Check all destination chains', parseBool)
+  .option('--days <number>', 'Number of days to search', parseString)
+  .option('--format <string>', 'Output format. Options are "table,csv,json"', parseString)
   .action(actionHandler(main))
 
 async function main (source: any) {
-  const { config, token, allChains, sourceChain, destinationChain } = source
-  if (!token) {
-    throw new Error('token is required')
-  }
-  if (!sourceChain) {
-    throw new Error('source chain is required')
-  }
-  if (!destinationChain && !allChains) {
-    throw new Error('destination chain is required')
-  }
-  if (destinationChain && allChains) {
-    throw new Error('only destinationChain or allChains can be defined')
-  }
-
-  const destinationChains = []
-  if (allChains) {
-    for (const chain of chains) {
-      if (chain !== sourceChain) {
-        destinationChains.push(chain)
-      }
-    }
-  } else {
-    destinationChains.push(destinationChain)
-  }
-
-  for (const chain of destinationChains) {
-    console.log(`\n\n\nGetting incomplete settlements for token ${token}: source ${sourceChain} and destination ${chain}`)
-    const transferRoot = await getIncompleteSettlements(
-      token,
-      sourceChain,
-      chain
-    )
-    console.log(JSON.stringify(transferRoot, null, 2))
-  }
+  const { config, token, days, format } = source
+  const watcher = new IncompleteSettlementsWatcher({
+    token,
+    days,
+    format
+  })
+  await watcher.start()
+  console.log('done')
 }

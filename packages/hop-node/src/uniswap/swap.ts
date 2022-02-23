fb522cdd3d5574fe66f50a9df6ab14851b198818
Miguel Mota <hello@miguelmota.com>
2022-02-22 19:13:10 -0800

    hop-node: SwapCli: Add polygon uniswap addresses

--- packages/hop-node/src/uniswap/swap.ts
+++ packages/hop-node/src/uniswap/swap.ts
@@ -126,51 +126,72 @@ type Config = {
   toToken: string
   amount: number
   max?: boolean
-  recipient?: string
+  recipient: string
   slippage?: number
-  deadline?: number
+  deadline: number
 }
 
 const addresses: any = {
   ethereum: {
     swapRouter: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-    ethPools: {
-      USDC: '0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8',
-      USDT: '0x4e68ccd3e89f51c3074ca5072bbac773960dfa36',
-      DAI: '0x60594a405d53811d3bc4766596efd80fd545a270',
-      MATIC: '0x290A6a7460B308ee3F19023D2D00dE604bcf5B42'
+    pools: {
+      ETH: { // ETH is "toToken"
+        USDC: '0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8',
+        USDT: '0x4e68ccd3e89f51c3074ca5072bbac773960dfa36',
+        DAI: '0x60594a405d53811d3bc4766596efd80fd545a270',
+        MATIC: '0x290A6a7460B308ee3F19023D2D00dE604bcf5B42',
+        WBTC: '0x4585fe77225b41b697c938b018e2ac67ac5a20c0'
+      }
     }
   },
   optimism: {
     swapRouter: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-    ethPools: {
-      USDC: '0xB589969D38CE76D3d7AA319De7133bC9755fD840',
-      USDT: '0xc858A329Bf053BE78D6239C4A4343B8FbD21472b',
-      DAI: '0x03aF20bDAaFfB4cC0A521796a223f7D85e2aAc31'
+    pools: {
+      ETH: { // ETH is "toToken"
+        USDC: '0xB589969D38CE76D3d7AA319De7133bC9755fD840',
+        USDT: '0xc858A329Bf053BE78D6239C4A4343B8FbD21472b',
+        DAI: '0x03aF20bDAaFfB4cC0A521796a223f7D85e2aAc31',
+        WBTC: '0x73b14a78a0d396c521f954532d43fd5ffe385216'
+      }
     }
   },
   arbitrum: {
     swapRouter: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
-    ethPools: {
-      USDC: '0x17c14d2c404d167802b16c450d3c99f88f2c4f4d',
-      USDT: '0x641c00a822e8b671738d32a431a4fb6074e5c79d'
+    pools: {
+      ETH: { // ETH is "toToken"
+        USDC: '0x17c14d2c404d167802b16c450d3c99f88f2c4f4d',
+        USDT: '0x641c00a822e8b671738d32a431a4fb6074e5c79d',
+        DAI: '0xa961f0473da4864c5ed28e00fcc53a3aab056c1b',
+        WBTC: '0x2f5e87c9312fa29aed5c179e456625d79015299c'
+      }
+    }
+  },
+  // UNTESTED!
+  polygon: {
+    swapRouter: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
+    pools: {
+      MATIC: { // MATIC is "toToken"
+        USDC: '0xa374094527e1673a86de625aa59517c5de346d32',
+        USDT: '0x9b08288c3be4f62bbf8d1c20ac9c5e6f9467d8b7',
+        ETH: '0x86f1d8390222a3691c28938ec7404a1661e618e0'
+      }
     }
   }
 }
 
 export async function swap (config: Config) {
   let { chain, fromToken, toToken, amount, max, slippage, recipient, deadline } = config
-  if (toToken !== 'ETH') {
-    throw new Error('only ETH as the "to" token is supported at this time')
-  }
   if (!addresses[chain]) {
     throw new Error(`chain "${chain}" currently not supported at at this time`)
   }
-  const { swapRouter, ethPools } = addresses[chain]
+  const { swapRouter, pools } = addresses[chain]
   const wallet = wallets.get(chain)
-  const poolAddress = ethPools[fromToken]
+  if (!pools[toToken]) {
+    throw new Error(`"${toToken}" currently not supported at at this time`)
+  }
+  const poolAddress = pools[toToken][fromToken]
   if (!poolAddress) {
-    throw new Error(`"from" token "${fromToken}" is not supported at this time. Supported options are ${Object.keys(ethPools).join(',')}`)
+    throw new Error(`"from" token "${fromToken}" is not supported at this time. Supported options are ${Object.keys(pools[toToken]).join(',')}`)
   }
   if (amount && max) {
     throw new Error('only "amount" or "max" can be set, but not both')
@@ -190,6 +211,8 @@ export async function swap (config: Config) {
     wallet
   )
 
+  logger.debug('got pool information')
+
   const token1 = getToken(
     pool.token1.address,
     wallet
@@ -200,14 +223,14 @@ export async function swap (config: Config) {
   let routeToken1: any = pool.token1
 
   const token0Symbol = await token0.symbol()
-  if (token0Symbol === 'WETH' || token0Symbol === 'ETH') {
+  if ((chain !== 'polygon' && token0Symbol === 'WETH' || token0Symbol === 'ETH') || (chain === 'polygon' && token0Symbol === 'WMATIC' || token0Symbol === 'MATIC')) { // eslint-disable-line
     sourceToken = token1
     const tmp = routeToken0
     routeToken0 = routeToken1
     routeToken1 = tmp
   }
 
-  if (toToken === 'ETH') {
+  if ((chain !== 'polygon' && toToken === 'ETH') || (chain === 'polygon' && toToken === 'MATIC')) { // eslint-disable-line
     routeToken1 = Ether.onChain(chainSlugToId(chain)!) // eslint-disable-line
   }
 
@@ -223,15 +246,28 @@ export async function swap (config: Config) {
     parsedAmount = parseUnits(amount.toString(), decimals)
   }
 
+  logger.debug('getting trade route')
+
   const trade = await Trade.fromRoute(
     new Route([pool], routeToken0, routeToken1),
     CurrencyAmount.fromRawAmount(routeToken0, parsedAmount.toString()),
     TradeType.EXACT_INPUT
   )
 
+  logger.debug('got trade route')
+
   const slippageTolerance = new Percent((slippage ?? 1) * 100, 10000)
   recipient = recipient ?? sender
   deadline = (Date.now() / 1000 + (deadline || 300)) | 0 // eslint-disable-line @typescript-eslint/prefer-nullish-coalescing
+
+  if (!recipient) {
+    throw new Error('recipient is required')
+  }
+  if (!deadline) {
+    throw new Error('deadline is required')
+  }
+
+  logger.debug('getting swap call parameters')
 
   const { calldata, value } = SwapRouter.swapCallParameters(trade, {
     slippageTolerance,

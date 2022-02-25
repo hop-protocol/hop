9231a4c1e7e2739dcc6b433f99cee9237d83d02d
Miguel Mota <hello@miguelmota.com>
2022-02-25 13:38:01 -0800

    frontend: detailed fee breakdown in convert page. Closes #390

--- packages/frontend/src/hooks/useFeeConversions.ts
+++ packages/frontend/src/hooks/useFeeConversions.ts
@@ -1,35 +1,39 @@
 import { toTokenDisplay } from 'src/utils'
 import { useMemo } from 'react'
 
+export function getConvertedFees(destinationTxFee, bonderFee, estimatedReceived, destToken) {
+  // Base -> converted values (displayed to user)
+  const destinationTxFeeDisplay = toTokenDisplay(
+    destinationTxFee,
+    destToken?.decimals,
+    destToken?.symbol
+  )
+  const bonderFeeDisplay = toTokenDisplay(bonderFee, destToken?.decimals, destToken?.symbol)
+  const totalBonderFee =
+    destinationTxFee && bonderFee ? destinationTxFee.add(bonderFee) : destinationTxFee
+  const totalBonderFeeDisplay = toTokenDisplay(
+    totalBonderFee,
+    destToken?.decimals,
+    destToken?.symbol
+  )
+  const estimatedReceivedDisplay = toTokenDisplay(
+    estimatedReceived,
+    destToken?.decimals,
+    destToken?.symbol
+  )
+
+  return {
+    destinationTxFeeDisplay,
+    bonderFeeDisplay,
+    totalBonderFee,
+    totalBonderFeeDisplay,
+    estimatedReceivedDisplay,
+  }
+}
+
 export function useFeeConversions(destinationTxFee, bonderFee, estimatedReceived, destToken) {
   const convertedFees = useMemo(() => {
-    // Base -> converted values (displayed to user)
-    const destinationTxFeeDisplay = toTokenDisplay(
-      destinationTxFee,
-      destToken?.decimals,
-      destToken?.symbol
-    )
-    const bonderFeeDisplay = toTokenDisplay(bonderFee, destToken?.decimals, destToken?.symbol)
-    const totalBonderFee =
-      destinationTxFee && bonderFee ? destinationTxFee.add(bonderFee) : destinationTxFee
-    const totalBonderFeeDisplay = toTokenDisplay(
-      totalBonderFee,
-      destToken?.decimals,
-      destToken?.symbol
-    )
-    const estimatedReceivedDisplay = toTokenDisplay(
-      estimatedReceived,
-      destToken?.decimals,
-      destToken?.symbol
-    )
-
-    return {
-      destinationTxFeeDisplay,
-      bonderFeeDisplay,
-      totalBonderFee,
-      totalBonderFeeDisplay,
-      estimatedReceivedDisplay,
-    }
+    return getConvertedFees(destinationTxFee, bonderFee, estimatedReceived, destToken)
   }, [destinationTxFee, bonderFee, estimatedReceived, destToken])
 
   return convertedFees

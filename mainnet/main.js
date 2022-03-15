eb356debe6a2aaa076e2189faf8192f291a3154c
Miguel Mota <hello@miguelmota.com>
2022-03-15 14:13:08 -0700

    Update mainnet subgraph endpoint

--- mainnet/main.js
+++ mainnet/main.js
@@ -373,6 +373,11 @@ function getUrl (chain) {
   if (chain === 'gnosis') {
     chain = 'xdai'
   }
+
+  if (chain === 'mainnet') {
+    return 'https://gateway.thegraph.com/api/bd5bd4881b83e6c2c93d8dc80c9105ba/subgraphs/id/Cjv3tykF4wnd6m9TRmQV7weiLjizDnhyt6x2tTJB42Cy'
+  }
+
   return `https://api.thegraph.com/subgraphs/name/hop-protocol/hop-${chain}`
 }
 

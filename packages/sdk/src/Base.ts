2f76a95877a88b04b340e661e6c48d7254ce5ca5
Isaac Kang <isaac.kang@protonmail.ch>
2022-03-28 21:28:58 -0400

    fix stats page data population

--- packages/sdk/src/Base.ts
+++ packages/sdk/src/Base.ts
@@ -26,7 +26,7 @@ import {
 } from '@hop-protocol/core/contracts'
 import { BigNumber, BigNumberish, Signer, constants, providers } from 'ethers'
 import { Chain, Token as TokenModel } from './models'
-import { ChainSlug, Errors, MinPolygonGasPrice, NetworkSlug } from './constants'
+import { CanonicalToken, ChainSlug, Errors, MinPolygonGasPrice, NetworkSlug } from './constants'
 import { TChain, TProvider, TToken } from './types'
 import { config, metadata } from './config'
 import { getContractFactory, predeploys } from '@eth-optimism/contracts'
@@ -557,12 +557,21 @@ class Base {
   getSupportedAssets () {
     const supported : any = {}
 
+    const unsupportedAssets: any = {
+      optimism: CanonicalToken.MATIC,
+      arbitrum: CanonicalToken.MATIC,
+    }
+
     for (const token in this.addresses) {
       for (const chain in this.addresses[token]) {
         if (!supported[chain]) {
           supported[chain] = {}
         }
-        supported[chain][token] = true
+        if (chain in unsupportedAssets && unsupportedAssets[chain] === token) {
+          supported[chain][token] = false
+        } else {
+          supported[chain][token] = true
+        }
       }
     }
     return supported

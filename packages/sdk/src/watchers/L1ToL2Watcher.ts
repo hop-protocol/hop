f8a47f9eed7fd9871223076444fcafdd02733833
Miguel Mota <hello@miguelmota.com>
2022-04-12 18:24:39 -0700

    sdk: Update L2->L1 tx watcher

--- packages/sdk/src/watchers/L1ToL2Watcher.ts
+++ packages/sdk/src/watchers/L1ToL2Watcher.ts
@@ -1,9 +1,9 @@
 import EventEmitter from 'eventemitter3'
-import fetch from 'isomorphic-fetch'
 import { default as BaseWatcher } from './BaseWatcher'
 import { BigNumber } from 'ethers'
 import { DateTime } from 'luxon'
 import { EventNames } from '../constants'
+import { makeRequest } from './makeRequest'
 import {
   transferSentToL2Topic
 } from '../constants/eventTopics'
@@ -58,12 +58,11 @@ class L1ToL2Watcher extends BaseWatcher {
       }
       return false
     }
-    let tailBlock : number
     return async () => {
       const dateTime = DateTime.fromSeconds(this.sourceBlock.timestamp)
       const startTime = Math.floor(dateTime.minus({ hour: 1 }).toSeconds())
       const endTime = Math.floor(dateTime.plus({ hour: 24 }).toSeconds())
-      const events = await getL2Events(this.destinationChain.slug, startTime, endTime)
+      const events = await getTransferFromL1CompletedEvents(this.destinationChain.slug, startTime, endTime)
       for (const event of events) {
         if (event.recipient.toLowerCase() === recipient.toLowerCase()) {
           if (event.amount.toString() === amount.toString()) {
@@ -78,38 +77,7 @@ class L1ToL2Watcher extends BaseWatcher {
   }
 }
 
-async function makeQuery (url: string, query: string, variables?: any) {
-  const res = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    },
-    body: JSON.stringify({
-      query,
-      variables: variables || {}
-    })
-  })
-  const jsonRes = await res.json()
-  if (jsonRes.errors) {
-    throw new Error(jsonRes.errors[0].message)
-  }
-  return jsonRes.data
-}
-
-function getUrl (chain: string) {
-  if (chain === 'gnosis') {
-    chain = 'xdai'
-  }
-
-  if (chain === 'mainnet') {
-    // return 'https://gateway.thegraph.com/api/bd5bd4881b83e6c2c93d8dc80c9105ba/subgraphs/id/Cjv3tykF4wnd6m9TRmQV7weiLjizDnhyt6x2tTJB42Cy'
-  }
-
-  return `https://api.thegraph.com/subgraphs/name/hop-protocol/hop-${chain}`
-}
-
-async function getL2Events (chain: string, startTime: number, endTime: number) {
+async function getTransferFromL1CompletedEvents (chain: string, startTime: number, endTime: number) {
   const query = `
     query TransferFromL1Completed($startTime: Int, $endTime: Int) {
       events: transferFromL1Completeds(
@@ -132,8 +100,7 @@ async function getL2Events (chain: string, startTime: number, endTime: number) {
     }
   `
 
-  const url = getUrl(chain)
-  const data = await makeQuery(url, query, {
+  const data = await makeRequest(chain, query, {
     startTime,
     endTime
   })

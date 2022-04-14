d24347dd4f7bf22866483b0d2b3b26558769bc70
Isaac Kang <isaac.kang@protonmail.ch>
2022-04-14 12:56:02 -0400

    simplify spenders

--- packages/frontend/src/utils/nativeBridges.ts
+++ packages/frontend/src/utils/nativeBridges.ts
@@ -6,18 +6,10 @@ export const nativeBridges = {
       bridgeDeployedBlockNumber: 12650032,
     },
     gnosis: {
+      spender: '0x88ad09518695c6c3712AC10a214bE5109a655671', // ForeignOmnibridge
       l1CanonicalBridge: '0x88ad09518695c6c3712AC10a214bE5109a655671', // ForeignOmnibridge
-      l1MessengerWrapper: '0x12e59C59D282D2C00f3166915BED6DC2F5e2B5C7',
-      l2CanonicalBridge: '0xf6A78083ca3e2a662D6dd1703c939c8aCE2e268d',
-      l2CanonicalToken: '0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83',
-      l2Bridge: '0x25D8039bB044dC227f741a9e381CA4cEAE2E6aE8',
-      l2HopBridgeToken: '0x9ec9551d4A1a1593b0ee8124D98590CC71b3B09D',
-      l2AmmWrapper: '0x76b22b8C1079A44F1211D867D68b1eda76a635A7',
       l2SaddleSwap: '0x5C32143C8B198F392d01f8446b754c181224ac26', // USDC + hUSDC Swap
       l2SaddleLpToken: '0x9D373d22FD091d7f9A6649EB067557cc12Fb1A0A', // USDC LP Token
-      l1Amb: '0x4C36d2919e407f0Cc2Ee3c993ccF8ac26d9CE64e',
-      l2Amb: '0x75Df5AF045d91108662D8080fD1FEFAd6aA0bb59',
-      bridgeDeployedBlockNumber: 16617211,
     },
     polygon: {
       l1CanonicalBridge: '0xA0c68C638235ee32657e8f720a23ceC1bFc77C77', // PoS bridge
@@ -31,10 +23,12 @@ export const nativeBridges = {
       l2SaddleLpToken: '0x9D373d22FD091d7f9A6649EB067557cc12Fb1A0A', // USDC LP Token
       l1FxBaseRootTunnel: '0x4e9840f3C1ff368a10731D15c11516b9Fe7E1898',
       l1PosRootChainManager: '0xA0c68C638235ee32657e8f720a23ceC1bFc77C77',
+      spender: '0x40ec5B33f54e0E8A33A975908C5BA1c14e5BbbDf',
       l1PosPredicate: '0x40ec5B33f54e0E8A33A975908C5BA1c14e5BbbDf',
       bridgeDeployedBlockNumber: 15810014,
     },
     optimism: {
+      spender: '0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1',
       l1CanonicalBridge: '0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1',
       l1MessengerWrapper: '0x1ba1f1368ecEB7bFcbdE20e1F803771b7B401F7d',
       l2CanonicalBridge: '0x4200000000000000000000000000000000000010',
@@ -48,16 +42,7 @@ export const nativeBridges = {
     },
     arbitrum: {
       l1CanonicalBridge: '0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef', // L1GatewayRouter
-      l1MessengerWrapper: '0xaC9BABf20eF2338D7F4a152Af43bedDC80C6ae2a',
       l1Gateway: '0xcEe284F754E854890e311e3280b767F80797180d', // L1CustomGateway
-      l2CanonicalBridge: '0x0000000000000000000000000000000000000000',
-      l2CanonicalToken: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
-      l2Bridge: '0x0e0E3d2C5c292161999474247956EF542caBF8dd',
-      l2HopBridgeToken: '0x0ce6c85cF43553DE10FC56cecA0aef6Ff0DD444d',
-      l2AmmWrapper: '0xe22D2beDb3Eca35E6397e0C6D62857094aA26F52',
-      l2SaddleSwap: '0x10541b07d8Ad2647Dc6cD67abd4c03575dade261',
-      l2SaddleLpToken: '0xB67c014FA700E69681a673876eb8BAFAA36BFf71',
-      bridgeDeployedBlockNumber: 440027,
     },
   },
   USDT: {
@@ -67,20 +52,11 @@ export const nativeBridges = {
       bridgeDeployedBlockNumber: 12860139,
     },
     gnosis: {
+      spender: '0x88ad09518695c6c3712AC10a214bE5109a655671', // ForeignOmnibridge
       l1CanonicalBridge: '0x88ad09518695c6c3712AC10a214bE5109a655671', // ForeignOmnibridge
-      l1MessengerWrapper: '0xC08055b634D43F2176d721E26A3428D3b7E7DdB5',
-      l2CanonicalBridge: '0xf6A78083ca3e2a662D6dd1703c939c8aCE2e268d',
-      l2CanonicalToken: '0x4ECaBa5870353805a9F068101A40E0f32ed605C6',
-      l2Bridge: '0xFD5a186A7e8453Eb867A360526c5d987A00ACaC2',
-      l2HopBridgeToken: '0x91f8490eC27cbB1b2FaEdd29c2eC23011d7355FB',
-      l2AmmWrapper: '0x49094a1B3463c4e2E82ca41b8e6A023bdd6E222f',
-      l2SaddleSwap: '0x3Aa637D6853f1d9A9354FE4301Ab852A88b237e7',
-      l2SaddleLpToken: '0x5b10222f2Ada260AAf6C6fC274bd5810AF9d33c0',
-      l1Amb: '0x4C36d2919e407f0Cc2Ee3c993ccF8ac26d9CE64e',
-      l2Amb: '0x75Df5AF045d91108662D8080fD1FEFAd6aA0bb59',
-      bridgeDeployedBlockNumber: 17155195,
     },
     polygon: {
+      spender: '0xA0c68C638235ee32657e8f720a23ceC1bFc77C77', // PoS bridge
       l1CanonicalBridge: '0xA0c68C638235ee32657e8f720a23ceC1bFc77C77', // PoS bridge
       l1MessengerWrapper: '0x2D8b884f7aaEa1Dd13a805071530Ba9Ee9a7E035',
       l2CanonicalBridge: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
@@ -92,10 +68,12 @@ export const nativeBridges = {
       l2SaddleLpToken: '0x3cA3218D6c52B640B0857cc19b69Aa9427BC842C',
       l1FxBaseRootTunnel: '0x2D8b884f7aaEa1Dd13a805071530Ba9Ee9a7E035',
       l1PosRootChainManager: '0xA0c68C638235ee32657e8f720a23ceC1bFc77C77',
+      spender2: '0x40ec5B33f54e0E8A33A975908C5BA1c14e5BbbDf',
       l1PosPredicate: '0x40ec5B33f54e0E8A33A975908C5BA1c14e5BbbDf',
       bridgeDeployedBlockNumber: 17058878,
     },
     optimism: {
+      spender: '0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1', // Optimism: Gateway
       l1CanonicalBridge: '0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1', // Optimism: Gateway
       l1MessengerWrapper: '0xC78C53102e161094D848EE167145E5d45EAA6853',
       l2CanonicalBridge: '0x4200000000000000000000000000000000000010',
@@ -110,6 +88,7 @@ export const nativeBridges = {
     arbitrum: {
       l1CanonicalBridge: '0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef', // L1GatewayRouter
       l1MessengerWrapper: '0x70A772DDc3413e3456e5A3b2C30cB749C9577d1F',
+      spender: '0xcEe284F754E854890e311e3280b767F80797180d', // L1CustomGateway
       l1Gateway: '0xcEe284F754E854890e311e3280b767F80797180d', // L1CustomGateway
       l2CanonicalBridge: '0x0000000000000000000000000000000000000000',
       l2CanonicalToken: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
@@ -164,6 +143,7 @@ export const nativeBridges = {
       bridgeDeployedBlockNumber: 13226217,
     },
     gnosis: {
+      spender: '0x4aa42145Aa6Ebf72e164C9bBC74fbD3788045016', // POA xDAI bridge
       l1CanonicalBridge: '0x4aa42145Aa6Ebf72e164C9bBC74fbD3788045016', // POA xDAI bridge
       l1MessengerWrapper: '0x723194C6Cfed24883b598931Ee802ab80952C001',
       l2CanonicalBridge: '0x7301CFA0e1756B71869E93d4e4Dca5c7d0eb0AA6', // HomeBridgeErcToNative

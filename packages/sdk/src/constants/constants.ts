a298c0434b61b7992e1187b76ddf296abce7d4c5
Isaac Kang <isaac.kang@protonmail.ch>
2022-04-20 16:06:54 -0400

    address review comment

--- packages/sdk/src/constants/constants.ts
+++ packages/sdk/src/constants/constants.ts
@@ -1,3 +1,5 @@
+import { BigNumber } from 'ethers'
+
 export enum NetworkSlug {
   Mainnet = 'mainnet',
   Staging = 'staging',
@@ -91,6 +93,7 @@ export const SettlementGasLimitPerTx: Record<string, number> = {
 export const LpFeeBps = '4'
 export const PendingAmountBuffer = '50000'
 export const MinPolygonGasPrice = 30_000_000_000
+export const MinPolygonGasLimit = BigNumber.from(300_000)
 
 export enum Errors {
   NotEnoughAllowance = 'Not enough allowance. Please call `approve` on token contract to allow contract to move tokens.',

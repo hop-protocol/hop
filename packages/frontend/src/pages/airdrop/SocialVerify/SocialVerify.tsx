5f568eb3975843eea47b59bedda74d76ddd6e1bc
Miguel Mota <hello@miguelmota.com>
2022-04-27 12:20:56 -0700

    frontend: Airdrop: Add social auth permissions notice

--- packages/frontend/src/pages/airdrop/SocialVerify/SocialVerify.tsx
+++ packages/frontend/src/pages/airdrop/SocialVerify/SocialVerify.tsx
@@ -16,16 +16,21 @@ export function SocialVerify() {
           Please login with Discord or Twitter to verify your eligibility for the <strong>Active User</strong> airdrop tokens
         </Typography>
       </Box>
-      <Box>
-        <Box m={4} maxWidth={250}>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Box m={0} maxWidth={250}>
           <DiscordLoginButton onClick={() => {
             window.location.href = "https://social-auth.hop.exchange/discord/oauth"
             }} />
         </Box>
-        <Box m={4} maxWidth={250}>
+        <Box m={2} maxWidth={250}>
           <TwitterLoginButton onClick={() => {
             window.location.href = "https://social-auth.hop.exchange/twitter/oauth"
             }} />
+        </Box>
+        <Box marginBottom={4} m={3} maxWidth={500} textAlign="center">
+          <Typography variant="body2" color="textSecondary">
+            <em>We only require your username and user ID from the social media platforms for verification. Unfortunately the minimum permissions scope for Twitter ask for additional data, which we don't need or use.</em>
+          </Typography>
         </Box>
       </Box>
     </Box>

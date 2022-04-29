97f87f021110b7cbb813e0b0cfc803e407254618
Miguel Mota <hello@miguelmota.com>
2022-04-29 10:13:41 -0700

    Add per page options

--- pages/index.tsx
+++ pages/index.tsx
@@ -1526,6 +1526,9 @@ const Index: NextPage = () => {
                   <option value="25">25</option>
                   <option value="50">50</option>
                   <option value="100">100</option>
+                  <option value="250">250</option>
+                  <option value="500">500</option>
+                  <option value="1000">1000</option>
                 </select>
               </div>
               <div>
@@ -1752,6 +1755,9 @@ const Index: NextPage = () => {
               <option value="25">25</option>
               <option value="50">50</option>
               <option value="100">100</option>
+              <option value="250">250</option>
+              <option value="500">500</option>
+              <option value="1000">1000</option>
             </select>
           </div>
           <div className="pagination">

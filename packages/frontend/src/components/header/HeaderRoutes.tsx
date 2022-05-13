a05d4e5643b40dd0b31fa8e72c332099c629fe98
Miguel Mota <hello@miguelmota.com>
2022-05-12 22:22:53 -0700

    frontend: Claim: Style claim hop navbar button

--- packages/frontend/src/components/header/HeaderRoutes.tsx
+++ packages/frontend/src/components/header/HeaderRoutes.tsx
@@ -3,6 +3,7 @@ import { useLocation, useHistory } from 'react-router-dom'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import { isMainnet } from 'src/config'
+import { useClaim } from 'src/pages/Claim/useClaim'
 
 const HeaderRoutes: FC = () => {
   const { pathname, search } = useLocation()
@@ -17,15 +18,26 @@ const HeaderRoutes: FC = () => {
   }
 
   const value = pathname.split('/').slice(0, 2).join('/')
+  const { claimableTokens } = useClaim()
+  const canClaim = claimableTokens.gt(0)
 
   return (
-    <Tabs value={value || '/send'} onChange={handleChange}>
+    <Tabs value={value || '/send'} onChange={handleChange} style={{ width: 'max-content' }} variant="scrollable"
+  scrollButtons="auto">
       <Tab label="Send" value="/send" />
       <Tab label="Pool" value="/pool" />
       <Tab label="Convert" value="/convert" />
       <Tab label="Stake" value="/stake" />
       <Tab label="Airdrop" value="/airdrop" />
-      <Tab label="Claim" value="/claim" />
+      {canClaim && (
+        <Tab label="Claim HOP" value="/claim" style={{
+          background: 'rgba(0, 0, 0, 0) linear-gradient(99.85deg, rgb(179, 46, 255) -18.29%, rgb(242, 164, 152) 109.86%) repeat scroll 0% 0%',
+          color: '#fff',
+          padding: '1rem 3rem',
+          margin: '1rem 0 1rem 1rem',
+          opacity: 1
+        }}/>
+      )}
       {!isMainnet && <Tab label="Faucet" value="/faucet" />}
     </Tabs>
   )

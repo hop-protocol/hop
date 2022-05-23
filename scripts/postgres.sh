acdeb3f0c9800ebea91976f4200760513d2be74f
Miguel Mota <hello@miguelmota.com>
2022-05-23 13:57:10 -0700

    Add postgres.sh

---
+++ scripts/postgres.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+log_group_name="HopExplorerPostgres"
+
+docker stop pg
+docker rm pg
+docker run \
+        -d \
+        --name pg \
+        --net mynetwork \
+        --log-driver=awslogs \
+        --log-opt awslogs-region=us-east-1 \
+        --log-opt awslogs-group=$log_group_name \
+        --log-opt awslogs-create-group=true \
+        -p 5432:5432 \
+        -v $PWD/pgdata:/var/lib/postgresql/data \
+        -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" \
+        postgres

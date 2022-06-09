6bc652b7991deec1bc3b7f77c610597e24c7d540
Miguel Mota <hello@miguelmota.com>
2022-06-09 09:49:26 -0700

    frontend: Claim: Update config to mainnet

--- packages/frontend/src/pages/Claim/config.ts
+++ packages/frontend/src/pages/Claim/config.ts
@@ -1,15 +1,4 @@
-// TODO: update for prod
-// export const claimChainId = 1 // mainnet
-export const claimChainId = 5 // goerli
-
-// TODO: update for prod
-// export const claimTokenAddress = '0xc5102fe9359fd9a28f877a67e36b0f050d81a3cc' // mainnet
-export const claimTokenAddress = '0x5793728958f629894d9b0EB59e3a1B784677d764' // goerli
-
-// TODO: replace this with url from airdrop delegates repo
-// export const delegatesJsonUrl = 'https://raw.githubusercontent.com/hop-protocol/governance/master/delegates/delegates.json' // mainnet
-export const delegatesJsonUrl = 'https://gist.githubusercontent.com/miguelmota/ca85b07ea6bf0cec934b41656e585e43/raw/a2cbcde5ac356fe39aac45edfef40e62693ac7fc/delegates.json' // goerli
-
-// TODO: use github repo url once live
-// export const merkleBaseUrl = 'https://raw.githubusercontent.com/hop-protocol/governance/master/airdrops/mainnet' // mainnet
-export const merkleBaseUrl = 'https://gist.githubusercontent.com/miguelmota/86814b3bcd0bb8ffbd5b4fa9d1cb52ba/raw/a633c647f657b9a2c436ac8e8b8e11b805bba87a' // goerli
+export const claimChainId = 1 // mainnet
+export const claimTokenAddress = '0xc5102fe9359fd9a28f877a67e36b0f050d81a3cc' // mainnet
+export const delegatesJsonUrl = 'https://raw.githubusercontent.com/hop-protocol/governance/master/delegates/delegates.json' // mainnet
+export const merkleBaseUrl = 'https://raw.githubusercontent.com/hop-protocol/governance/master/airdrops/mainnet' // mainnet

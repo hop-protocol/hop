cacd8b0bdb426cac7807a46c763685e77a9d4602
Miguel Mota <hello@miguelmota.com>
2022-06-28 12:55:59 -0700

    hop-node: Change back to using lodash chunk until p-queue import error is figured out

--- packages/hop-node/src/utils/promiseQueue.ts
+++ packages/hop-node/src/utils/promiseQueue.ts
@@ -1,9 +1,23 @@
-import PQueue from 'p-queue'
+// import PQueue from 'p-queue'
+import { chunk } from 'lodash'
 
 export type Options = {
   concurrency: number
 }
 
+export async function promiseQueue (items: any[], cb: any, options: Options) {
+  const { concurrency } = options
+  const allChunks = chunk(items, concurrency)
+  let i = 0
+  for (const chunks of allChunks) {
+    await Promise.all(chunks.map(async (item) => {
+      i++
+      await cb(item, i)
+    }))
+  }
+}
+
+/*
 export async function promiseQueue (items: any[], cb: any, options: Options) {
   const { concurrency } = options
   const queue = new PQueue({ concurrency })
@@ -13,3 +27,4 @@ export async function promiseQueue (items: any[], cb: any, options: Options) {
   await queue.onEmpty()
   await queue.onIdle()
 }
+*/

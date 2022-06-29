7e957f0f82ae015119544f4f0519c387e55eaf60
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2022-06-28 20:09:49 -0700

    hop-node: check if the transfer is spent prior to bonding

--- packages/hop-node/src/cli/bondWithdrawal.ts
+++ packages/hop-node/src/cli/bondWithdrawal.ts
@@ -1,6 +1,7 @@
 import {
   getBondWithdrawalWatcher
 } from 'src/watchers/watchers'
+import { Chain } from 'src/constants'
 
 import { actionHandler, parseBool, parseString, parseStringArray, root } from './shared'
 
@@ -30,6 +31,12 @@ async function main (source: any) {
     throw new Error('watcher not found')
   }
 
+  const chain = Chain.Arbitrum
+  const arbitrumWatcher = await getBondWithdrawalWatcher({ token, dryMode, chain })
+  if (!watcher) {
+    throw new Error('watcher not found')
+  }
+
   for (const transferId of transferIds) {
     const dbTransfer: any = await watcher.db.transfers.getByTransferId(transferId)
     if (!dbTransfer) {
@@ -39,6 +46,11 @@ async function main (source: any) {
     if (dbTransfer.attemptSwap && dbTransfer.destinationChainId === 1) {
       throw new Error('Cannot bond transfer because a swap is being attempted on mainnet. Please withdraw instead.')
     }
+
+    // Check if spent and skip if so
+    const isSpent = await arbitrumWatcher.bridge.isTransferIdSpent(transferId)
+    if (isSpent) continue
+
     await watcher.sendBondWithdrawalTx(dbTransfer)
   }
 }

a88e241d48b3791e470c84b4366da428ddc026e8
Miguel Mota <hello@miguelmota.com>
2022-06-29 11:00:01 -0700

    hop-node: rateLimitRetry: Add additional checks

--- packages/hop-node/src/utils/rateLimitRetry.ts
+++ packages/hop-node/src/utils/rateLimitRetry.ts
@@ -4,12 +4,12 @@ import wait from 'src/utils/wait'
 import { Notifier } from 'src/notifier'
 import { hostname, rateLimitMaxRetries, rpcTimeoutSeconds } from 'src/config'
 
-const logger = new Logger('rateLimitRetry')
+const _logger = new Logger('rateLimitRetry')
 const notifier = new Notifier(`rateLimitRetry, host: ${hostname}`)
 
 export default function rateLimitRetry<FN extends (...args: any[]) => Promise<any>> (fn: FN): (...args: Parameters<FN>) => Promise<Awaited<ReturnType<FN>>> {
   const id = `${process.hrtime.bigint()}`
-  const _logger = logger.create({ id })
+  const logger = _logger.create({ id })
   return async (...args: Parameters<FN>): Promise<Awaited<ReturnType<FN>>> => {
     let retries = 0
     const retry = () => promiseTimeout(fn(...args), rpcTimeoutSeconds * 1000) // eslint-disable-line
@@ -18,7 +18,7 @@ export default function rateLimitRetry<FN extends (...args: any[]) => Promise<an
         // the await here is intentional so it's caught in the try/catch below.
         const result = await retry()
         if (retries > 0) {
-          _logger.debug(`rateLimitRetry attempt #${retries} successful`)
+          logger.debug(`rateLimitRetry attempt #${retries} successful`)
         }
         return result
       } catch (err) {
@@ -28,29 +28,33 @@ export default function rateLimitRetry<FN extends (...args: any[]) => Promise<an
         const connectionErrorRegex = /(ETIMEDOUT|ENETUNREACH|ECONNRESET|ECONNREFUSED|SERVER_ERROR|EPROTO)/i
         const badResponseErrorRegex = /(bad response|response error|missing response|processing response error|invalid json response body|FetchError)/i
         const revertErrorRegex = /revert/i
+        const oversizedDataRegex = /oversized data/i
+        const bridgeContractError = /BRG:/
 
         const isRateLimitError = rateLimitErrorRegex.test(errMsg)
         const isTimeoutError = timeoutErrorRegex.test(errMsg)
         const isConnectionError = connectionErrorRegex.test(errMsg)
         const isBadResponseError = badResponseErrorRegex.test(errMsg)
+        const isOversizedDataError = oversizedDataRegex.test(errMsg)
+        const isBridgeContractError = bridgeContractError.test(errMsg)
 
         // a connection error, such as 'ECONNREFUSED', will cause ethers to return a "missing revert data in call exception" error,
         // so we want to exclude server connection errors from actual contract call revert errors.
         const isRevertError = revertErrorRegex.test(errMsg) && !isConnectionError && !isTimeoutError
 
-        const shouldRetry = (isRateLimitError || isTimeoutError || isConnectionError || isBadResponseError) && !isRevertError
+        const shouldRetry = (isRateLimitError || isTimeoutError || isConnectionError || isBadResponseError) && !isRevertError && !isOversizedDataError && !isBridgeContractError
 
-        log.debug(`isRateLimitError: ${isRateLimitError}, isTimeoutError: ${isTimeoutError}, isConnectionError: ${isConnectionError}, isBadResponseError: ${isBadResponseError}, isRevertError: ${isRevertError}, shouldRetry: ${shouldRetry}`)
+        logger.debug(`isRateLimitError: ${isRateLimitError}, isTimeoutError: ${isTimeoutError}, isConnectionError: ${isConnectionError}, isBadResponseError: ${isBadResponseError}, isRevertError: ${isRevertError}, shouldRetry: ${shouldRetry}`)
 
         // throw error as usual if it's not a rate limit error
         if (!shouldRetry) {
-          log.error(errMsg)
+          logger.error(errMsg)
           throw err
         }
         retries++
         // if it's a rate limit error, then throw error after max retries attempted.
         if (retries >= rateLimitMaxRetries) {
-          _logger.error(`max retries reached (${rateLimitMaxRetries}). Error: ${err}`)
+          logger.error(`max retries reached (${rateLimitMaxRetries}). Error: ${err}`)
           // this must be a regular console log to print original function name
           console.error('max retries reached', fn, id, ...args)
           notifier.error(`max retries (${rateLimitMaxRetries}) reached (logId: ${id}). Error: ${errMsg}`)
@@ -58,7 +62,7 @@ export default function rateLimitRetry<FN extends (...args: any[]) => Promise<an
         }
 
         const delayMs = (1 << retries) * 1000
-        log.warn(
+        logger.warn(
           `retry attempt #${retries} failed with error "${
             errMsg
           }". retrying again in ${delayMs / 1000} seconds.`

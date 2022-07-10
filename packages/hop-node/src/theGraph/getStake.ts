c55054cb4e4d7374074cd395202d19da69ebbf33
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2022-07-10 12:34:37 -0700

    hop-node: add total stake command

---
+++ packages/hop-node/src/theGraph/getStake.ts
@@ -0,0 +1,48 @@
+import makeRequest from './makeRequest'
+import { constants } from 'ethers'
+import { normalizeEntity } from './shared'
+
+export default async function getStake (
+  chain: string,
+  token: string,
+  bonder: string,
+  lastId: string = constants.AddressZero
+) {
+  bonder = bonder.toLowerCase()
+  const query = `
+    query Stake($token: String, $bonder: String, $lastId: ID) {
+      stakes(
+        where: {
+          id_gt: $lastId
+          account: $bonder
+          token: $token
+        },
+        orderBy: id,
+        orderDirection: asc,
+        first: 1000
+      ) {
+        id
+        amount
+      }
+    }
+  `
+  const jsonRes = await makeRequest(chain, query, {
+    token,
+    bonder,
+    lastId: lastId
+  })
+  let stakes = jsonRes.stakes.map((x: any) => normalizeEntity(x))
+
+  const maxItemsLength = 1000
+  if (stakes.length === maxItemsLength) {
+    lastId = stakes[stakes.length - 1].id
+    stakes = stakes.concat(await getStake(
+      chain,
+      token,
+      bonder,
+      lastId
+    ))
+  }
+
+  return stakes
+}

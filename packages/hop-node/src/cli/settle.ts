d8434e099ca1141fa3cb38ee936208661ac9e345
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2022-07-11 17:44:26 -0700

    hop-node: add ability to settle based on TheGraph data

--- packages/hop-node/src/cli/settle.ts
+++ packages/hop-node/src/cli/settle.ts
@@ -1,7 +1,10 @@
 import { actionHandler, parseBool, parseString, root } from './shared'
+import chainIdToSlug from 'src/utils/chainIdToSlug'
 import {
   getSettleBondedWithdrawalsWatcher
 } from 'src/watchers/watchers'
+import getTransferIdsForTransferRoot from 'src/theGraph/getTransferIdsForTransferRoot'
+import getTransferRoot from 'src/theGraph/getTransferRoot'
 
 root
   .command('settle')
@@ -11,6 +14,7 @@ root
   .option('--transfer-root-hash <id>', 'Transfer root hash', parseString)
   .option('--transfer-id <id>', 'Transfer ID', parseString)
   .option('--bonder <address>', 'Bonder address', parseString)
+  .option('--use-db [boolean]', 'Use the DB to construct the roots', parseBool)
   .option(
     '--dry [boolean]',
     'Start in dry mode. If enabled, no transactions will be sent.',
@@ -19,7 +23,7 @@ root
   .action(actionHandler(main))
 
 async function main (source: any) {
-  const { sourceChain: chain, token, transferRootHash, transferId, bonder, dry: dryMode } = source
+  let { sourceChain: chain, token, transferRootHash, transferId, bonder, useDb, dry: dryMode } = source
   if (!chain) {
     throw new Error('source chain is required')
   }
@@ -30,14 +34,35 @@ async function main (source: any) {
     throw new Error('transferRootHash or transferId is required')
   }
 
+  if (typeof useDb === 'undefined') {
+    useDb = true
+  }
+
   const watcher = await getSettleBondedWithdrawalsWatcher({ chain, token, dryMode })
   if (!watcher) {
     throw new Error('watcher not found')
   }
 
-  if (transferRootHash) {
-    await watcher.checkTransferRootHash(transferRootHash, bonder)
+  if (useDb) {
+    if (transferRootHash) {
+      await watcher.checkTransferRootHash(transferRootHash, bonder)
+    } else {
+      await watcher.checkTransferId(transferId)
+    }
   } else {
-    await watcher.checkTransferId(transferId)
+    const transferRoot: any = await getTransferRoot(chain, token, transferRootHash)
+    const destinationChainId: number = transferRoot.destinationChainId
+    const destinationChain = chainIdToSlug(destinationChainId)
+    const transferIds: string[] = []
+    for (const transfer of transferRoot.transferIds) {
+      transferIds.push(transfer.transferId)
+    }
+
+    const destinationChainWatcher = await getSettleBondedWithdrawalsWatcher({ chain: destinationChain, token, dryMode })
+    await destinationChainWatcher.bridge.settleBondedWithdrawals(
+      bonder,
+      transferIds,
+      transferRoot.totalAmount
+    )
   }
 }

2854aa7b7f02e6b39e988dc34f47ce69e8659229
Miguel Mota <hello@miguelmota.com>
2022-08-22 13:45:03 -0700

    frontend: instantiate erc20 mintable contract for goerli faucet

--- packages/frontend/src/pages/Faucet/FaucetContext.tsx
+++ packages/frontend/src/pages/Faucet/FaucetContext.tsx
@@ -1,5 +1,7 @@
 import React, { FC, createContext, useContext, useState } from 'react'
 import { parseUnits } from 'ethers/lib/utils'
+import { Contract } from 'ethers'
+import erc20Abi from '@hop-protocol/core/abi/static/ERC20Mintable.json'
 import { useApp } from 'src/contexts/AppContext'
 import { useWeb3Context } from 'src/contexts/Web3Context'
 import Transaction from 'src/models/Transaction'
@@ -36,7 +38,7 @@ const FaucetContextProvider: FC = ({ children }) => {
   const [isMinting, setMinting] = useState<boolean>(false)
   const { txHistory, tokens } = useApp()
   const selectedNetwork = l1Network
-  const { checkConnectedNetworkId, address } = useWeb3Context()
+  const { checkConnectedNetworkId, address, provider } = useWeb3Context()
   const [selectedToken, setSelectedToken] = useState<Token | undefined>(tokens[0])
   const [error, setError] = useState<string | null | undefined>(null)
 
@@ -45,10 +47,26 @@ const FaucetContextProvider: FC = ({ children }) => {
       if (!selectedToken?.symbol) {
         return
       }
-      const contract = addresses.tokens[selectedToken.symbol][L1_NETWORK]?.l1CanonicalToken
+
+      const address = addresses.tokens[selectedToken.symbol][L1_NETWORK]?.l1CanonicalToken
+      if (!address) {
+        return
+      }
+
+      if (!provider) {
+        return
+      }
+
+      const signer = provider?.getSigner()
+      if (!signer) {
+        return
+      }
+
       setMinting(true)
+
       const recipient = address?.toString()
       const parsedAmount = parseUnits(mintAmount, selectedToken.decimals)
+      const contract = new Contract(address, erc20Abi, signer)
 
       const tx = await contract?.mint(recipient, parsedAmount)
       logger.debug('mint:', tx?.hash)

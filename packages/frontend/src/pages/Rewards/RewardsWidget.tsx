c80522108ef86194e3ee2cd0c82a2736c51c5472
Miguel Mota <hello@miguelmota.com>
2022-09-13 19:01:41 -0700

    frontend: RewardsPage: Update styling

--- packages/frontend/src/pages/Rewards/RewardsWidget.tsx
+++ packages/frontend/src/pages/Rewards/RewardsWidget.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Alert from 'src/components/alert/Alert'
 import Box from '@material-ui/core/Box'
+import Card from '@material-ui/core/Card'
 import Button from 'src/components/buttons/Button'
 import Typography from '@material-ui/core/Typography'
 import { toTokenDisplay } from 'src/utils'
 import InfoTooltip from 'src/components/InfoTooltip'
 import { useRewards } from './useRewards'
+import { Circle } from 'src/components/ui'
+import { makeStyles } from '@material-ui/core/styles'
 
 interface Props {
   rewardsContractAddress: string
@@ -14,19 +17,31 @@ interface Props {
   title: string
 }
 
-export function RewardsWidget(props: Props) {
-  const { rewardsContractAddress, merkleBaseUrl, requiredChainId, title } = props
-  const { tokenDecimals, tokenSymbol, claimableAmount, unclaimableAmount, latestRootTotal, latestRoot, claimRecipient, error, onchainRoot, loading, claim, claiming, estimatedDate } = useRewards({ rewardsContractAddress, merkleBaseUrl, requiredChainId })
+export const useStyles = makeStyles(theme => ({
+  box: {
+    boxShadow: theme.boxShadow.inner,
+    transition: 'all 0.15s ease-out',
+    minHeight: '200px',
+  },
+  root: {
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+      alignItems: 'center'
+    },
+  }
+}))
 
-  const claimableAmountDisplay = tokenDecimals ? toTokenDisplay(claimableAmount, tokenDecimals) : ''
-  const unclaimableAmountDisplay = tokenDecimals ? toTokenDisplay(unclaimableAmount, tokenDecimals) : ''
+export function RewardsWidget(props: Props) {
+  const styles = useStyles()
+  const { rewardsContractAddress, merkleBaseUrl, requiredChainId, title } = props
+  const { tokenDecimals, tokenSymbol, claimableAmount, unclaimableAmount, latestRootTotal, latestRoot, claimRecipient, error, onchainRoot, loading, claim, claiming, estimatedDate, claimChain, txHistoryLink } = useRewards({ rewardsContractAddress, merkleBaseUrl, requiredChainId })
+
+  const claimableAmountDisplay = tokenDecimals ? Number(toTokenDisplay(claimableAmount, tokenDecimals)).toFixed(2) : ''
+  const unclaimableAmountDisplay = tokenDecimals ? Number(toTokenDisplay(unclaimableAmount, tokenDecimals)).toFixed(2) : ''
   const latestRootTotalDisplay = tokenDecimals ? toTokenDisplay(latestRootTotal, tokenDecimals) : ''
 
   return (
-    <Box maxWidth="500px" margin="0 auto" flexDirection="column" display="flex" justifyContent="center" textAlign="center">
-      <Box mb={6}>
-        <Typography variant="h4">Rewards</Typography>
-      </Box>
+    <Box maxWidth="640px" margin="0 auto" flexDirection="column" display="flex" justifyContent="center" textAlign="center">
       {!claimRecipient && (
         <Box mb={4} display="flex" flexDirection="column" justifyContent="center" textAlign="center">
           <Typography variant="body1">
@@ -36,8 +51,8 @@ export function RewardsWidget(props: Props) {
       )}
       {!!claimRecipient && (
         <Box>
-          <Box mb={2}>
-            <Typography variant="h6">{title} <InfoTooltip title={<><div>Merkle rewards</div><div>published root: {onchainRoot}</div><div>Latest root: {latestRoot}</div><div>latest root total: {latestRootTotalDisplay}</div></>} /></Typography>
+          <Box mb={4} flexDirection="column" textAlign="left">
+            <Typography variant="h5">{title} <InfoTooltip title={<><div>Merkle rewards</div><div>published root: {onchainRoot}</div><div>Latest root: {latestRoot}</div><div>latest root total: {latestRootTotalDisplay}</div></>} /></Typography>
           </Box>
           {loading && (
             <Box mb={4} display="flex" flexDirection="column" justifyContent="center" textAlign="center">
@@ -46,21 +61,83 @@ export function RewardsWidget(props: Props) {
               </Typography>
             </Box>
           )}
-          <Box mb={4} display="flex" flexDirection="column" justifyContent="center" textAlign="center">
-            <Typography variant="body1">
-              Claimable: <strong>{claimableAmountDisplay} {tokenSymbol}</strong> <InfoTooltip title={'Tokens that can be claimed now'} />
-            </Typography>
-            <Typography variant="body1">
-              Locked: {unclaimableAmountDisplay} {tokenSymbol} <InfoTooltip title={'Tokens that will be claimable once merkle root is published on-chain'} />
-            </Typography>
-            {!!unclaimableAmountDisplay && (
-              <Typography variant="body1">
-                <small>estimated to unlock {estimatedDate}</small>
-              </Typography>
-            )}
-          </Box>
-          <Box mb={2}>
-            <Button variant="contained" onClick={claim} loading={claiming} disabled={claiming || claimableAmount.eq(0)} highlighted={claimableAmount.gt(0)}>Claim Rewards</Button>
+          <Box display="flex" justifyContent="space-between" className={styles.root}>
+            <Box mb={4} display="flex" flexDirection="column" textAlign="left" width="300px">
+              <Card className={styles.box}>
+                <Box mb={4}>
+                  <Box mb={2}>
+                    <Typography variant="h6">
+                      Claimable {tokenSymbol} <InfoTooltip title={'Tokens that can be claimed now'} />
+                    </Typography>
+                  </Box>
+                  <Box mb={2}>
+                    <Typography variant="subtitle1">
+                      {claimChain && (
+                      <Circle style={{
+                        width: '32px',
+                        height: '32px',
+                        display: 'inline-block',
+                        verticalAlign: 'middle',
+                        marginRight: '1rem'
+                      }}>
+                        <img src={claimChain.imageUrl} alt={claimChain.name} title={claimChain.name} width="100%" />
+                      </Circle>
+                      )}
+                      {claimableAmountDisplay} {tokenSymbol}
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box mb={2}>
+                  <Button variant="contained" onClick={claim} loading={claiming} disabled={claiming || claimableAmount.eq(0)} highlighted={claimableAmount.gt(0)} fullWidth large>Claim</Button>
+                </Box>
+              </Card>
+            </Box>
+            <Box mb={4} display="flex" flexDirection="column" textAlign="left" width="300px">
+              <Card className={styles.box}>
+                <Box mb={4}>
+                  <Box mb={2}>
+                    <Typography variant="h6">
+                      Pending {tokenSymbol} <InfoTooltip title={'Tokens that will be claimable once merkle root is published on-chain'} />
+                    </Typography>
+                  </Box>
+                  <Box mb={2} display="flex" justifyContent="space-between">
+                    <Box>
+                      <Typography variant="subtitle1">
+                        {claimChain && (
+                          <Circle style={{
+                            width: '32px',
+                            height: '32px',
+                            display: 'inline-block',
+                            verticalAlign: 'middle',
+                            marginRight: '1rem'
+                          }}>
+                            <img src={claimChain.imageUrl} alt={claimChain.name} title={claimChain.name} width="100%" />
+                          </Circle>
+                        )}
+                        {unclaimableAmountDisplay} {tokenSymbol}
+                      </Typography>
+                    </Box>
+                    {!!unclaimableAmountDisplay && (
+                      <Box>
+                        <Box>
+                          <Typography variant="body1">
+                            <strong>Countdown</strong>
+                          </Typography>
+                        </Box>
+                        <Box>
+                          <Typography variant="body1">
+                            {estimatedDate}
+                          </Typography>
+                        </Box>
+                      </Box>
+                    )}
+                  </Box>
+                </Box>
+                <Box mb={2}>
+                  <Button variant="contained" href={txHistoryLink} fullWidth large target="_blank" rel="noopener noreferrer">Tx History →</Button>
+                </Box>
+              </Card>
+            </Box>
           </Box>
         </Box>
       )}

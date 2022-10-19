9f9b2887ab5fd0cee41fda362066ccd1c97bdf03
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2022-10-19 15:59:22 -0700

    stats-worker: add rewards contracts

--- packages/stats-worker/src/AprStats.ts
+++ packages/stats-worker/src/AprStats.ts
@@ -21,73 +21,74 @@ const oneYearDays = 365
 const stakingRewardsContracts: any = {
   arbitrum: {
     ETH: [
-      'TODO',
+      '0x755569159598f3702bdD7DFF6233A317C156d3Dd',
     ],
     USDC: [
-      'TODO',
+      '0xb0CabFE930642AD3E7DECdc741884d8C3F7EbC70',
     ],
     DAI: [
-      'TODO',
+      '0xd4D28588ac1D9EF272aa29d4424e3E2A03789D1E',
     ],
     USDT: [
-      'TODO',
-    ]
+      '0x9Dd8685463285aD5a94D2c128bda3c5e8a6173c8',
+    ],
   },
   optimism: {
     ETH: [
-      'TODO',
+      '0x95d6A95BECfd98a7032Ed0c7d950ff6e0Fa8d697',
     ],
     USDC: [
-      'TODO',
+      '0xf587B9309c603feEdf0445aF4D3B21300989e93a',
     ],
     DAI: [
-      'TODO',
+      '0x392B9780cFD362bD6951edFA9eBc31e68748b190',
     ],
     USDT: [
-      'TODO',
+      '0xAeB1b49921E0D2D96FcDBe0D486190B2907B3e0B',
     ],
     SNX: [
-      'TODO',
-    ]
+      '0x25a5A48C35e75BD2EFf53D94f0BB60d5A00E36ea',
+      '0x09992Dd7B32f7b35D347DE9Bdaf1919a57d38E82',
+    ],
   },
   polygon: {
     ETH: [
       '0x7bCeDA1Db99D64F25eFA279BB11CE48E15Fda427',
-      'TODO',
+      '0xAA7b3a4A084e6461D486E53a03CF45004F0963b7',
     ],
     USDC: [
       '0x2C2Ab81Cf235e86374468b387e241DF22459A265',
-      'TODO',
+      '0x7811737716942967Ae6567B26a5051cC72af550E',
     ],
     DAI: [
       '0x4Aeb0B5B1F3e74314A7Fa934dB090af603E8289b',
-      'TODO',
+      '0xd6dC6F69f81537Fe9DEcc18152b7005B45Dc2eE7',
     ],
     USDT: [
       '0x07932e9A5AB8800922B2688FB1FA0DAAd8341772',
-      'TODO',
+      '0x297E5079DF8173Ae1696899d3eACD708f0aF82Ce',
     ],
     MATIC: [
       '0x7dEEbCaD1416110022F444B03aEb1D20eB4Ea53f',
-    ]
+    ],
   },
   gnosis: {
     ETH: [
       '0xC61bA16e864eFbd06a9fe30Aab39D18B8F63710a',
-      'TODO',
+      '0x712F0cf37Bdb8299D0666727F73a5cAbA7c1c24c',
     ],
     USDC: [
       '0x5D13179c5fa40b87D53Ff67ca26245D3D5B2F872',
-      'TODO',
+      '0x636A7ee78faCd079DaBC8f81EDA1D09AA9D440A7',
     ],
     DAI: [
       '0x12a3a66720dD925fa93f7C895bC20Ca9560AdFe7',
-      'TODO',
+      '0xBF7a02d963b23D84313F07a04ad663409CEE5A92',
     ],
     USDT: [
       '0x2C2Ab81Cf235e86374468b387e241DF22459A265',
-      'TODO',
-    ]
+      '0x3d4Cc8A61c7528Fd86C55cfe061a78dCBA48EDd1',
+    ],
   }
 }
 

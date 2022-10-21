989cd776c2108c3389403528ac02cb9827583ee4
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2022-10-21 14:16:49 -0700

    stats-worker: lint

--- packages/stats-worker/src/YieldStats.ts
+++ packages/stats-worker/src/YieldStats.ts
@@ -19,75 +19,57 @@ const oneYearDays = 365
 
 const stakingRewardsContracts: any = {
   arbitrum: {
-    ETH: [
-      '0x755569159598f3702bdD7DFF6233A317C156d3Dd',
-    ],
-    USDC: [
-      '0xb0CabFE930642AD3E7DECdc741884d8C3F7EbC70',
-    ],
-    DAI: [
-      '0xd4D28588ac1D9EF272aa29d4424e3E2A03789D1E',
-    ],
-    USDT: [
-      '0x9Dd8685463285aD5a94D2c128bda3c5e8a6173c8',
-    ],
+    ETH: ['0x755569159598f3702bdD7DFF6233A317C156d3Dd'],
+    USDC: ['0xb0CabFE930642AD3E7DECdc741884d8C3F7EbC70'],
+    DAI: ['0xd4D28588ac1D9EF272aa29d4424e3E2A03789D1E'],
+    USDT: ['0x9Dd8685463285aD5a94D2c128bda3c5e8a6173c8']
   },
   optimism: {
-    ETH: [
-      '0x95d6A95BECfd98a7032Ed0c7d950ff6e0Fa8d697',
-    ],
-    USDC: [
-      '0xf587B9309c603feEdf0445aF4D3B21300989e93a',
-    ],
-    DAI: [
-      '0x392B9780cFD362bD6951edFA9eBc31e68748b190',
-    ],
-    USDT: [
-      '0xAeB1b49921E0D2D96FcDBe0D486190B2907B3e0B',
-    ],
+    ETH: ['0x95d6A95BECfd98a7032Ed0c7d950ff6e0Fa8d697'],
+    USDC: ['0xf587B9309c603feEdf0445aF4D3B21300989e93a'],
+    DAI: ['0x392B9780cFD362bD6951edFA9eBc31e68748b190'],
+    USDT: ['0xAeB1b49921E0D2D96FcDBe0D486190B2907B3e0B'],
     SNX: [
       '0x25a5A48C35e75BD2EFf53D94f0BB60d5A00E36ea',
-      '0x09992Dd7B32f7b35D347DE9Bdaf1919a57d38E82',
-    ],
+      '0x09992Dd7B32f7b35D347DE9Bdaf1919a57d38E82'
+    ]
   },
   polygon: {
     ETH: [
       '0x7bCeDA1Db99D64F25eFA279BB11CE48E15Fda427',
-      '0xAA7b3a4A084e6461D486E53a03CF45004F0963b7',
+      '0xAA7b3a4A084e6461D486E53a03CF45004F0963b7'
     ],
     USDC: [
       '0x2C2Ab81Cf235e86374468b387e241DF22459A265',
-      '0x7811737716942967Ae6567B26a5051cC72af550E',
+      '0x7811737716942967Ae6567B26a5051cC72af550E'
     ],
     DAI: [
       '0x4Aeb0B5B1F3e74314A7Fa934dB090af603E8289b',
-      '0xd6dC6F69f81537Fe9DEcc18152b7005B45Dc2eE7',
+      '0xd6dC6F69f81537Fe9DEcc18152b7005B45Dc2eE7'
     ],
     USDT: [
       '0x07932e9A5AB8800922B2688FB1FA0DAAd8341772',
-      '0x297E5079DF8173Ae1696899d3eACD708f0aF82Ce',
+      '0x297E5079DF8173Ae1696899d3eACD708f0aF82Ce'
     ],
-    MATIC: [
-      '0x7dEEbCaD1416110022F444B03aEb1D20eB4Ea53f',
-    ],
+    MATIC: ['0x7dEEbCaD1416110022F444B03aEb1D20eB4Ea53f']
   },
   gnosis: {
     ETH: [
       '0xC61bA16e864eFbd06a9fe30Aab39D18B8F63710a',
-      '0x712F0cf37Bdb8299D0666727F73a5cAbA7c1c24c',
+      '0x712F0cf37Bdb8299D0666727F73a5cAbA7c1c24c'
     ],
     USDC: [
       '0x5D13179c5fa40b87D53Ff67ca26245D3D5B2F872',
-      '0x636A7ee78faCd079DaBC8f81EDA1D09AA9D440A7',
+      '0x636A7ee78faCd079DaBC8f81EDA1D09AA9D440A7'
     ],
     DAI: [
       '0x12a3a66720dD925fa93f7C895bC20Ca9560AdFe7',
-      '0xBF7a02d963b23D84313F07a04ad663409CEE5A92',
+      '0xBF7a02d963b23D84313F07a04ad663409CEE5A92'
     ],
     USDT: [
       '0x2C2Ab81Cf235e86374468b387e241DF22459A265',
-      '0x3d4Cc8A61c7528Fd86C55cfe061a78dCBA48EDd1',
-    ],
+      '0x3d4Cc8A61c7528Fd86C55cfe061a78dCBA48EDd1'
+    ]
   }
 }
 
@@ -284,9 +266,11 @@ class YieldStats {
 
         const stakingContracts = stakingRewardsContracts[chain][token]
         if (stakingContracts?.length > 0) {
-          if(!yieldData.stakingRewards) yieldData.stakingRewards = {}
-          if(!yieldData.stakingRewards[token]) yieldData.stakingRewards[token] = {}
-          if(!yieldData.stakingRewards[token][chain]) yieldData.stakingRewards[token][chain] = {}
+          if (!yieldData.stakingRewards) yieldData.stakingRewards = {}
+          if (!yieldData.stakingRewards[token])
+            yieldData.stakingRewards[token] = {}
+          if (!yieldData.stakingRewards[token][chain])
+            yieldData.stakingRewards[token][chain] = {}
           for (const stakingContract of stakingContracts) {
             yieldData.stakingRewards[token][chain][stakingContract] = {
               apr: 0,
@@ -297,9 +281,9 @@ class YieldStats {
           }
         }
 
-        if(!yieldData.optimalYield) yieldData.optimalYield = {}
-        if(!yieldData.optimalYield[token]) yieldData.optimalYield[token] = {}
-        if(!yieldData.optimalYield[token][chain]) {
+        if (!yieldData.optimalYield) yieldData.optimalYield = {}
+        if (!yieldData.optimalYield[token]) yieldData.optimalYield[token] = {}
+        if (!yieldData.optimalYield[token][chain]) {
           yieldData.optimalYield[token][chain] = {
             apr: 0,
             apy: 0,
@@ -323,7 +307,11 @@ class YieldStats {
       let areAllChainsMissing = true
       for (const chain in tokenData) {
         const chainData = tokenData[chain]
-        const isInactive = !chainData || chainData?.apr === 0 || chainData?.apr === null || isNaN(chainData?.apr)
+        const isInactive =
+          !chainData ||
+          chainData?.apr === 0 ||
+          chainData?.apr === null ||
+          isNaN(chainData?.apr)
         if (!isInactive) {
           areAllChainsMissing = false
           break
@@ -357,7 +345,7 @@ class YieldStats {
         }
         // If there is no optimal staking contract, just use the pool yield
         const isNoOptimalStakingContract = yieldData.optimalYield[token][chain].apy === 0
-        if  (isNoOptimalStakingContract) {
+        if (isNoOptimalStakingContract) {
           yieldData.optimalYield[token][chain] = {
             apr: yieldData.pools[token][chain].apr,
             apy: yieldData.pools[token][chain].apy,
@@ -443,10 +431,7 @@ class YieldStats {
       const tokenUsdPrice = await bridge.priceFeed.getPriceByTokenSymbol(token)
 
       const rewardsTokenAddress = await stakingRewards.rewardsToken()
-      const rewardsToken = ERC20__factory.connect(
-        rewardsTokenAddress,
-        provider
-      )
+      const rewardsToken = ERC20__factory.connect(rewardsTokenAddress, provider)
       const rewardsTokenSymbol = await rewardsToken.symbol()
       const rewardTokenUsdPrice = await bridge?.priceFeed.getPriceByTokenSymbol(
         rewardsTokenSymbol
@@ -477,7 +462,6 @@ class YieldStats {
         throw new Error('Cannot have negative APR and positive APY or vice versa')
       }
 
-      // If the rewards have expired, do not log a token symbol
       let rewardToken = ''
       let stakingRewardsContractAddress = ''
       const isActiveRewards = apr > 0 && apy > 0
@@ -488,7 +472,6 @@ class YieldStats {
         continue
       }
 
-
       stakingYieldData.push({
         apr,
         apy,
@@ -496,10 +479,8 @@ class YieldStats {
         stakingRewardsContractAddress,
         isOptimalStakingContract: apr > currentOptimalApr
       })
-
       currentOptimalApr = apr > currentOptimalApr ? apr : currentOptimalApr
     }
-    
     return stakingYieldData
   }
 
@@ -586,16 +567,16 @@ class YieldStats {
 
   shouldSkipYields (bridges: any, chain: string, token: string) {
     if (chain === 'ethereum') {
-      return true 
+      return true
     }
     if (!bridges[token][chain]) {
-      return true 
+      return true
     }
     if (bridges[token][chain].l2CanonicalToken === constants.AddressZero) {
-      return true 
+      return true
     }
     if (token === 'HOP') {
-      return true 
+      return true
     }
     if (bridges[token][chain].l2SaddleSwap === constants.AddressZero) {
       return true

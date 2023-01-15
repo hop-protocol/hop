179a91046c056ab6410ab1d9ff043fe4b312b62e
Miguel Mota <hello@miguelmota.com>
2023-01-15 00:36:57 -0800

    frontend: RewardsPage: Add optional merkleBaseUrl query param to fetch merkle data

--- packages/frontend/src/pages/Rewards/config.ts
+++ packages/frontend/src/pages/Rewards/config.ts
@@ -1,4 +1,5 @@
 import { reactAppNetwork } from 'src/config'
+import { getQueryParam } from 'src/utils/getQueryParam'
 
 let configs :any[] = []
 
@@ -6,13 +7,13 @@ if (reactAppNetwork === 'goerli') {
   configs = [{
     chainId: 420,
     rewardsContractAddress: '0x5D13179c5fa40b87D53Ff67ca26245D3D5B2F872',
-    merkleBaseUrl: 'https://raw.githubusercontent.com/hop-protocol/goerli-test-merkle-rewards-data/master'
+    merkleBaseUrl: getQueryParam('merkleBaseUrl') || 'https://raw.githubusercontent.com/hop-protocol/goerli-test-merkle-rewards-data/master'
   }]
 } else {
   configs = [{
     chainId: 10,
     rewardsContractAddress: '0x45269F59aA76bB491D0Fc4c26F468D8E1EE26b73',
-    merkleBaseUrl: 'https://raw.githubusercontent.com/hop-protocol/optimism-refund-merkle-rewards/master'
+    merkleBaseUrl: getQueryParam('merkleBaseUrl') || 'https://raw.githubusercontent.com/hop-protocol/optimism-refund-merkle-rewards/master'
   }]
 }
 

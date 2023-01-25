291821fc09b533a1707533691122272c0ac0045d
Miguel Mota <hello@miguelmota.com>
2023-01-25 12:20:24 -0800

    frontend: Update rpc docs url

--- packages/frontend/src/utils/format.ts
+++ packages/frontend/src/utils/format.ts
@@ -20,6 +20,8 @@ export function formatError(error: any, network?: Network) {
     return formatError(error[0], network)
   }
 
+  const rpcEndpointsDocs = 'https://docs.hop.exchange/v/developer-docs/rpc/rpc-endpoints'
+
   // TODO: handle custom error messages elsewhere (and better)
   if (
     errMsg.includes('not enough funds for gas') ||
@@ -29,32 +31,32 @@ export function formatError(error: any, network?: Network) {
     const feeToken = network?.nativeTokenSymbol || 'funds'
     errMsg = `Insufficient balance. Please add ${feeToken} to pay for tx fees. Error: ${errMsg}`
   } else if (errMsg.includes('NetworkError when attempting to fetch resource')) {
-    errMsg = `${errMsg} Please check your wallet network settings are correct and try again. More info: https://docs.hop.exchange/rpc-endpoints`
+    errMsg = `${errMsg} Please check your wallet network settings are correct and try again. More info: ${rpcEndpointsDocs}`
   } else if (
     errMsg.includes('[ethjs-query]') ||
     errMsg.includes('while formatting outputs from RPC')
   ) {
-    errMsg = `An RPC error occured. Please check your wallet network settings are correct and refresh page to try again. More info: https://docs.hop.exchange/rpc-endpoints. Error: ${errMsg}`
+    errMsg = `An RPC error occured. Please check your wallet network settings are correct and refresh page to try again. More info: ${rpcEndpointsDocs}. Error: ${errMsg}`
   } else if (
     errMsg.includes('Failed to fetch') ||
     errMsg.includes('could not detect network') ||
     errMsg.includes('Not Found') ||
     errMsg.includes('Non-200 status code')
   ) {
-    errMsg = `There was a network error. Please disable any ad blockers and check your wallet network settings are correct and refresh page to try again. More info: https://docs.hop.exchange/rpc-endpoints. Error: ${errMsg}`
+    errMsg = `There was a network error. Please disable any ad blockers and check your wallet network settings are correct and refresh page to try again. More info: ${rpcEndpointsDocs}. Error: ${errMsg}`
   } else if (errMsg.includes('Internal JSON-RPC error') || errMsg.includes('Internal error')) {
     const feeToken = network?.nativeTokenSymbol || 'funds'
-    errMsg = `An RPC error occured. Please check you have enough ${feeToken} to pay for fees and check your wallet network settings are correct. Refresh to try again. More info: https://docs.hop.exchange/rpc-endpoints. Error: ${errMsg}`
+    errMsg = `An RPC error occured. Please check you have enough ${feeToken} to pay for fees and check your wallet network settings are correct. Refresh to try again. More info: ${rpcEndpointsDocs}. Error: ${errMsg}`
   } else if (errMsg.includes('call revert exception') || errMsg.includes('missing revert data')) {
-    errMsg = `An RPC error occured. Please check your wallet network settings are correct and refresh page to try again. More info: https://docs.hop.exchange/rpc-endpoints. Error: ${errMsg}`
+    errMsg = `An RPC error occured. Please check your wallet network settings are correct and refresh page to try again. More info: ${rpcEndpointsDocs}. Error: ${errMsg}`
   } else if (errMsg.includes('unsupported block number') || errMsg.includes('rlp: expected List') || errMsg.includes('PermissionDenied, permission denied for tx type: Call')) {
     errMsg = `An RPC error occured. Please refresh page to try again. Error: ${errMsg}`
   } else if (errMsg.includes('transaction underpriced')) {
     errMsg = `An RPC error occured. The transaction is underpriced. Please try again and increase gas price. If you are seeing is error a lot, try resetting the nonce for your wallet account. Error: ${errMsg}`
   } else if (errMsg.includes('header not found')) {
-    errMsg = `An RPC error occurred. Please check your wallet network settings are correct and try again. Consider using a different RPC provider if you are seeing this error frequently. More info: https://docs.hop.exchange/rpc-endpoints. Error: ${errMsg}`
+    errMsg = `An RPC error occurred. Please check your wallet network settings are correct and try again. Consider using a different RPC provider if you are seeing this error frequently. More info: ${rpcEndpointsDocs}. Error: ${errMsg}`
   } else if (errMsg.includes('sequencer transaction forwarding not configured') || errMsg.includes('rate limit') || errMsg.includes('compute units')) {
-    errMsg = `An RPC error occurred. Please try again. Consider using a different RPC provider if you are seeing this error often. More info: https://docs.hop.exchange/rpc-endpoints. Error: ${errMsg}`
+    errMsg = `An RPC error occurred. Please try again. Consider using a different RPC provider if you are seeing this error often. More info: ${rpcEndpointsDocs}. Error: ${errMsg}`
   }
 
   return prettifyErrorMessage(errMsg)

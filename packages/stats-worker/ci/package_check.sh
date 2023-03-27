5821ead63ceb39878023a786c58e7e24a66ebe87
Miguel Mota <hello@miguelmota.com>
2023-03-27 11:02:06 -0700

    circleci: Install npm

--- packages/stats-worker/ci/package_check.sh
+++ packages/stats-worker/ci/package_check.sh
@@ -1,9 +1,9 @@
 #/bin/env bash
 
-npx npm info @hop-protocol/core --json | jq '."dist-tags".latest' > /tmp/core1.txt
+npm info @hop-protocol/core --json | jq '."dist-tags".latest' > /tmp/core1.txt
 cat package.json | jq '.dependencies."@hop-protocol/core"' > /tmp/core2.txt
 diff /tmp/core1.txt /tmp/core2.txt || sleep 180
 
-npx npm info @hop-protocol/sdk --json | jq '."dist-tags".latest' > /tmp/sdk1.txt
+npm info @hop-protocol/sdk --json | jq '."dist-tags".latest' > /tmp/sdk1.txt
 cat package.json | jq '.dependencies."@hop-protocol/sdk"' > /tmp/sdk2.txt
 diff /tmp/sdk1.txt /tmp/sdk2.txt || sleep 180

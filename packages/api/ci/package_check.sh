2e6705016239e25275ae4a44687fcf875bb8d9d7
Miguel Mota <hello@miguelmota.com>
2023-03-27 10:42:20 -0700

    circleci: Move package check to script

---
+++ packages/api/ci/package_check.sh
@@ -0,0 +1,2 @@
+diff <(npx npm info @hop-protocol/core --json | jq '."dist-tags".latest') <(cat package.json | jq '.dependencies."@hop-protocol/core"') || sleep 180
+diff <(npx npm info @hop-protocol/sdk --json | jq '."dist-tags".latest') <(cat package.json | jq '.dependencies."@hop-protocol/sdk"') || sleep 180

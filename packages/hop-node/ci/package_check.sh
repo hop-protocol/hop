1bfb3882fb8f9b6abe39ae271de0c134dc6822c5
Miguel Mota <hello@miguelmota.com>
2023-03-27 10:56:09 -0700

    circleci: Update package check

--- packages/hop-node/ci/package_check.sh
+++ packages/hop-node/ci/package_check.sh
@@ -1,4 +1,9 @@
 #/bin/env bash
 
-diff <(npx npm info @hop-protocol/core --json | jq '."dist-tags".latest') <(cat package.json | jq '.dependencies."@hop-protocol/core"') || sleep 180
-diff <(npx npm info @hop-protocol/sdk --json | jq '."dist-tags".latest') <(cat package.json | jq '.dependencies."@hop-protocol/sdk"') || sleep 180
+npx npm info @hop-protocol/core --json | jq '."dist-tags".latest' > /tmp/core1.txt
+cat ../package.json | jq '.dependencies."@hop-protocol/core"' > /tmp/core2.txt
+diff /tmp/core1.txt /tmp/core2.txt || sleep 180
+
+npx npm info @hop-protocol/sdk --json | jq '."dist-tags".latest' > /tmp/sdk1.txt
+cat ../package.json | jq '.dependencies."@hop-protocol/sdk"' > /tmp/sdk2.txt
+diff /tmp/sdk1.txt /tmp/sdk2.txt || sleep 180

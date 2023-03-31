dbfcf3a17ef35a7d6580db1d03ea71a27d284226
Miguel Mota <hello@miguelmota.com>
2023-03-31 12:54:36 -0700

    core: network: Update linea urls

--- packages/core/src/networks/goerli.ts
+++ packages/core/src/networks/goerli.ts
@@ -55,10 +55,10 @@ export const networks: Networks = {
     name: chains.linea.name,
     image: chains.linea.image,
     networkId: 59140,
-    publicRpcUrl: 'https://consensys-zkevm-goerli-prealpha.infura.io/v3/faf4bc4ea7344e5da5e56c55de087480',
+    publicRpcUrl: 'https://rpc.goerli.linea.build',
     fallbackPublicRpcUrls: [],
-    explorerUrls: ['https://explorer.goerli.zkevm.consensys.net'],
-    nativeBridgeUrl: 'https://bridge.goerli.zkevm.consensys.net',
+    explorerUrls: ['https://explorer.goerli.linea.build'],
+    nativeBridgeUrl: 'https://bridge.goerli.linea.build',
     waitConfirmations: 1
   },
   scrollzk: {

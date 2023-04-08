3579bec28b0f959b4f4f6b82ee8e301d548ee506
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2023-04-07 18:03:47 -0700

    hop-node: linting and various updates

--- packages/hop-node/src/cli/bondedUnconfirmedRoots.ts
+++ packages/hop-node/src/cli/bondedUnconfirmedRoots.ts
@@ -7,6 +7,13 @@ import { Chain, PreRegenesisRootsCommitted } from 'src/constants'
 import { DateTime } from 'luxon'
 import { actionHandler, parseString, root } from './shared'
 
+type RootCommitted = {
+  rootHash: string
+  totalAmount: BigNumber
+  destinationChainId: string
+  rootCommittedAt: string
+}
+
 root
   .command('bonded-unconfirmed-roots')
   .description('Get all roots that have been bonded but not confirmed')
@@ -17,55 +24,86 @@ root
 export async function main (source: any) {
   let { token, chain, endTimestamp } = source
 
+  const shouldLogData = isCliCall()
+
   if (!endTimestamp) {
     const now = DateTime.now().toUTC()
     endTimestamp = Math.floor(now.toSeconds())
   }
 
   const startTimestamp = 0
-  const rootsCommittedAndTotalAmounts = await getRootsCommittedAndTotalAmounts(chain, token, startTimestamp, endTimestamp)
+  const rootsCommitted = await getRootsCommitted(chain, token, startTimestamp, endTimestamp)
   const rootsConfirmed = await getTransferRootConfirmed(Chain.Ethereum, token, startTimestamp, endTimestamp)
   const rootsBonded = await getTransferRootBonded(Chain.Ethereum, token, startTimestamp, endTimestamp)
 
   const rootHashesConfirmed = rootsConfirmed.map((rootConfirmed: any) => rootConfirmed.rootHash)
   const rootHashesBonded = rootsBonded.map((rootBonded: any) => rootBonded.root)
 
+  const rootsToLog: RootCommitted[] = []
   let bondedUnconfirmedRootAmount: BigNumber = BigNumber.from(0)
-  for (const rootHash in rootsCommittedAndTotalAmounts) {
+  for (const rootCommitted of rootsCommitted) {
+    const rootHash = rootCommitted.rootHash
     if (rootHashesConfirmed.includes(rootHash)) continue
     if (!rootHashesBonded.includes(rootHash)) continue
 
-    bondedUnconfirmedRootAmount = bondedUnconfirmedRootAmount.add(rootsCommittedAndTotalAmounts[rootHash])
+    bondedUnconfirmedRootAmount = bondedUnconfirmedRootAmount.add(rootCommitted.totalAmount)
+    rootsToLog.push(rootCommitted)
   }
 
+  if (shouldLogData) {
+    console.log('rootHash, totalAmount, destinationChainId, rootCommittedAt')
+    const sortedRootsToLog = rootsToLog.sort((a, b) => Number(b.rootCommittedAt) - Number(a.rootCommittedAt))
+    for (const rootToLog of sortedRootsToLog) {
+      const date = DateTime.fromSeconds(Number(rootToLog.rootCommittedAt)).toUTC()
+      console.log(`${rootToLog.rootHash}, ${rootToLog.totalAmount.toString()}, ${rootToLog.destinationChainId || 'pre-regenesis'}, ${date}`)
+    }
+  }
   return bondedUnconfirmedRootAmount
 }
 
-async function getRootsCommittedAndTotalAmounts (
+function isCliCall (): boolean {
+  return process.argv[2] === 'bonded-unconfirmed-roots'
+}
+
+async function getRootsCommitted (
   chain: string,
   token: string,
   startTimestamp: number,
   endTimestamp: number
-): Promise<Record<string, BigNumber>> {
-  const rootsCommittedAmounts: Record<string, BigNumber> = {}
+): Promise<RootCommitted[]> {
+  const rootsCommitted: RootCommitted[] = []
   // This represents all destination chains
   const destinationChainId = 0
   const commitsRes = await getTransfersCommitted(chain, token, destinationChainId, startTimestamp, endTimestamp)
   for (const res of commitsRes) {
-    rootsCommittedAmounts[res.rootHash] = BigNumber.from(res.totalAmount)
+    rootsCommitted.push({
+      rootHash: res.rootHash,
+      totalAmount: BigNumber.from(res.totalAmount),
+      destinationChainId: res.destinationChainId,
+      rootCommittedAt: res.rootCommittedAt
+    })
   }
 
-  for (const rootHash in PreRegenesisRootsCommitted) {
-    if (rootsCommittedAmounts[rootHash]) {
-      throw new Error(`Duplicate root hash: ${rootHash}`)
+  for (const preRegenesisRootCommitted of PreRegenesisRootsCommitted) {
+    if (preRegenesisRootCommitted.token !== token) continue
+    if (chainIdToSlug(preRegenesisRootCommitted.sourceChainId) !== chain) continue
+
+    rootsCommitted.push({
+      rootHash: preRegenesisRootCommitted.rootHash,
+      totalAmount: BigNumber.from(preRegenesisRootCommitted.totalAmount),
+      destinationChainId: preRegenesisRootCommitted.destinationChainId,
+      rootCommittedAt: preRegenesisRootCommitted.rootCommittedAt
+    })
+  }
+
+  // Sanity check
+  let rootHashesSeen: string[] = []
+  for (const rootCommitted of rootsCommitted) {
+    if (rootHashesSeen.includes(rootCommitted.rootHash)) {
+      throw new Error(`Duplicate root hash: ${rootCommitted.rootHash}`)
     }
-
-    const nonRpcRootData = PreRegenesisRootsCommitted[rootHash]
-    if (nonRpcRootData.token !== token) continue
-    if (chainIdToSlug(nonRpcRootData.sourceChainId) !== chain) continue
-
-    rootsCommittedAmounts[rootHash] = BigNumber.from(nonRpcRootData.totalAmount)
+    rootHashesSeen.push(rootCommitted.rootHash)
   }
 
-  return rootsCommittedAmounts
+  return rootsCommitted
 }

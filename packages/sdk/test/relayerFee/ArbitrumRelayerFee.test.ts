bb812728309740d366429b9b0d323753109811f5
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2023-04-10 09:58:25 -0700

    core/sdk: bump

--- packages/sdk/test/relayerFee/ArbitrumRelayerFee.test.ts
+++ packages/sdk/test/relayerFee/ArbitrumRelayerFee.test.ts
@@ -3,8 +3,8 @@ import { RelayerFee } from '../../src/relayerFee'
 
 describe('RelayerFee', () => {
   it('Arbitrum relayer fee test', async () => {
-    const relayerFee = new RelayerFee('mainnet', 'USDC')
-    const relayCost = await relayerFee.getRelayCost(Chain.Arbitrum.slug)
-    expect(relayCost).toBeTruthy()
+    const relayerFee = new RelayerFee()
+    const gasCost = await relayerFee.getRelayCost('mainnet', Chain.Arbitrum.slug, 'USDC')
+    expect(gasCost).toBeTruthy()
   }, 60 * 1000)
 })

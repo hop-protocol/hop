ca1d5d53e12fa5647f0811e49de84e4e16550ef9
Miguel Mota <hello@miguelmota.com>
2023-04-30 20:22:36 -0700

    sdk: Add arbitrum goerli subgraph url

--- packages/sdk/src/utils/getGraphUrl.ts
+++ packages/sdk/src/utils/getGraphUrl.ts
@@ -20,7 +20,7 @@ export function getGraphUrl (network: string, chain: string) {
       throw new Error(`chain "${chain}" is not supported on goerli subgraphs`)
     }
     if (chain === 'arbitrum') {
-      throw new Error(`chain "${chain}" is not supported on goerli subgraphs`)
+      return 'https://arbitrum-goerli.subgraph.hop.exchange/subgraphs/name/hop-protocol/hop-arbitrum-goerli'
     }
     if (chain === 'xdai') {
       throw new Error(`chain "${chain}" is not supported on goerli subgraphs`)

4ebdaed840d745978dff7a6ffc6c0aeac1026487
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2023-05-15 14:46:57 -0700

    hop-node: lint

--- packages/hop-node/src/utils/getExponentialBackoffDelayMs.ts
+++ packages/hop-node/src/utils/getExponentialBackoffDelayMs.ts
@@ -2,4 +2,4 @@ const getExponentialBackoffDelayMs = (backoffIndex: number): number => {
   return (1 << backoffIndex) * 60 * 1000 // eslint-disable
 }
 
-export default getExponentialBackoffDelayMs 
+export default getExponentialBackoffDelayMs

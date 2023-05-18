078583afa40470281613d8b6abb42d5906188ddf
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2023-05-18 15:17:40 -0700

    hop-node: handle any unexpected sendTransaction err

--- packages/hop-node/src/types/error.ts
+++ packages/hop-node/src/types/error.ts
@@ -4,3 +4,4 @@ export class NonceTooLowError extends Error {}
 export class EstimateGasError extends Error {}
 export class PossibleReorgDetected extends Error {}
 export class RedundantProviderOutOfSync extends Error {}
+export class UnexpectedSendTransactionError extends Error {}

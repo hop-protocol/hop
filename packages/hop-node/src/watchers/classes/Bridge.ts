a5df6a616097a29737f839b7f0adbe213ce6c68b
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2023-06-05 19:56:47 -0700

    hop-node: lint

--- packages/hop-node/src/watchers/classes/Bridge.ts
+++ packages/hop-node/src/watchers/classes/Bridge.ts
@@ -14,9 +14,9 @@ import { L2_Bridge as L2BridgeContract } from '@hop-protocol/core/contracts/gene
 import { MultipleWithdrawalsSettledEvent, TransferRootSetEvent, WithdrawalBondSettledEvent, WithdrawalBondedEvent, WithdrewEvent } from '@hop-protocol/core/contracts/generated/Bridge'
 import { PriceFeed } from '@hop-protocol/sdk'
 import { State } from 'src/db/SyncStateDb'
-import { config as globalConfig } from 'src/config'
 import { formatUnits, parseEther, parseUnits, serializeTransaction } from 'ethers/lib/utils'
 import { getContractFactory, predeploys } from '@eth-optimism/contracts'
+import { config as globalConfig } from 'src/config'
 
 export type EventsBatchOptions = {
   syncCacheKey: string

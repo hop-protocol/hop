ddc95b4ee52dd4490f2bf359ca314cbed8f72757
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2023-06-06 18:37:55 -0700

    hop-node: add temp block times for urs

--- packages/hop-node/src/constants/constants.ts
+++ packages/hop-node/src/constants/constants.ts
@@ -50,7 +50,9 @@ export { nativeChainTokens }
 export const AvgBlockTimeSeconds: Record<string, number> = {
   [Chain.Ethereum]: 12,
   [Chain.Polygon]: 2,
-  [Chain.Gnosis]: 5
+  [Chain.Gnosis]: 5,
+  [Chain.Optimism]: 2, // TODO: temp remove
+  [Chain.Arbitrum]: 2 // TODO: temp remove
 }
 
 export const SettlementGasLimitPerTx: Record<string, number> = {

14fe6ad05ede3dc1b16d355c78e36c6ae296bc49
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2023-08-03 15:34:18 -0700

    fe: lint

--- packages/frontend/src/contexts/Web3Context.tsx
+++ packages/frontend/src/contexts/Web3Context.tsx
@@ -76,7 +76,7 @@ function getOnboardChains(): any {
         id: chainIdToHex(goerliChains.linea.networkId),
         token: 'ETH',
         label: 'Linea Goerli',
-        rpcUrl: 'https://rpc.goerli.linea.build' // NOTE: this rpc url has write access but it's more rate limitted
+        rpcUrl: 'https://rpc.goerli.linea.build' // NOTE: this rpc url has write access but it's more rate limited
       },
       {
         id: chainIdToHex(goerliChains.scrollzk.networkId),

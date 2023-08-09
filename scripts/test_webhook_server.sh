e59bd9e605a488c681974e0538968d4a9c061655
Miguel Mota <hello@miguelmota.com>
2023-08-09 14:07:22 -0700

    update scripts'

--- scripts/test_webhook_server.sh
+++ scripts/test_webhook_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mock_response='{"payload":{"reponame":"explorer","branch":"production-backend","build_parameters":{"CIRCLE_JOB":""},"outcome":"success","build_url":"https://app.circleci.com/jobs/github/hop-protocol/explorer/420"}}'
+mock_response='{"payload":{"reponame":"explorer","branch":"production-backend","build_parameters":{"CIRCLE_JOB":""},"outcome":"success","build_url":"https://app.circleci.com/jobs/github/hop-protocol/explorer/1"}}'
 
 curl -X POST -d $mock_response http://localhost:4665/postreceive

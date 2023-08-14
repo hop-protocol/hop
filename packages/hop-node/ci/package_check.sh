91cf35bea5e1bafaa5acd00a6d241799c8c550a7
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2023-08-14 15:32:56 -0700

    hop-node: update ci npm package check speed

--- packages/hop-node/ci/package_check.sh
+++ packages/hop-node/ci/package_check.sh
@@ -2,8 +2,8 @@
 
 curl -s "https://registry.npmjs.org/@hop-protocol/core/latest" | jq '.version'  > /tmp/core1.txt
 cat package.json | jq '.dependencies."@hop-protocol/core"' > /tmp/core2.txt
-diff /tmp/core1.txt /tmp/core2.txt || sleep 180
+diff /tmp/core1.txt /tmp/core2.txt || sleep 60
 
 curl -s "https://registry.npmjs.org/@hop-protocol/sdk/latest" | jq '.version' > /tmp/sdk1.txt
 cat package.json | jq '.dependencies."@hop-protocol/sdk"' > /tmp/sdk2.txt
-diff /tmp/sdk1.txt /tmp/sdk2.txt || sleep 180
+diff /tmp/sdk1.txt /tmp/sdk2.txt || sleep 60

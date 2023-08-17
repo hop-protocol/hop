d755417303face24b6edf0e0f118d4800ee5b6bb
Max Andrew <maxsandrew@gmail.com>
2023-08-17 19:22:05 -0400

    Update claim alert

--- packages/frontend/src/pages/Airdrop/ClaimDateMessage.tsx
+++ packages/frontend/src/pages/Airdrop/ClaimDateMessage.tsx
@@ -5,9 +5,9 @@ import Alert from 'src/components/alert/Alert'
 export function ClaimDateMessage () {
   return (
     <Box m={3} maxWidth={600} textAlign="center">
-      <Alert severity="warning">
+      <Alert severity="info">
         <Box textAlign="left">
-          The verified address snapshot for claiming was taken June 7 2022 8PM UTC, but eligible accounts can still submit an address up to 6 months after the initial claim period to receive tokens through a governance proposal.
+          The claim window has been reopened following a successful governance vote and will remain open for a period of 6 months. The verified address snapshot for claiming was taken June 7 2022 8PM UTC and eligible accounts that have not yet claimed can submit an address now.
         </Box>
       </Alert>
     </Box>

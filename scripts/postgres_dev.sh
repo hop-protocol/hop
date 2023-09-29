cc746069ebb107471375df8c3d76a72eb0e1d53a
Max Andrew <maxsandrew@gmail.com>
2023-09-29 12:37:34 -0400

    Update docker run command

--- scripts/postgres_dev.sh
+++ scripts/postgres_dev.sh
@@ -1,8 +1,6 @@
-#!/bin/env bash
-
 # docker create network=mynetwork
 
-docker run --net=host --rm --name pg -p 5432:5432 -e POSTGRES_PASSWORD=password postgres
+docker run -v $PWD/pgdata:/var/lib/postgresql/data --rm --name pg -p 5432:5432 -e POSTGRES_PASSWORD=password postgres
 
 # connect
 # docker run -it --rm --net=host postgres psql -h localhost -U postgres

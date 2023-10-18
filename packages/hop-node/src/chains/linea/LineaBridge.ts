9b90922f63d5b6635f20d2fe5ad94ad512a4b734
Max Andrew <maxsandrew@gmail.com>
2023-10-18 17:38:27 -0400

    hop-node: wip linea updates

--- packages/hop-node/src/chains/linea/LineaBridge.ts
+++ packages/hop-node/src/chains/linea/LineaBridge.ts
@@ -1,7 +1,8 @@
 import AbstractChainBridge from '../AbstractChainBridge'
 import { IChainBridge } from '../IChainBridge'
-import { Signer, providers, BigNumber } from 'ethers'
+import { Signer, providers, BigNumber, Contract } from 'ethers'
 import { LineaSDK } from '@consensys/linea-sdk'
+// import { L1MessageServiceContract, L2MessageServiceContract } from "../contracts"
 
 class LineaBridge extends AbstractChainBridge implements IChainBridge {
   l1Provider: any
@@ -39,36 +40,54 @@ class LineaBridge extends AbstractChainBridge implements IChainBridge {
   }
 
   private async _relayXDomainMessage (txHash: string, networkId: number, wallet: Signer): Promise<providers.TransactionResponse> {
-    const isRelayable = await this._isCheckpointed(txHash, networkId)
+    const contract = networkId === 0 ? this.LineaSDK.getL1Contract  : this.LineaSDK.getL2Contract
+
+    const isRelayable = await this._isCheckpointed(contract)
     if (!isRelayable) {
       throw new Error('expected deposit to be claimable')
     }
 
-    const contract = networkId === 0 ? this.LineaSDK.getL1Contract : this.LineaSDK.getL2Contract 
+    const messages = await contract.getMessagesByTransactionHash("0x4b72c6abacd3e2372a32e2797c41cab08df8d5e6fb2eb453e896e52fe7b70a27")
+    const txReceipt = await contract.getTransactionReceiptByMessageHash("0x13dd0f5e3611b44c88e80f5206bbe1ce1c6996514cef1e209e9eb06d9f5b9a2d")
 
     // return await contract.claim({ // claims message by message 
-    await contract.connect(this.l2Wallet).claim({
-      messageSender: "", // address of message sender
-      messageHash: "", // message hash
-      fee: BigNumber.from(1), // fee
-      destination: "", // destination address of message
-      value: BigNumber.from(2), // value of message
-      calldata: "0x", // call data
-      messageNonce: BigNumber.from(1), // message nonce
-      feeRecipient: "0x", // address that will receive fees. by default it is the message sender
+    return await contract.connect(this.l2Wallet).claim({
+      messageSender: txReceipt.messageSender, // "", // address of message sender
+      messageHash: txReceipt.messageHash, // "", // message hash
+      fee: txReceipt.fee, // BigNumber.from(1), // fee
+      destination: txReceipt.destination, // "", // destination address of message
+      value: txReceipt.value, // BigNumber.from(2), // value of message
+      calldata: txReceipt.calldata, // "0x", // call data
+      messageNonce: txReceipt.messageNonce, // BigNumber.from(1), // message nonce
+      feeRecipient: txReceipt.feeRecipient // "0x", // address that will receive fees. by default it is the message sender
     })
   }
 
-  private async _isCheckpointed (txHash: string, networkId: number): Promise<boolean> {
-    // if (networkId === 0) {
-    //   return this.LineaSDK.isDepositClaimable(txHash)
-    // } else if (networkId === 1) {
-    //   return this.LineaSDK.isWithdrawExitable(txHash)
+  private async _isCheckpointed (contract): Promise<boolean> {
+    const messageStatus = await contract.getMessageStatus("0x13dd0f5e3611b44c88e80f5206bbe1ce1c6996514cef1e209e9eb06d9f5b9a2d")
+    
+    // if (messageStatus === 'isClaimable' || 'isExitable') {
+    //   return true
     // } else {
-    //   throw new Error('invalid networkId')
+    //   return false
     // }
 
     return Promise.resolve(true)
   }
 }
 export default LineaBridge
+
+
+/*
+  import AbstractChainBridge from '../AbstractChainBridge'
+  import { IChainBridge } from '../IChainBridge'
+  import { providers } from 'ethers'
+
+  class LineaBridge extends AbstractChainBridge implements IChainBridge {
+    async relayL2ToL1Message (txHash: string): Promise<providers.TransactionResponse> {
+      throw new Error('unimplemented')
+    }
+  }
+
+  export default LineaBridge
+*/

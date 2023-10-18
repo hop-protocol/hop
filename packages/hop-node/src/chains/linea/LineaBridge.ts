76257e97c189c926bcc1b30ba21143019e02b1a3
Max Andrew <maxsandrew@gmail.com>
2023-10-18 15:36:29 -0400

    hop-node: wip tightening linea bridge file

--- packages/hop-node/src/chains/linea/LineaBridge.ts
+++ packages/hop-node/src/chains/linea/LineaBridge.ts
@@ -1,17 +1,9 @@
 import AbstractChainBridge from '../AbstractChainBridge'
 import { IChainBridge } from '../IChainBridge'
-import { Signer, providers } from 'ethers'
+import { Signer, providers, BigNumber } from 'ethers'
 import { LineaSDK } from '@consensys/linea-sdk'
 
-/*
-import wait from 'src/utils/wait'
-import { CanonicalMessengerRootConfirmationGasLimit } from 'src/constants'
-import { Web3ClientPlugin } from '@maticnetwork/maticjs-ethers'
-import { ZkEvmClient, setProofApi, use } from '@maticnetwork/maticjs'
-*/
-
 class LineaBridge extends AbstractChainBridge implements IChainBridge {
-  ready: boolean = false
   l1Provider: any
   l2Provider: any
   l1Wallet: Signer
@@ -20,33 +12,18 @@ class LineaBridge extends AbstractChainBridge implements IChainBridge {
   apiUrl: string
   lineaMainnetChainId: number = 59144
   LineaSDK: LineaSDK
-  messengerAddress: string
 
   constructor (chainSlug: string) {
     super(chainSlug)
 
-    // this.apiUrl = `https://proof-generator.polygon.technology/api/v1/${
-    //   this.chainId === this.polygonzkMainnetChainId ? 'matic' : 'mumbai'
-    // }/block-included`
-
-    // use(Web3ClientPlugin)
-    // setProofApi('https://proof-generator.polygon.technology/')
-
     this.LineaSDK = new LineaSDK({
       l1RpcUrl: process.env.L1_RPC_URL ?? "", // L1 rpc url
       l2RpcUrl: process.env.L2_RPC_URL ?? "", // L2 rpc url
-      l1SignerPrivateKey: this.l1Wallet // process.env.L1_SIGNER_PRIVATE_KEY ?? "", // L1 account private key (optional if you use mode = read-only)
-      l2SignerPrivateKey: this.l2Wallet // process.env.L2_SIGNER_PRIVATE_KEY ?? "", // L2 account private key (optional if you use mode = read-only)
+      // l1SignerPrivateKey: this.l1Wallet // process.env.L1_SIGNER_PRIVATE_KEY ?? "", // L1 account private key (optional if you use mode = read-only)
+      // l2SignerPrivateKey: this.l2Wallet // process.env.L2_SIGNER_PRIVATE_KEY ?? "", // L2 account private key (optional if you use mode = read-only)
       network: "linea-goerli", // network you want to interact with (either linea-mainnet or linea-goerli)
-      mode: "read-write", // contract wrapper class mode (read-only or read-write), read-only: only read contracts state, read-write: read contracts state and claim messages 
+      mode: "read-only", // contract wrapper class mode (read-only or read-write), read-only: only read contracts state, read-write: read contracts state and claim messages 
     })
-
-    // this.messengerAddress = this.chainId === this.polygonzkMainnetChainId ? '0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe' : '0xF6BEEeBB578e214CA9E23B0e9683454Ff88Ed2A7'
-
-    // this.init()
-    //   .catch((err: any) => {
-    //     this.logger.error('zkEvmClient initialize error:', err)
-    //   })
   }
 
   async relayL1ToL2Message (l1TxHash: string): Promise<providers.TransactionResponse> {
@@ -67,10 +44,10 @@ class LineaBridge extends AbstractChainBridge implements IChainBridge {
       throw new Error('expected deposit to be claimable')
     }
 
-    const isParent = networkId === 0
-    const claimPayload = await this.zkEvmClient.bridgeUtil.buildPayloadForClaim(txHash, isParent, networkId)
+    const contract = networkId === 0 ? this.LineaSDK.getL1Contract : this.LineaSDK.getL2Contract 
 
-    return await l2Contract.claim({ // claims message by message 
+    // return await contract.claim({ // claims message by message 
+    await contract.connect(this.l2Wallet).claim({
       messageSender: "", // address of message sender
       messageHash: "", // message hash
       fee: BigNumber.from(1), // fee
@@ -83,13 +60,15 @@ class LineaBridge extends AbstractChainBridge implements IChainBridge {
   }
 
   private async _isCheckpointed (txHash: string, networkId: number): Promise<boolean> {
-    if (networkId === 0) {
-      return this.LineaSDK.isDepositClaimable(txHash)
-    } else if (networkId === 1) {
-      return this.LineaSDK.isWithdrawExitable(txHash)
-    } else {
-      throw new Error('invalid networkId')
-    }
+    // if (networkId === 0) {
+    //   return this.LineaSDK.isDepositClaimable(txHash)
+    // } else if (networkId === 1) {
+    //   return this.LineaSDK.isWithdrawExitable(txHash)
+    // } else {
+    //   throw new Error('invalid networkId')
+    // }
+
+    return Promise.resolve(true)
   }
 }
 export default LineaBridge

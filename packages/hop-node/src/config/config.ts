fb017685968bcb353f7f4970ef4deb3bf95b8b23
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2023-12-20 13:13:47 -0800

    hop-node: enforcement bug fix

--- packages/hop-node/src/config/config.ts
+++ packages/hop-node/src/config/config.ts
@@ -67,7 +67,7 @@ export const expectedNameservers = normalizeEnvVarArray(process.env.EXPECTED_APP
 export const modifiedLiquidityRoutes = process.env.MODIFIED_LIQUIDITY_ROUTES?.split(',') ?? []
 export const wsEnabledChains = process.env.WS_ENABLED_CHAINS?.split(',') ?? []
 export const BondThreshold = normalizeEnvVarNumber(process.env.BOND_THRESHOLD) ?? DefaultBondThreshold
-export const EnforceRelayerFee = process.env.ENFORCE_RELAYER_FEE ?? false
+export const EnforceRelayerFee = !!process.env.ENFORCE_RELAYER_FEE ?? false
 
 // Decreasing SyncCyclesPerFullSync will result in more full syncs (root data) more often. This is useful for the
 // available liquidity watcher to have up-to-date info

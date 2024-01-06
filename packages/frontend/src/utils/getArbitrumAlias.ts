2994d50c135bcc2d6761a51ad2feb852ba83e071
Miguel Mota <hello@miguelmota.com>
2024-01-06 10:57:26 -0800

    frontend: remove unused utils

--- packages/frontend/src/utils/getArbitrumAlias.ts
+++
@@ -1,9 +0,0 @@
-import { BigNumber } from 'ethers'
-
-export const getArbitrumAlias = (address: string): string => {
-  const addressBn: BigNumber = BigNumber.from(address)
-  const aliasMask: string = '0x1111000000000000000000000000000000001111'
-  const aliasMaskBn: BigNumber = BigNumber.from(aliasMask)
-  const boundary: BigNumber = BigNumber.from('0x10000000000000000000000000000000000000000')
-  return addressBn.add(aliasMaskBn).mod(boundary).toHexString()
-}

0a746f87ce6a1ef6ca5c742ea894ecdadd701c43
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-01-15 13:05:50 -0800

    all: various lint updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:json/recommended"
   ],
   "parser": "@typescript-eslint/parser",

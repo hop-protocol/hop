3754a398c65c9a246c66409f3bb7da3f4d1c3149
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-02-07 18:55:22 -0800

    hop-node: fix polygon imports

--- packages/hop-node/src/chains/Chains/polygonzk/PolygonZkMessageService.ts
+++ packages/hop-node/src/chains/Chains/polygonzk/PolygonZkMessageService.ts
@@ -2,7 +2,7 @@ import wait from 'src/utils/wait'
 import { AbstractMessageService, IMessageService, MessageDirection } from 'src/chains/Services/AbstractMessageService'
 import { CanonicalMessengerRootConfirmationGasLimit } from 'src/constants'
 import { Web3ClientPlugin } from '@maticnetwork/maticjs-ethers'
-import { ZkEvmBridge, ZkEvmClient, setProofApi, use } from '@maticnetwork/maticjs-zkevm'
+import { ZkEvmBridge, ZkEvmClient, setProofApi, use } from '@maticnetwork/maticjs-pos-zkevm'
 import { providers } from 'ethers'
 
 /**

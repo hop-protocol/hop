0410507757b97931b6dadbf4ce3a83165bb175c2
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-02-07 19:23:08 -0800

    stats-worker: fix polygon

--- packages/stats-worker/src/utils/getBlockNumberFromDate.ts
+++ packages/stats-worker/src/utils/getBlockNumberFromDate.ts
@@ -11,7 +11,7 @@ export async function getBlockNumberFromDate (
   try {
     const useEtherscan = etherscanApiKeys[chain]
     // Polygon's Etherscan returns invalid and inconsistent data, so skip it
-    if (useEtherscan || chain !== 'polygon') {
+    if (useEtherscan && chain !== 'polygon') {
       return await getBlockNumberFromDateUsingEtherscan(chain, timestamp)
     }
 
@@ -63,7 +63,7 @@ async function getBlockNumberFromDateUsingLib (
   let info
   while (true) {
     try {
-      info = blockDater.getDate(date)
+      info = await (blockDater.getDate(date) as any)
       if (!info) {
         throw new Error('could not retrieve block number')
       }

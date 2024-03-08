1b2cbf56e401d5c2c1618b7684f855c958d62cac
Miguel Mota <hello@miguelmota.com>
2024-03-07 22:15:11 -0800

    core: add sepolia native usdc addresses

--- packages/core/src/addresses/sepolia.ts
+++ packages/core/src/addresses/sepolia.ts
@@ -4,5 +4,19 @@ export const addresses: Addresses = {
   bonders: {
   },
   bridges: {
+    USDC: {
+      ethereum: {
+        l1CanonicalToken: '0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238',
+        l1Bridge: '' // TODO cctp address
+      },
+      arbitrum: {
+        l2CanonicalToken: '0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d',
+        l2Bridge: '' // TODO cctp address
+      },
+      base: {
+        l2CanonicalToken: '0x036CbD53842c5426634e7929541eC2318f3dCF7e',
+        l2Bridge: '' // TODO cctp address
+      }
+    },
   }
 }

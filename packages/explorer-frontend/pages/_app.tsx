0074a07abb8a99b04b9906fef7ad1105eaec341a
Miguel Mota <hello@miguelmota.com>
2024-03-26 19:25:43 -0700

    explorer-frontend: build fixes

--- packages/explorer-frontend/pages/_app.tsx
+++ packages/explorer-frontend/pages/_app.tsx
@@ -2,8 +2,9 @@ import '../styles/globals.css'
 import { ThemeProvider } from '@mui/material/styles'
 import { useTheme } from '../components/_useTheme'
 import type { AppProps } from 'next/app'
+import { ReactElement } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
   const { theme, dark, toggleTheme } = useTheme()
 
   return (

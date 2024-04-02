7c178994211d5d78699a36a0b8c6075d9c242b08
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-04-02 11:23:42 -0700

    all: rm core

--- packages/sdk-core/eslint.config.mjs
+++ packages/sdk-core/eslint.config.mjs
@@ -4,3 +4,18 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   ...baseConfig
 )
+// extends: '../../.eslintrc.js',
+// // TODO: Remove this later
+// ignorePatterns: [
+//   // TODO: Remove after fixing up tests
+//   'test',
+//   // TODO: Remove after removal of typechain
+//   'src/contracts',
+//   // TODO: Remove after removal of top-level dirs
+//   'abi',
+//   'addresses',
+//   'config',
+//   'contracts',
+//   'metadata',
+//   'networks'
+// ]

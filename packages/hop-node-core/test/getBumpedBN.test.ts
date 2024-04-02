47cce607fdb9df9aaf8a82020d2861ad817dc60f
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-04-02 07:48:10 -0700

    all: update lint

--- packages/hop-node-core/test/getBumpedBN.test.ts
+++ packages/hop-node-core/test/getBumpedBN.test.ts
@@ -1,5 +1,5 @@
-import { getBumpedBN } from '#utils/getBumpedBN.js'
 import { BigNumber } from 'ethers'
+import { getBumpedBN } from '#utils/getBumpedBN.js'
 
 test('getBumpedBN', () => {
   expect(getBumpedBN(BigNumber.from('20'), 1.5).toString()).toBe(BigNumber.from('30').toString())

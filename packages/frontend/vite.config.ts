5a10cf1571fca9e13362fc3a9018a962bf2a985f
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-04-09 07:05:00 -0700

    fe: rm unused vite alias stream

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -205,8 +205,7 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      'src': path.resolve(__dirname, './src'),
-      'stream': 'stream-browserify',
+      'src': path.resolve(__dirname, './src')
     },
   },
 })

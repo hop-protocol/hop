6c228f5d361af1625d6030f0ce66677c00351ba6
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-04-10 17:06:05 -0700

    sdk-core: revert to working esm/cjs

--- packages/sdk-core/src/utils/promiseQueue.ts
+++ packages/sdk-core/src/utils/promiseQueue.ts
@@ -1,18 +1,15 @@
-// import PQueue from 'p-queue'
+import PQueue from 'p-queue'
 
 export type Options = {
   concurrency: number
 }
 
 export async function promiseQueue (items: any[], cb: any, options: Options) {
-  await Promise.all(items.map(async (item, i) => {
-    await cb(item, i)
-  }))
-  // const { concurrency } = options
-  // const queue = new PQueue({ concurrency })
-  // for (let i = 0; i < items.length; i++) {
-  //   queue.add(async () => cb(items[i], i))
-  // }
-  // await queue.onEmpty()
-  // await queue.onIdle()
+  const { concurrency } = options
+  const queue = new PQueue({ concurrency })
+  for (let i = 0; i < items.length; i++) {
+    queue.add(async () => cb(items[i], i))
+  }
+  await queue.onEmpty()
+  await queue.onIdle()
 }

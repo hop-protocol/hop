bd4a06e8d2e3199d37c497f640786bfd372b491a
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-04-19 12:02:45 -0700

    hnc: update pzk times

--- packages/hop-node-core/src/finality/strategies/default/PolygonZkStrategy.ts
+++ packages/hop-node-core/src/finality/strategies/default/PolygonZkStrategy.ts
@@ -12,9 +12,9 @@ import type { IFinalityStrategy } from '../IFinalityStrategy.js'
 
 export class PolygonZkStrategy extends FinalityStrategy implements IFinalityStrategy {
   override getSafeBlockNumber = async (): Promise<number> => {
-    // Somewhat arbitrary, about 25 minutes
+    // Somewhat arbitrary, about 30 minutes
     // Approximately 1/2 the time to full finality
-    const confirmations = 512
+    const confirmations = 600
     return this.getProbabilisticBlockNumber(confirmations)
   }
 }

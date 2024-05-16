26a3614a52c59fa462f62def2a764e32e387621a
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-05-16 13:10:23 -0700

    gh: update gh flow

--- packages/hop-node/src/types/error.ts
+++ packages/hop-node/src/types/error.ts
@@ -2,4 +2,4 @@ export class BonderFeeTooLowError extends Error {}
 export class RelayerFeeTooLowError extends Error {}
 export class BonderTooEarlyError extends Error {}
 export class UnfinalizedTransferBondError extends Error {}
-const cacheBuster = 28
+const cacheBuster = 29

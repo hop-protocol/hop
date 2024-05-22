316fa057eec83d6848f14a768fe8360fd24a86ae
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-05-22 09:56:42 -0700

    gh: test cache

--- packages/frontend/src/config/config.ts
+++ packages/frontend/src/config/config.ts
@@ -3,7 +3,7 @@ import { providers } from 'ethers'
 export const showBannerMessage = process.env.REACT_APP_SHOW_BANNER_MESSAGE ?? ''
 export const gitRevision = process.env.REACT_APP_GIT_SHA ?? ''
 
-// show rewards header nav route
+// show rewards header nav route.
 let showRewards = true
 if (process.env.REACT_APP_NETWORK === 'goerli') {
   showRewards = false

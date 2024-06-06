8b31ec716245acd9947c570f09ba5ee5388f1a06
Miguel Mota <hello@miguelmota.com>
2024-06-06 14:26:03 -0700

    sdk: update public rpcs, frontend: mui error fixes

--- packages/frontend/src/components/Footer/useBlockNumber.tsx
+++ packages/frontend/src/components/Footer/useBlockNumber.tsx
@@ -1,13 +1,17 @@
 import { ethers } from 'ethers'
 import { useQuery } from 'react-query'
+import { reactAppNetwork } from '#config/index.js'
+import { getChain, ChainSlug, NetworkSlug } from '@hop-protocol/sdk'
 
 export function useBlockNumber() {
+
   const { data: blockNumber } = useQuery(
     [
       'useBlockNumber',
     ],
     async () => {
-      const provider = new ethers.providers.JsonRpcProvider({ allowGzip: true, url: 'https://mainnet.infura.io/v3/84842078b09946638c03157f83405213' })
+      const publicRpcUrl = getChain(reactAppNetwork as NetworkSlug, ChainSlug.Ethereum).publicRpcUrl
+      const provider = new ethers.providers.JsonRpcProvider({ allowGzip: true, url: publicRpcUrl })
       const blockNumber = await provider.getBlockNumber()
       return blockNumber.toString()
     },

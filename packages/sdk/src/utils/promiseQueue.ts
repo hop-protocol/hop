9636a36c32c6354340d9add1c3d8ae821ba0fc0d
Miguel Mota <hello@miguelmota.com>
2024-06-11 23:45:02 -0700

    sdk: promiseQueue temp fixes

--- packages/sdk/src/utils/promiseQueue.ts
+++ packages/sdk/src/utils/promiseQueue.ts
@@ -7,7 +7,15 @@ export type Options = {
 export async function promiseQueue (items: any[], cb: any, options: Options): Promise<void> {
   const { concurrency } = options
   // TODO debug why PQueue is not being imported correctly when sdk-core is used in v2-sdk
-  const queue = (PQueue as any)?.default ? new (PQueue as any).default({ concurrency }) : new PQueue({ concurrency })
+  let Q: any = PQueue
+  if (Q?.default?.PQueue) {
+    Q = Q.default.PQueue
+  } else if (Q?.PQueue) {
+    Q = Q.PQueue
+  } else if (Q?.default) {
+    Q = Q.default
+  }
+  const queue = new Q({ concurrency })
   for (let i = 0; i < items.length; i++) {
     queue.add(async () => cb(items[i], i))
   }

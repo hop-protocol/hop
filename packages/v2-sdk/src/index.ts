0bb80d5a83e30f4ae43162e588f149ef4ad9493d
Miguel Mota <hello@miguelmota.com>
2024-06-13 01:01:15 -0700

    v2-sdk: add event type exports

--- packages/v2-sdk/src/index.ts
+++ packages/v2-sdk/src/index.ts
@@ -6,3 +6,17 @@ export {
   ApproveSendTokensInput,
   Hop
 } from './Hop.js'
+export {
+  TransferSent,
+  TransferBonded
+} from './railsGateway/index.js'
+export {
+  BundleCommitted,
+  BundleForwarded,
+  BundleReceived,
+  BundleSet,
+  FeesSentToHub,
+  MessageBundled,
+  MessageExecuted,
+  MessageSent
+} from './messenger/index.js'

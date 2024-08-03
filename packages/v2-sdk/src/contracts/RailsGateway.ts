b53c849319c48db97cd557f40fcdb558726f4318
Miguel Mota <hello@miguelmota.com>
2024-08-02 20:32:50 -0700

    v2-explorer-frontend: update toml, clean cache

--- packages/v2-sdk/src/contracts/RailsGateway.ts
+++ packages/v2-sdk/src/contracts/RailsGateway.ts
@@ -26,7 +26,7 @@ import type {
   TypedListener,
   OnEvent,
   PromiseOrValue,
-} from "./common";
+} from "./common.js";
 
 export type HopStruct = {
   pathId: PromiseOrValue<BytesLike>;
--- packages/v2-sdk/src/contracts/factories/RailsGateway__factory.ts
+++ packages/v2-sdk/src/contracts/factories/RailsGateway__factory.ts
@@ -4,7 +4,7 @@
 
 import { Contract, Signer, utils } from "ethers";
 import type { Provider } from "@ethersproject/providers";
-import type { RailsGateway, RailsGatewayInterface } from "../RailsGateway";
+import type { RailsGateway, RailsGatewayInterface } from "../RailsGateway.js";
 
 const _abi = [
   {

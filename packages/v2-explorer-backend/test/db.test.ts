f931f7693bc0aa0d598816a43be9597fd9912771
Miguel Mota <hello@miguelmota.com>
2024-08-22 21:44:50 -0700

    v2-explorer-backend: deterministic stringify util

--- packages/v2-explorer-backend/test/db.test.ts
+++ packages/v2-explorer-backend/test/db.test.ts
@@ -14,32 +14,7 @@ import { PriceTable } from '#pgDb/prices/index.js'
 import { TokenTable } from '#pgDb/tokens/index.js'
 import { postgresConfig } from '#config/index.js'
 import { generateMockBundleCommitted, generateMockEventContext, generateRandomInt, generateMockTransferSent, generateMockTransferBonded, generateRandomAddress, generateRandomUint256, generateMockBundleForwarded, generateMockBundleReceived, generateMockBundleSet, generateMockFeesSentToHub, generateMockMessageBundled, generateMockMessageExecuted, generateMockMessageSent, generateRandomBytes32, generateMockPath, generateMockToken, generateRandomString, generateMockPrice } from '#utils/mockDataGenerator.js'
-import stringify from 'json-stable-stringify'
-
-// Helper function to recursively sort arrays of objects
-function sortNestedArrays(obj: any): any {
-  if (Array.isArray(obj)) {
-    return obj.map(sortNestedArrays).sort((a, b) => {
-      if (typeof a === 'object' && typeof b === 'object') {
-        return JSON.stringify(a).localeCompare(JSON.stringify(b))
-      }
-      return 0
-    })
-  } else if (obj !== null && typeof obj === 'object') {
-    const sortedObj: Record<string, any> = {}
-    Object.keys(obj).sort().forEach(key => {
-      sortedObj[key] = sortNestedArrays(obj[key])
-    })
-    return sortedObj
-  }
-  return obj
-}
-
-// Generalized function to stringify objects deterministically
-function deterministicStringify(obj: any): string {
-  const sortedObj = sortNestedArrays(obj)
-  return stringify(sortedObj)
-}
+import { deterministicStringify } from '#utils/deterministicStringify.js'
 
 describe.only('Db', () => {
   const db = pgp({})({ ...postgresConfig })

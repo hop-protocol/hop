771c3a11fbdd6a001d8c4f4c17fd9a18f51694fb
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-09-10 15:31:02 -0700

    v2hn: simplified gasboost err

--- packages/v2-hop-node/src/gasboost/utils.ts
+++ packages/v2-hop-node/src/gasboost/utils.ts
@@ -1,22 +1,5 @@
-import {
-  NonceTooLowError,
-  EstimateGasError,
-  InsufficientFundsError
-} from '#types/error.js'
-import type { EVMError } from './types.js'
+import { EVMError } from './error.js'
 
 export function isEVMError (err: unknown): err is EVMError {
-  if (err instanceof NonceTooLowError) {
-    // TODO: Log?
-    return true
-  } else if (err instanceof EstimateGasError) {
-    // TODO: Log?
-    return true
-  } else if (err instanceof InsufficientFundsError) {
-    // TODO: Log?
-    return true
-  } else {
-    // todo
-    return true
-  }
-
+  return err instanceof EVMError
+}

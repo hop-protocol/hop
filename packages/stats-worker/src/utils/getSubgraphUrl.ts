44e5f3a80438df0c9d23cb7c834723881dcca9db
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2024-09-19 14:15:43 -0700

    sw: patch and bump

--- packages/stats-worker/src/utils/getSubgraphUrl.ts
+++ packages/stats-worker/src/utils/getSubgraphUrl.ts
@@ -1,4 +1,4 @@
-import { getChain } from '@hop-protocol/sdk/chains'
+import { type NetworkSlug, type ChainSlug, getChain } from '@hop-protocol/sdk/chains'
 
 export function getSubgraphUrl (
   chain: string,
@@ -8,7 +8,9 @@ export function getSubgraphUrl (
     return `http://localhost:8000/subgraphs/name/hop-protocol/hop-${chain}`
   }
 
-  const url = getChain(chain)?.subgraphUrl
+  const networkSlug = 'mainnet' as NetworkSlug
+  const chainSlug = chain as ChainSlug
+  const url = getChain(networkSlug, chainSlug)?.subgraphUrl
   if (!url) {
     throw new Error(`subgraph url not found for chain ${chain}`)
   }

a4e285be205f4a18bebbb5540893dc97a3a74e72
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2025-02-26 15:03:48 -0800

    v2hn: faster arb sync

--- packages/v2-hop-node/src/constants/constants.ts
+++ packages/v2-hop-node/src/constants/constants.ts
@@ -59,7 +59,7 @@ export const MAX_BLOCK_RANGE_PER_GET_LOG_CALL: Record<ChainSlug, number> = {
   [ChainSlug.Polygon]: DEFAULT_MAX_BLOCK_RANGE,
   [ChainSlug.Gnosis]: DEFAULT_MAX_BLOCK_RANGE,
   [ChainSlug.Optimism]: DEFAULT_MAX_BLOCK_RANGE,
-  [ChainSlug.Arbitrum]: DEFAULT_MAX_BLOCK_RANGE,
+  [ChainSlug.Arbitrum]: 20_000,
   [ChainSlug.Nova]: DEFAULT_MAX_BLOCK_RANGE,
   [ChainSlug.ZkSync]: DEFAULT_MAX_BLOCK_RANGE,
   [ChainSlug.Linea]: DEFAULT_MAX_BLOCK_RANGE,

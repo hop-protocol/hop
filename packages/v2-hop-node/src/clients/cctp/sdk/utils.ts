beb1285cdf9187fa6de70e6a3cb3198f9c793789
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2025-02-27 04:50:45 -0800

    v2hn: updated base cctp time

--- packages/v2-hop-node/src/clients/cctp/sdk/utils.ts
+++ packages/v2-hop-node/src/clients/cctp/sdk/utils.ts
@@ -15,7 +15,7 @@ export const AttestationTimeForChainIdMs: Record<string, Partial<Record<ChainSlu
     [ChainSlug.Ethereum]: 25 * 60 * 1000,
     [ChainSlug.Optimism]: 25 * 60 * 1000,
     [ChainSlug.Arbitrum]: 25 * 60 * 1000,
-    [ChainSlug.Base]: 25 * 60 * 1000,
+    [ChainSlug.Base]: 30 * 60 * 1000,
     [ChainSlug.Polygon]: 35 * 60 * 1000
   },
   [NetworkSlug.Sepolia]: {

3793260d8950a7d089bcf17b027ef48a61b4729d
Miguel Mota <hello@miguelmota.com>
2025-04-21 23:50:37 -0700

    frontend: minor import fixes

--- packages/frontend/src/pages/Send/index.tsx
+++ packages/frontend/src/pages/Send/index.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense, lazy } from 'react'
-import { Loading } from '../../components/Loading'
+import { Loading } from '#components/Loading/index.js'
 
-const SendComponent = lazy(() => import('./Send'))
+const SendComponent = lazy(() => import('./Send.js'))
 
 const Send: React.FC = () => {
   return (

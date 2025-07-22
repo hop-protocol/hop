504e371ba6fb6262da0ec11d9bc630f932f89aba
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2025-07-22 14:26:50 -0700

    v2: updated claimReadded

--- packages/v2-sdk/src/railsGateway/events/ClaimRemoved.ts
+++ packages/v2-sdk/src/railsGateway/events/ClaimRemoved.ts
@@ -4,12 +4,10 @@ import { RailsPath__factory } from '#contracts/factories/RailsPath__factory.js'
 
 // event from RailsPath
 export interface ClaimRemoved {
-  pathId: string
   claimId: string
 }
 
 export type ClaimRemovedIndexes = {
-  pathId?: string
   claimId?: string
 }
 
@@ -18,28 +16,22 @@ export class ClaimRemovedEventFetcher extends Event<ClaimRemoved> {
   override abi = RailsPath__factory.abi
   override factory = RailsPath__factory
 
-  getPathIdFilter (pathId: string): EventFilter {
-    return this.getFilterWithIndexes({ pathId })
-  }
-
   getClaimIdFilter (claimId: string): EventFilter {
     return this.getFilterWithIndexes({ claimId })
   }
 
-  getFilterWithIndexes ({ pathId, claimId } : ClaimRemovedIndexes): EventFilter {
+  getFilterWithIndexes ({ claimId } : ClaimRemovedIndexes): EventFilter {
     const railsGateway = this.getContract()
-    const filter = railsGateway.filters.ClaimRemoved(pathId ?? null, claimId ?? null)
+    const filter = railsGateway.filters.ClaimRemoved(claimId ?? null)
     return filter
   }
 
   override toTypedEvent (ethersEvent: EthersEvent): ClaimRemoved {
     const parsed = this.parseEthersEventLog(ethersEvent)
 
-    const pathId = parsed.args.pathId.toString()
     const claimId = parsed.args.claimId.toString()
 
     return {
-      pathId,
       claimId
     }
   }

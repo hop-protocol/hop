a6d4d147b9ac3ec4145a80a67d305e8f1811cbdf
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2025-08-06 12:23:59 -0700

    fe: correct token

--- packages/frontend/src/pages/Convert/ConvertOption/HopConvertOption.tsx
+++ packages/frontend/src/pages/Convert/ConvertOption/HopConvertOption.tsx
@@ -124,7 +124,7 @@ class HopConvertOption extends ConvertOption {
     let warning : any
 
     console.log('debug1')
-    const isDeprecatedRouteWithdrawal = ['hUSDC.e', 'hMAGIC', 'DAI'].includes(token.symbol) && destNetwork?.slug === ChainSlug.Ethereum
+    const isDeprecatedRouteWithdrawal = ['hUSDC.e', 'hMAGIC', 'hDAI'].includes(token.symbol) && destNetwork?.slug === ChainSlug.Ethereum
 
     // note: bypass bonder fee since USDC.e out of L2 to ethereum (deprecated token route) will have to go through the 7 day exit time and be manually withdrawn.
     if (isDeprecatedRouteWithdrawal) {

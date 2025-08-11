23df3916a2e6f1526e3eb837b898367b8f5763a3
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2025-08-11 12:33:48 -0700

    sdk: lint

--- packages/v2-sdk/src/rails/contracts/RailsFactory.ts
+++ packages/v2-sdk/src/rails/contracts/RailsFactory.ts
@@ -1,7 +1,7 @@
 import type { RPC } from '../types.js'
 import { RailsGateway } from './RailsGateway.js'
 import { RailsPath } from './RailsPath.js'
-import { getGateway} from '#models/index.js'
+import { getGateway } from '#models/index.js'
 
 export function getRailsGateway(chainId: string, rpc: RPC): RailsGateway {
   const address = getGateway(chainId).railsGateway
